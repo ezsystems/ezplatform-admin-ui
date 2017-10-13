35c75d8237edaede19fc48f235658c0a71ac4b17
Nattfarinn <nattfarinn@gmail.com>
2017-10-13 14:24:24 +0200

    EZP-27945: As an editor I would like to use multi-file upload in the location view

--- src/bundle/Resources/public/js/scripts/admin.location.view.js
+++ src/bundle/Resources/public/js/scripts/admin.location.view.js
@@ -1,12 +1,38 @@
 (function () {
     const listContainers = [...document.querySelectorAll('.ez-sil')];
+    const mfuContainer = document.querySelector('#ez-mfu')
     const token = document.querySelector('meta[name="CSRF-Token"]').content;
     const siteaccess = document.querySelector('meta[name="SiteAccess"]').content;
+    const mfuAttrs = {
+            adminUiConfig: {
+                ...window.eZ.adminUiConfig,
+            token,
+            siteaccess
+        },
+        parentInfo: {
+        contentTypeIdentifier: mfuContainer.dataset.parentContentTypeIdentifier,
+            contentTypeId: mfuContainer.dataset.parentContentTypeId,
+            locationPath: mfuContainer.dataset.parentLocationPath,
+            language: mfuContainer.dataset.parentContentLanguage
+        },
+    };
 
     listContainers.forEach(container => {
         ReactDOM.render(React.createElement(SubItems.default, {
             startingLocationId: container.dataset.location,
-            restInfo: {token, siteaccess}
+            restInfo: {token, siteaccess},
+            extraActions: [{
+                component: MultiFileUpload.default,
+                attrs: Object.assign({}, mfuAttrs, {
+                    onPopupClose: (itemsUploaded) => {
+                        if (itemsUploaded.length) {
+                            window.location.reload(true);
+                        }
+                    },
+                    popupOnly: false,
+                    asButton: true
+                })
+            }]
         }), container);
     });
 })();
---
+++ src/lib/ApplicationConfig/Providers/ContentTypeMappings.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\EzPlatformAdminUi\ApplicationConfig\Providers;
+
+use EzSystems\EzPlatformAdminUi\UI\Config\ProviderInterface;
+
+/**
+ * Class responsible for generating PlatformUI configuration for Multi File Upload functionality.
+ */
+class ContentTypeMappings implements ProviderInterface
+{
+    /** @var array */
+    protected $locationMappings = [];
+
+    /** @var array */
+    protected $defaultMappings = [];
+
+    /** @var array */
+    protected $fallbackContentType = [];
+
+    /** @var int */
+    protected $maxFileSize = 0;
+
+    /**
+     * @param array $locationMappings
+     * @param array $defaultMappings
+     * @param array $fallbackContentType
+     * @param int $maxFileSize
+     */
+    public function __construct(
+        array $locationMappings,
+        array $defaultMappings,
+        array $fallbackContentType,
+        $maxFileSize
+    ) {
+        $this->locationMappings = $locationMappings;
+        $this->defaultMappings = $defaultMappings;
+        $this->fallbackContentType = $fallbackContentType;
+        $this->maxFileSize = $maxFileSize;
+    }
+
+    /**
+     * Returns configuration structure compatible with PlatformUI.
+     *
+     * @return array
+     */
+    public function getConfig(): array
+    {
+        $structure = [
+            'locationMappings' => [],
+            'defaultMappings' => [],
+            'fallbackContentType' => $this->buildFallbackContentTypeStructure($this->fallbackContentType),
+            'maxFileSize' => $this->maxFileSize,
+        ];
+
+        foreach ($this->locationMappings as $locationIdentifier => $locationConfiguration) {
+            $structure['locationMappings'][$locationIdentifier] = [
+                'contentTypeIdentifier' => $locationConfiguration['content_type_identifier'],
+                'mimeTypeFilter' => $locationConfiguration['mime_type_filter'],
+                'mappings' => [],
+            ];
+
+            foreach ($locationConfiguration['mappings'] as $mappingGroup) {
+                $structure['locationMappings'][$locationIdentifier]['mappings'][] = $this->buildMappingGroupStructure($mappingGroup);
+            }
+        }
+
+        foreach ($this->defaultMappings as $mappingGroup) {
+            $structure['defaultMappings'][] = $this->buildMappingGroupStructure($mappingGroup);
+        }
+
+        return $structure;
+    }
+
+    /**
+     * @param array $mappingGroup
+     *
+     * @return array
+     */
+    private function buildMappingGroupStructure(array $mappingGroup)
+    {
+        return [
+            'mimeTypes' => $mappingGroup['mime_types'],
+            'contentTypeIdentifier' => $mappingGroup['content_type_identifier'],
+            'contentFieldIdentifier' => $mappingGroup['content_field_identifier'],
+            'nameFieldIdentifier' => $mappingGroup['name_field_identifier'],
+        ];
+    }
+
+    /**
+     * @param array $fallbackContentType
+     *
+     * @return array
+     */
+    private function buildFallbackContentTypeStructure(array $fallbackContentType)
+    {
+        return [
+            'contentTypeIdentifier' => $fallbackContentType['content_type_identifier'],
+            'contentFieldIdentifier' => $fallbackContentType['content_field_identifier'],
+            'nameFieldIdentifier' => $fallbackContentType['name_field_identifier'],
+        ];
+    }
+}
