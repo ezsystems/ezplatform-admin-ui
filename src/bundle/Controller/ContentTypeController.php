f61023a53a0545357faabc17a37153b9870e4fe6
Nattfarinn <nattfarinn@gmail.com>
2017-10-24 20:15:16 +0200

    EZP-27935: Unified forms (Content Types)

--- src/bundle/Controller/ContentTypeController.php
+++ src/bundle/Controller/ContentTypeController.php
@@ -8,12 +8,12 @@ declare(strict_types=1);
 
 namespace EzSystems\EzPlatformAdminUiBundle\Controller;
 
+use eZ\Publish\API\Repository\ContentTypeService;
 use eZ\Publish\API\Repository\Values\ContentType\ContentType;
 use eZ\Publish\API\Repository\Values\ContentType\ContentTypeDraft;
 use eZ\Publish\API\Repository\Values\ContentType\ContentTypeGroup;
-use eZ\Publish\Core\Base\Exceptions\InvalidArgumentException;
+use EzSystems\EzPlatformAdminUi\Form\SubmitHandler;
 use EzSystems\EzPlatformAdminUi\Notification\NotificationHandlerInterface;
-use EzSystems\EzPlatformAdminUi\Service\ContentTypeService;
 use EzSystems\RepositoryForms\Data\Mapper\ContentTypeDraftMapper;
 use EzSystems\RepositoryForms\Form\ActionDispatcher\ActionDispatcherInterface;
 use EzSystems\RepositoryForms\Form\Type\ContentType\ContentTypeUpdateType;
@@ -36,6 +36,12 @@ class ContentTypeController extends Controller
     /** @var ActionDispatcherInterface */
     private $contentTypeActionDispatcher;
 
+    /** @var array */
+    private $languages;
+
+    /** @var SubmitHandler */
+    private $submitHandler;
+
     /**
      * ContentTypeController constructor.
      *
@@ -43,22 +49,28 @@ class ContentTypeController extends Controller
      * @param TranslatorInterface $translator
      * @param ContentTypeService $contentTypeService
      * @param ActionDispatcherInterface $contentTypeActionDispatcher
+     * @param SubmitHandler $submitHandler
+     * @param array $languages
      */
     public function __construct(
         NotificationHandlerInterface $notificationHandler,
         TranslatorInterface $translator,
         ContentTypeService $contentTypeService,
-        ActionDispatcherInterface $contentTypeActionDispatcher)
-    {
+        ActionDispatcherInterface $contentTypeActionDispatcher,
+        SubmitHandler $submitHandler,
+        array $languages
+    ) {
         $this->notificationHandler = $notificationHandler;
         $this->translator = $translator;
         $this->contentTypeService = $contentTypeService;
         $this->contentTypeActionDispatcher = $contentTypeActionDispatcher;
+        $this->submitHandler = $submitHandler;
+        $this->languages = $languages;
     }
 
     public function listAction(ContentTypeGroup $group): Response
     {
-        $types = $this->contentTypeService->getContentTypes($group);
+        $types = $this->contentTypeService->loadContentTypes($group, $this->languages);
 
         return $this->render('@EzPlatformAdminUi/admin/content_type/list.html.twig', [
             'content_type_group' => $group,
@@ -68,14 +80,19 @@ class ContentTypeController extends Controller
 
     public function addAction(ContentTypeGroup $group): Response
     {
-        $contentType = $this->contentTypeService->createContentType($group);
-        $contentTypeDraft = $this->contentTypeService->getContentTypeDraft((int)$contentType->id);
+        $mainLanguageCode = reset($this->languages);
+
+        $createStruct = $this->contentTypeService->newContentTypeCreateStruct('__new__' . md5((string)microtime(true)));
+        $createStruct->mainLanguageCode = $mainLanguageCode;
+        $createStruct->names = [ $mainLanguageCode => 'New Content Type' ];
+
+        $contentTypeDraft = $this->contentTypeService->createContentType($createStruct, [$group]);
 
         $form = $this->createUpdateForm($group, $contentTypeDraft);
 
         return $this->render('@EzPlatformAdminUi/admin/content_type/create.html.twig', [
             'content_type_group' => $group,
-            'content_type' => $contentType,
+            'content_type' => $contentTypeDraft,
             'form' => $form->createView(),
         ]);
     }
@@ -95,37 +112,49 @@ class ContentTypeController extends Controller
     {
         $form = $this->createUpdateForm($group, $contentType);
         $form->handleRequest($request);
-        if ($form->isValid()) {
-            $languageCode = $this->contentTypeService->getPrioritizedLanguage($contentType);
+        if ($form->isSubmitted()) {
+            $result = $this->submitHandler->handle($form, function() use ($form, $group, $contentType) {
+                $languageCode = reset($this->languages);
 
-            // FIXME: Move to ContentTypeService
-            $this->contentTypeActionDispatcher->dispatchFormAction(
-                $form,
-                $form->getData(),
-                $form->getClickedButton() ? $form->getClickedButton()->getName() : null,
-                ['languageCode' => $languageCode]
-            );
+                foreach ($this->languages as $prioritizedLanguage) {
+                    if (isset($contentType->names[$prioritizedLanguage])) {
+                        $languageCode = $prioritizedLanguage;
+                        break;
+                    }
+                }
 
-            if ($response = $this->contentTypeActionDispatcher->getResponse()) {
-                return $response;
+                $this->contentTypeActionDispatcher->dispatchFormAction(
+                    $form,
+                    $form->getData(),
+                    $form->getClickedButton() ? $form->getClickedButton()->getName() : null,
+                    ['languageCode' => $languageCode]
+                );
+
+                if ($response = $this->contentTypeActionDispatcher->getResponse()) {
+                    return $response;
+                }
+
+                $this->notificationHandler->success(
+                    $this->translator->trans(
+                        /** @Desc("Content type '%name%' updated.") */'content_type.update.success',
+                        ['%name%' => $contentType->getName()],
+                        'content_type'
+                    )
+                );
+
+                $routeName = 'publishContentType' === $form->getClickedButton()->getName()
+                    ? 'ezplatform.content_type.view'
+                    : 'ezplatform.content_type.edit';
+
+                return $this->redirectToRoute($routeName, [
+                    'contentTypeGroupId' => $group->id,
+                    'contentTypeId' => $contentType->id,
+                ]);
+            });
+
+            if ($result instanceof Response) {
+                return $result;
             }
-
-            $this->notificationHandler->success(
-                $this->translator->trans(
-                    /** @Desc("Content type '%name%' updated.") */'content_type.update.success',
-                    ['%name%' => $contentType->getName()],
-                    'content_type'
-                )
-            );
-
-            $routeName = 'publishContentType' === $form->getClickedButton()->getName()
-                ? 'ezplatform.content_type.view'
-                : 'ezplatform.content_type.edit';
-
-            return $this->redirectToRoute($routeName, [
-                'contentTypeGroupId' => $group->id,
-                'contentTypeId' => $contentType->id,
-            ]);
         }
 
         return $this->render('@EzPlatformAdminUi/admin/content_type/edit.html.twig', [
@@ -139,9 +168,11 @@ class ContentTypeController extends Controller
     {
         $form = $this->createDeleteForm($group, $contentType);
         $form->handleRequest($request);
-        if ($form->isValid()) {
-            try {
+
+        if ($form->isSubmitted()) {
+            $result = $this->submitHandler->handle($form, function() use ($contentType) {
                 $this->contentTypeService->deleteContentType($contentType);
+
                 $this->notificationHandler->success(
                     $this->translator->trans(
                         /** @Desc("Content type '%name%' deleted.") */'content_type.delete.success',
@@ -149,8 +180,10 @@ class ContentTypeController extends Controller
                         'content_type'
                     )
                 );
-            } catch (InvalidArgumentException $e) {
-                $this->notificationHandler->error($e->getMessage());
+            });
+
+            if ($result instanceof Response) {
+                return $result;
             }
         }
 
@@ -179,7 +212,14 @@ class ContentTypeController extends Controller
             $contentTypeDraft
         );
 
-        $languageCode = $this->contentTypeService->getPrioritizedLanguage($contentTypeDraft);
+        $languageCode = reset($this->languages);
+
+        foreach ($this->languages as $prioritizedLanguage) {
+            if (isset($contentType->names[$prioritizedLanguage])) {
+                $languageCode = $prioritizedLanguage;
+                break;
+            }
+        }
 
         return $this->createForm(ContentTypeUpdateType::class, $contentTypeData, [
             'method' => Request::METHOD_POST,
