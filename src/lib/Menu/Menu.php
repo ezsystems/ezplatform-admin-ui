f4e08563ce5ef9a570786e89364a071c98677112
Maciej Kobus <webhdx@users.noreply.github.com>
2017-10-26 16:53:12 +0200

    Revert "EZP-28127: Create extendable left sidebar"

---
+++ src/lib/Menu/Menu.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\EzPlatformAdminUi\Menu;
+
+use EzSystems\EzPlatformAdminUi\Exception\MenuItemExists;
+use EzSystems\EzPlatformAdminUi\Exception\MenuItemNotExists;
+
+class Menu implements MenuItemInterface
+{
+    /** @var string */
+    public $identifier;
+
+    /** @var Item[] */
+    public $items;
+
+    /**
+     * @param string $identifier
+     * @param array $items
+     */
+    public function __construct(string $identifier, array $items = [])
+    {
+        $this->identifier = $identifier;
+    }
+
+    public function addItem(Item $item)
+    {
+        if (isset($this->items[$item->getIdentifier()])) {
+            throw new MenuItemExists(sprintf('Menu Item %s already exists.', $item->getIdentifier()));
+        }
+
+        $this->items[$item->getIdentifier()] = $item;
+    }
+
+    public function getItem(string $identifier): Item
+    {
+        if (!isset($this->items[$identifier])) {
+            throw new MenuItemNotExists(sprintf('No Menu Item %s found', $identifier));
+        }
+
+        return $this->items[$identifier];
+    }
+
+    public function getItems()
+    {
+        return $this->items;
+    }
+}
