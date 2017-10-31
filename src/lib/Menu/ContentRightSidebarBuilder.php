ac82bb938b599277a9cbdfb9709eab3ff663c82f
Dariusz Szut <dew326@gmail.com>
2017-10-31 10:45:48 +0100

    EZP-28092: Implemented Edit content button with business logic

--- src/lib/Menu/ContentRightSidebarBuilder.php
+++ src/lib/Menu/ContentRightSidebarBuilder.php
@@ -6,12 +6,16 @@
  */
 namespace EzSystems\EzPlatformAdminUi\Menu;
 
+use eZ\Publish\API\Repository\Exceptions as ApiExceptions;
+use eZ\Publish\API\Repository\PermissionResolver;
 use eZ\Publish\API\Repository\Values\Content\Location;
 use EzSystems\EzPlatformAdminUi\Menu\Event\ConfigureMenuEvent;
 use InvalidArgumentException;
 use JMS\TranslationBundle\Model\Message;
 use JMS\TranslationBundle\Translation\TranslationContainerInterface;
+use Knp\Menu\FactoryInterface;
 use Knp\Menu\ItemInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
  * KnpMenuBundle Menu Builder service implementation for AdminUI Location View contextual sidebar menu.
@@ -21,9 +25,25 @@ use Knp\Menu\ItemInterface;
 class ContentRightSidebarBuilder extends AbstractBuilder implements TranslationContainerInterface
 {
     /* Menu items */
+    const ITEM__EDIT = 'content__sidebar_right__edit';
     const ITEM__SEND_TO_TRASH = 'content__sidebar_right__send_to_trash';
     const ITEM__COPY = 'content__sidebar_right__copy';
     const ITEM__MOVE = 'content__sidebar_right__move';
+
+    /** @var PermissionResolver */
+    private $permissionResolver;
+
+    /**
+     * @param PermissionResolver $permissionResolver
+     * @param FactoryInterface $factory
+     * @param EventDispatcherInterface $eventDispatcher
+     */
+    public function __construct(FactoryInterface $factory, EventDispatcherInterface $eventDispatcher, PermissionResolver $permissionResolver)
+    {
+        parent::__construct($factory, $eventDispatcher);
+
+        $this->permissionResolver = $permissionResolver;
+    }
 
     /**
      * @return string
@@ -38,6 +58,8 @@ class ContentRightSidebarBuilder extends AbstractBuilder implements TranslationC
      *
      * @return ItemInterface
      *
+     * @throws ApiExceptions\InvalidArgumentException
+     * @throws ApiExceptions\BadStateException
      * @throws InvalidArgumentException
      */
     public function createStructure(array $options): ItemInterface
@@ -46,8 +68,25 @@ class ContentRightSidebarBuilder extends AbstractBuilder implements TranslationC
         $location = $options['location'];
         /** @var ItemInterface|ItemInterface[] $menu */
         $menu = $this->factory->createItem('root');
+        $canEdit = $this->permissionResolver->canUser('content', 'edit', $location->getContentInfo());
+
+        $editButtonAttributes = [
+            'class' => 'ez-btn--extra-actions ez-btn--edit',
+            'data-actions' => 'edit',
+        ];
+
+        if (!$canEdit) {
+            $editButtonAttributes['disabled'] = 'disabled';
+        }
 
         $menu->setChildren([
+            self::ITEM__EDIT => $this->createMenuItem(
+                self::ITEM__EDIT,
+                [
+                    'extras' => ['icon' => 'edit'],
+                    'attributes' => $editButtonAttributes,
+                ]
+            ),
             self::ITEM__SEND_TO_TRASH => $this->createMenuItem(
                 self::ITEM__SEND_TO_TRASH,
                 [
@@ -94,6 +133,7 @@ class ContentRightSidebarBuilder extends AbstractBuilder implements TranslationC
     public static function getTranslationMessages(): array
     {
         return [
+            (new Message(self::ITEM__EDIT, 'menu'))->setDesc('Edit'),
             (new Message(self::ITEM__SEND_TO_TRASH, 'menu'))->setDesc('Send to Trash'),
             (new Message(self::ITEM__COPY, 'menu'))->setDesc('Copy'),
             (new Message(self::ITEM__MOVE, 'menu'))->setDesc('Move'),
