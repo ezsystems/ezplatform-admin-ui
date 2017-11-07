42f393b498496adbb0356bc17d6de1ffeeaf0b41
Nattfarinn <nattfarinn@gmail.com>
2017-11-07 14:42:05 +0100

    EZP-28163: 500 Internal Server Error on admin/trash/list

--- src/lib/Form/Type/Trash/TrashItemChoiceType.php
+++ src/lib/Form/Type/Trash/TrashItemChoiceType.php
@@ -6,9 +6,12 @@
  */
 namespace EzSystems\EzPlatformAdminUi\Form\Type\Trash;
 
+use eZ\Publish\API\Repository\ContentTypeService;
 use eZ\Publish\API\Repository\TrashService;
 use eZ\Publish\API\Repository\Values\Content\Query;
+use eZ\Publish\API\Repository\Values\Content\TrashItem;
 use EzSystems\EzPlatformAdminUi\Form\Data\TrashItemData;
+use EzSystems\EzPlatformAdminUi\UI\Service\PathService;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -18,12 +21,25 @@ class TrashItemChoiceType extends AbstractType
     /** @var TrashService */
     private $trashService;
 
+    /** @var PathService */
+    private $pathService;
+
+    /** @var ContentTypeService */
+    private $contentTypeService;
+
     /**
      * @param TrashService $trashService
+     * @param PathService $pathService
+     * @param ContentTypeService $contentTypeService
      */
-    public function __construct(TrashService $trashService)
-    {
+    public function __construct(
+        TrashService $trashService,
+        PathService $pathService,
+        ContentTypeService $contentTypeService
+    ) {
         $this->trashService = $trashService;
+        $this->pathService = $pathService;
+        $this->contentTypeService = $contentTypeService;
     }
 
     /**
@@ -32,9 +48,7 @@ class TrashItemChoiceType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'choices' => $this->trashService->findTrashItems(new Query([
-                'sortClauses' => [new Query\SortClause\Location\Priority(Query::SORT_ASC)],
-            ])),
+            'choices' => $this->getTrashItemDataChoices(),
             'choice_attr' => function (TrashItemData $val) {
                 return [
                     'data-is-parent-in-trash' => (int)$val->isParentInTrash(),
@@ -49,5 +63,22 @@ class TrashItemChoiceType extends AbstractType
     public function getParent(): ?string
     {
         return ChoiceType::class;
+    }
+
+    /**
+     * @return array
+     */
+    private function getTrashItemDataChoices(): array
+    {
+        $trashItems = $this->trashService->findTrashItems(new Query([
+            'sortClauses' => [new Query\SortClause\Location\Priority(Query::SORT_ASC)],
+        ]))->items;
+
+        return array_map(function (TrashItem $item) {
+            $contentType = $this->contentTypeService->loadContentType($item->contentInfo->contentTypeId);
+            $ancestors = $this->pathService->loadPathLocations($item);
+
+            return new TrashItemData($item, $contentType, $ancestors);
+        }, $trashItems);
     }
 }
