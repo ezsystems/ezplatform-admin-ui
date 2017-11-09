62612e3c997ba3a2d8ba6d7f9f0e2b4065d1dc77
Piotr Nalepa <piotr.nalepa@ez.no>
2017-11-09 15:42:04 +0100

    EZP-27901: As an Editor, I want to edit a DateAndTime field type

---
+++ src/bundle/Resources/public/js/scripts/fieldType/ezdatetime.js
@@ -0,0 +1,79 @@
+(function (global) {
+    const SELECTOR_FIELD = '.ez-field-edit--ezdatetime';
+    const SELECTOR_INPUT = '.ez-data-source__input[data-seconds]';
+    const EVENT_VALUE_CHANGED = 'valueChanged';
+
+    class EzDateTimeValidator extends global.eZ.BaseFieldValidator {
+        /**
+         * Validates the input
+         *
+         * @method validateInput
+         * @param {Event} event
+         * @returns {Object}
+         * @memberof EzDateTimeValidator
+         */
+        validateInput(event) {
+            const target = event.target;
+            const isRequired = target.required;
+            const isEmpty = !target.value.trim().length;
+            const label = event.target.closest(this.fieldSelector).querySelector('.ez-field-edit__label').innerHTML;
+            let isError = false;
+            let errorMessage = '';
+
+            if (isRequired && isEmpty) {
+                isError = true;
+                errorMessage = window.eZ.errors.emptyField.replace('{fieldName}', label);
+            }
+
+            return {
+                isError,
+                errorMessage
+            };
+        }
+    };
+
+    const validator = new EzDateTimeValidator({
+        classInvalid: 'is-invalid',
+        fieldSelector: SELECTOR_FIELD,
+        eventsMap: [
+            {
+                selector: `${SELECTOR_FIELD} ${SELECTOR_INPUT}`,
+                eventName: EVENT_VALUE_CHANGED,
+                callback: 'validateInput',
+                invalidStateSelectors: [SELECTOR_FIELD],
+                errorNodeSelectors: ['.ez-field-edit__label-wrapper'],
+            },
+        ],
+    });
+
+    validator.init();
+
+    const datetimeFields = [...document.querySelectorAll(SELECTOR_FIELD)];
+    const datetimeConfig = {
+        enableTime: true,
+        time_24hr: true,
+        formatDate: (date) => (new Date(date)).toLocaleString()
+    };
+    const updateInputValue = (sourceInput, date) => {
+        sourceInput.value = Math.floor((new Date(date)).getTime() / 1000);
+        sourceInput.dispatchEvent(new CustomEvent(EVENT_VALUE_CHANGED));
+    };
+    const initFlatPickr = (field) => {
+        const sourceInput = field.querySelector(SELECTOR_INPUT);
+        let defaultDate;
+
+        if (sourceInput.value) {
+            defaultDate = new Date(sourceInput.value * 1000);
+        }
+
+        sourceInput.classList.add('ez-data-source__input--visually-hidden');
+
+        flatpickr(field.querySelector('.flatpickr-input'), Object.assign({}, datetimeConfig, {
+            onChange: updateInputValue.bind(null, sourceInput),
+            defaultDate,
+            enableSeconds: !!parseInt(sourceInput.dataset.seconds, 10),
+        }));
+    };
+
+    datetimeFields.forEach(initFlatPickr);
+})(window);
