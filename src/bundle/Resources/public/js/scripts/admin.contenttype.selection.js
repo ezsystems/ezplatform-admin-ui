244d391b0428b95a3021bbe52e09098dd1285aa7
Dariusz Szut <dew326@gmail.com>
2017-11-10 13:43:59 +0100

    EZP-28206: As an Administrator, I want to add options to the ezselection FieldType in ContentType

---
+++ src/bundle/Resources/public/js/scripts/admin.contenttype.selection.js
@@ -0,0 +1,34 @@
+(function (global, doc) {
+    const SELECTOR_TEMPLATE = '.ezselection-settings-option-value-prototype';
+    const SELECTOR_OPTION = '.ezselection-settings-option-value';
+    const SELECTOR_OPTIONS_LIST = '.ezselection-settings-option-list';
+    const SELECTOR_BTN_REMOVE = '.ezselection-settings-option-remove';
+    const SELECTOR_BTN_ADD = '.ezselection-settings-option-add';
+    const NUMBER_PLACEHOLDER = /__number__/g;
+
+    [...doc.querySelectorAll('.ezselection-settings.options')].forEach(container => {
+        const countExistingOptions = () => container.querySelectorAll(SELECTOR_OPTION).length;
+        const findCheckedOptions = () => [...container.querySelectorAll('.ezselection-settings-option-checkbox:checked')];
+        const toggleDisableState = () => {
+            const disabledState = !!findCheckedOptions().length;
+            const methodName = disabledState ? 'removeAttribute' : 'setAttribute';
+
+            container.querySelector(SELECTOR_BTN_REMOVE)[methodName]('disabled', disabledState);
+        };
+        const addOption = () => {
+            const template = container.querySelector(SELECTOR_TEMPLATE).innerHTML;
+
+            container
+                .querySelector(SELECTOR_OPTIONS_LIST)
+                .insertAdjacentHTML('beforeend', template.replace(NUMBER_PLACEHOLDER, countExistingOptions()));
+        };
+        const removeOptions = () => {
+            findCheckedOptions().forEach(element => element.closest(SELECTOR_OPTION).remove());
+            toggleDisableState();
+        };
+
+        container.querySelector(SELECTOR_OPTIONS_LIST).addEventListener('click', toggleDisableState, false);
+        container.querySelector(SELECTOR_BTN_ADD).addEventListener('click', addOption, false);
+        container.querySelector(SELECTOR_BTN_REMOVE).addEventListener('click', removeOptions, false);
+    });
+})(window, document);
