2ee8e407b28bace26855d62dac2c68a8c074c84e
Piotr Nalepa <piotr.nalepa@ez.no>
2017-11-22 15:38:49 +0100

    EZP-27900: As an Editor, I want to edit a Country field type

---
+++ src/bundle/Resources/public/js/scripts/fieldType/ezcountry.js
@@ -0,0 +1,45 @@
+(function (global) {
+    const SELECTOR_FIELD = '.ez-field-edit--ezcountry';
+
+    class EzCountryValidator extends global.eZ.BaseFieldValidator {
+        /**
+         * Validates the select box value
+         *
+         * @method validateSelection
+         * @param {Event} event
+         * @returns {Object}
+         * @memberof EzCountryValidator
+         */
+        validateSelection(event) {
+            const isRequired = event.target.required;
+            const isEmpty = !event.target.value.trim().length;
+            const isError = (isEmpty && isRequired);
+            const label = event.target.closest(SELECTOR_FIELD).querySelector('.ez-field-edit__label').innerHTML;
+            const result = { isError };
+
+            if (isError) {
+                result.errorMessage = global.eZ.errors.emptyField.replace('{fieldName}', label);
+            }
+
+            return result;
+        }
+    }
+
+    const validator = new EzCountryValidator({
+        classInvalid: 'is-invalid',
+        fieldSelector: SELECTOR_FIELD,
+        eventsMap: [{
+            selector: `${SELECTOR_FIELD} .ez-data-source__input`,
+            eventName: 'blur',
+            callback: 'validateSelection',
+            invalidStateSelectors: [SELECTOR_FIELD],
+            errorNodeSelectors: ['.ez-field-edit__label-wrapper'],
+        }],
+    });
+
+    validator.init();
+
+    global.eZ.fieldTypeValidators = global.eZ.fieldTypeValidators ?
+        [...global.eZ.fieldTypeValidators, validator] :
+        [validator];
+})(window);
