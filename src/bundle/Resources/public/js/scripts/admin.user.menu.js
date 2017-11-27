d7a7a4c681a2cf2b1a742a959e1d23f59232a00d
Dariusz Szut <dew326@gmail.com>
2017-11-27 13:40:50 +0100

    EZP-27894: Adds user menu styling and initial structure

---
+++ src/bundle/Resources/public/js/scripts/admin.user.menu.js
@@ -0,0 +1,23 @@
+(function (global, doc) {
+    const CLASS_HIDDEN = 'ez-user-menu__items--hidden';
+    const SELECTOR_MENU_ITEMS = '.ez-user-menu__items';
+    const SELECTOR_NAME_WRAPPER = '.ez-user-menu__name-wrapper';
+    const clickOutsideHandler = (event) => {
+        if (event.target.closest(SELECTOR_NAME_WRAPPER)) {
+            return;
+        }
+
+        doc.querySelector(SELECTOR_MENU_ITEMS).classList.add(CLASS_HIDDEN);
+        doc.querySelector('body').removeEventListener('click', clickOutsideHandler, false);
+    };
+    const toggleMenuItems = () => {
+        const menuItems = doc.querySelector('.ez-user-menu__items');
+        const methodName = menuItems.classList.contains('ez-user-menu__items--hidden') ? 'addEventListener' : 'removeEventListener';
+
+        doc.querySelector(SELECTOR_MENU_ITEMS).classList.toggle(CLASS_HIDDEN);
+
+        doc.querySelector('body')[methodName]('click', clickOutsideHandler, false);
+    };
+
+    doc.querySelector(SELECTOR_NAME_WRAPPER).addEventListener('click', toggleMenuItems, false);
+})(window, document);
