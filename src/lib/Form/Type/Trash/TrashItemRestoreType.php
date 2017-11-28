6d9b46124483d905992806f487d66fd6dd60bb93
Mikolaj Adamczyk <mikadamczyk@gmail.com>
2017-11-28 14:09:17 +0100

    EZP-28323: Added pagination to Trash (#103)
    * EZP-27052: Added pagination to trash.
    
    * EZP-27052: Refactor trash restore form
    
    EZP-27052: Added pagination to trash.

--- src/lib/Form/Type/Trash/TrashItemRestoreType.php
+++ src/lib/Form/Type/Trash/TrashItemRestoreType.php
@@ -4,11 +4,14 @@
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
+
 namespace EzSystems\EzPlatformAdminUi\Form\Type\Trash;
 
 use EzSystems\EzPlatformAdminUi\Form\Data\Trash\TrashItemRestoreData;
 use EzSystems\EzPlatformAdminUi\Form\Type\UniversalDiscoveryWidget\UniversalDiscoveryWidgetType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -20,35 +23,26 @@ class TrashItemRestoreType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add(
-                'trash_items',
-                TrashItemChoiceType::class,
-                [
-                    'multiple' => true,
-                    'expanded' => true,
-                    'choice_label' => false,
-                    'label' => false,
-                ]
-            )
-            ->add(
-                'location',
-                UniversalDiscoveryWidgetType::class,
-                [
-                    'multiple' => false,
-                    'label' => /** @Desc("Restore under new parent") */
-                        'trash_item_restore_form.restore_under_new_parent',
-                    'attr' => $options['attr'],
-                ]
-            )
-            ->add(
-                'restore',
-                SubmitType::class,
-                [
-                    'label' => /** @Desc("Restore selected") */
-                        'trash_item_restore_form.restore',
-                ]
-            );
+        $builder->add('trash_items', CollectionType::class, [
+            'entry_type' => TrashItemCheckboxType::class,
+            'entry_options' => [
+                'required' => false,
+            ],
+            'label' => false,
+            'allow_add' => true,
+        ]);
+
+        $builder->add('location', UniversalDiscoveryWidgetType::class, [
+            'multiple' => false,
+            'label' => /** @Desc("Restore under new parent") */
+                'trash_item_restore_form.restore_under_new_parent',
+            'attr' => $options['attr'],
+        ]);
+
+        $builder->add('restore', SubmitType::class, [
+            'label' => /** @Desc("Restore selected") */
+                'trash_item_restore_form.restore',
+        ]);
     }
 
     /**
---
+++ src/lib/Pagination/Pagerfanta/TrashItemAdapter.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\EzPlatformAdminUi\Pagination\Pagerfanta;
+
+use eZ\Publish\API\Repository\TrashService;
+use eZ\Publish\API\Repository\Values\Content\Query;
+use Pagerfanta\Adapter\AdapterInterface;
+
+/**
+ * Pagerfanta adapter for eZ Publish content search.
+ * Will return results as SearchHit objects.
+ */
+class TrashItemAdapter implements AdapterInterface
+{
+    /**
+     * @var \eZ\Publish\API\Repository\Values\Content\Query
+     */
+    private $query;
+
+    /**
+     * @var \eZ\Publish\API\Repository\TrashService
+     */
+    private $trashService;
+
+    /**
+     * @var int
+     */
+    private $nbResults;
+
+    public function __construct(Query $query, TrashService $trashService)
+    {
+        $this->query = $query;
+        $this->trashService = $trashService;
+    }
+
+    /**
+     * Returns the number of results.
+     *
+     * @return int the number of results
+     */
+    public function getNbResults()
+    {
+        if (isset($this->nbResults)) {
+            return $this->nbResults;
+        }
+
+        $countQuery = clone $this->query;
+        $countQuery->limit = 0;
+
+        return $this->nbResults = $this->trashService->findTrashItems($countQuery)->count;
+    }
+
+    /**
+     * Returns a slice of the results.
+     *
+     * @param int $offset the offset
+     * @param int $length the length
+     *
+     * @return \eZ\Publish\API\Repository\Values\ValueObject[]
+     */
+    public function getSlice($offset, $length): array
+    {
+        $query = clone $this->query;
+        $query->offset = $offset;
+        $query->limit = $length;
+        $query->performCount = false;
+
+        $trashItems = $this->trashService->findTrashItems($query);
+
+        if (null === $this->nbResults && null !== $trashItems->count) {
+            $this->nbResults = $trashItems->count;
+        }
+
+        return $trashItems->items;
+    }
+}
