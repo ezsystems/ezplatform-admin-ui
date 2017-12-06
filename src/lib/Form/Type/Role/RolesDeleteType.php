a310f579f10c9f0d594b8eb2c462c9664895da31
Mikołaj Adamczyk <mikadamczyk@gmail.com>
2017-12-06 16:21:30 +0100

    EZP-28414: Design improvements for Role list in Admin Panel

---
+++ src/lib/Form/Type/Role/RolesDeleteType.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUi\Form\Type\Role;
+
+use EzSystems\EzPlatformAdminUi\Form\Data\Role\RolesDeleteData;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RolesDeleteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'roles',
+                CollectionType::class,
+                [
+                    'label' => false,
+                    'allow_add' => true,
+                    'entry_type' => CheckboxType::class,
+                    'entry_options' => ['label' => false, 'required' => false],
+                ]
+            )
+            ->add(
+                'delete',
+                SubmitType::class,
+                [
+                    'attr' => ['hidden' => true],
+                    'label' => /** @Desc("Delete role") */ 'roles_delete_form.delete',
+                ]
+            );
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => RolesDeleteData::class,
+            'translation_domain' => 'forms',
+        ]);
+    }
+}
