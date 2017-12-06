6575d938aa25b9cdecdc58bc8aaf4087fcf21dcf
Mikołaj Adamczyk <mikadamczyk@gmail.com>
2017-12-06 17:20:42 +0100

    EZP-28418: Design improvements for Role create and edit actions

---
+++ src/lib/Menu/Admin/Role/RoleEditRightSidebarBuilder.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\EzPlatformAdminUi\Menu\Admin\Role;
+
+use eZ\Publish\API\Repository\Exceptions as ApiExceptions;
+use eZ\Publish\API\Repository\Values\User\Role;
+use EzSystems\EzPlatformAdminUi\Menu\AbstractBuilder;
+use EzSystems\EzPlatformAdminUi\Menu\Event\ConfigureMenuEvent;
+use InvalidArgumentException;
+use JMS\TranslationBundle\Model\Message;
+use JMS\TranslationBundle\Translation\TranslationContainerInterface;
+use Knp\Menu\ItemInterface;
+
+/**
+ * KnpMenuBundle Menu Builder service implementation for AdminUI Section Edit contextual sidebar menu.
+ *
+ * @see https://symfony.com/doc/current/bundles/KnpMenuBundle/menu_builder_service.html
+ */
+class RoleEditRightSidebarBuilder extends AbstractBuilder implements TranslationContainerInterface
+{
+    /* Menu items */
+    const ITEM__SAVE = 'role_edit__sidebar_right__save';
+    const ITEM__CANCEL = 'role_edit__sidebar_right__cancel';
+
+    /**
+     * @return string
+     */
+    protected function getConfigureEventName(): string
+    {
+        return ConfigureMenuEvent::ROLE_EDIT_SIDEBAR_RIGHT;
+    }
+
+    /**
+     * @param array $options
+     *
+     * @return ItemInterface
+     *
+     * @throws ApiExceptions\InvalidArgumentException
+     * @throws ApiExceptions\BadStateException
+     * @throws InvalidArgumentException
+     */
+    public function createStructure(array $options): ItemInterface
+    {
+        /** @var Role $role */
+        $role = $options['role'];
+
+        /** @var ItemInterface|ItemInterface[] $menu */
+        $menu = $this->factory->createItem('root');
+
+        $menu->setChildren([
+            self::ITEM__SAVE => $this->createMenuItem(
+                self::ITEM__SAVE,
+                [
+                    'attributes' => [
+                        'class' => 'btn--trigger',
+                        'data-click' => sprintf('#update-role-%d_save', $role->id),
+                    ],
+                    'extras' => ['icon' => 'save'],
+                ]
+            ),
+            self::ITEM__CANCEL => $this->createMenuItem(
+                self::ITEM__CANCEL,
+                [
+                    'route' => 'ezplatform.role.list',
+                    'extras' => ['icon' => 'circle-close'],
+                ]
+            ),
+        ]);
+
+        return $menu;
+    }
+
+    /**
+     * @return Message[]
+     */
+    public static function getTranslationMessages(): array
+    {
+        return [
+            (new Message(self::ITEM__SAVE, 'menu'))->setDesc('Save'),
+            (new Message(self::ITEM__CANCEL, 'menu'))->setDesc('Discard changes'),
+        ];
+    }
+}
