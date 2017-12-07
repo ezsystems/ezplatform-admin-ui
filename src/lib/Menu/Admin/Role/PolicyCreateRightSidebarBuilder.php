fc70e869666d232204a1d24c089c5021149a485c
Mikołaj Adamczyk <mikadamczyk@gmail.com>
2017-12-07 09:18:23 +0100

    EZP-28420: Design improvements for Policy create and edit actions in Admin Panel

---
+++ src/lib/Menu/Admin/Role/PolicyCreateRightSidebarBuilder.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\EzPlatformAdminUi\Menu\Admin\Role;
+
+use eZ\Publish\API\Repository\Exceptions as ApiExceptions;
+use eZ\Publish\API\Repository\Values\User\Role;
+use EzSystems\EzPlatformAdminUi\Menu\AbstractBuilder;
+use EzSystems\EzPlatformAdminUi\Menu\Event\ConfigureMenuEvent;
+use InvalidArgumentException;
+use JMS\TranslationBundle\Model\Message;
+use JMS\TranslationBundle\Translation\TranslationContainerInterface;
+use Knp\Menu\ItemInterface;
+
+/**
+ * KnpMenuBundle Menu Builder service implementation for AdminUI Section Edit contextual sidebar menu.
+ *
+ * @see https://symfony.com/doc/current/bundles/KnpMenuBundle/menu_builder_service.html
+ */
+class PolicyCreateRightSidebarBuilder extends AbstractBuilder implements TranslationContainerInterface
+{
+    /* Menu items */
+    const ITEM__CREATE = 'policy_create__sidebar_right__create';
+    const ITEM__CANCEL = 'policy_create__sidebar_right__cancel';
+
+    /**
+     * @return string
+     */
+    protected function getConfigureEventName(): string
+    {
+        return ConfigureMenuEvent::POLICY_CREATE_SIDEBAR_RIGHT;
+    }
+
+    /**
+     * @param array $options
+     *
+     * @return ItemInterface
+     *
+     * @throws ApiExceptions\InvalidArgumentException
+     * @throws ApiExceptions\BadStateException
+     * @throws InvalidArgumentException
+     */
+    public function createStructure(array $options): ItemInterface
+    {
+        /** @var Role $section */
+        $role = $options['role'];
+
+        /** @var ItemInterface|ItemInterface[] $menu */
+        $menu = $this->factory->createItem('root');
+
+        $menu->setChildren([
+            self::ITEM__CREATE => $this->createMenuItem(
+                self::ITEM__CREATE,
+                [
+                    'attributes' => [
+                        'class' => 'btn--trigger',
+                        'data-click' => '#policy_create_save',
+                    ],
+                    'extras' => ['icon' => 'save'],
+                ]
+            ),
+            self::ITEM__CANCEL => $this->createMenuItem(
+                self::ITEM__CANCEL,
+                [
+                    'route' => 'ezplatform.role.view',
+                    'routeParameters' => [
+                        'roleId' => $role->id,
+                    ],
+                    'extras' => ['icon' => 'circle-close'],
+                ]
+            ),
+        ]);
+
+        return $menu;
+    }
+
+    /**
+     * @return Message[]
+     */
+    public static function getTranslationMessages(): array
+    {
+        return [
+            (new Message(self::ITEM__CREATE, 'menu'))->setDesc('Create'),
+            (new Message(self::ITEM__CANCEL, 'menu'))->setDesc('Discard changes'),
+        ];
+    }
+}
