91ae2b0c75e79201d3392496661532086546a3ff
Maciej Kobus <webhdx@users.noreply.github.com>
2017-12-07 13:47:10 +0100

    EZP-28339: Add custom theming following guidelines for user create/edit (#112)
    * EZP-28187: Implement ezuser field type design
    
    * EZP-28339: Add custom theming following guidelines for user create/edit
    
    * EZP-28339: Adds processed CSS

---
+++ src/lib/Menu/UserEditRightSidebarBuilder.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\EzPlatformAdminUi\Menu;
+
+use eZ\Publish\API\Repository\Exceptions as ApiExceptions;
+use EzSystems\EzPlatformAdminUi\Menu\Event\ConfigureMenuEvent;
+use InvalidArgumentException;
+use JMS\TranslationBundle\Model\Message;
+use JMS\TranslationBundle\Translation\TranslationContainerInterface;
+use Knp\Menu\ItemInterface;
+
+/**
+ * KnpMenuBundle Menu Builder service implementation for AdminUI Content Edit contextual sidebar menu.
+ *
+ * @see https://symfony.com/doc/current/bundles/KnpMenuBundle/menu_builder_service.html
+ */
+class UserEditRightSidebarBuilder extends AbstractBuilder implements TranslationContainerInterface
+{
+    /* Menu items */
+    const ITEM__UPDATE = 'user_edit__sidebar_right__update';
+    const ITEM__CANCEL = 'user_edit__sidebar_right__cancel';
+
+    /**
+     * @return string
+     */
+    protected function getConfigureEventName(): string
+    {
+        return ConfigureMenuEvent::USER_EDIT_SIDEBAR_RIGHT;
+    }
+
+    /**
+     * @param array $options
+     *
+     * @return ItemInterface
+     *
+     * @throws ApiExceptions\InvalidArgumentException
+     * @throws ApiExceptions\BadStateException
+     * @throws InvalidArgumentException
+     */
+    public function createStructure(array $options): ItemInterface
+    {
+        /** @var ItemInterface|ItemInterface[] $menu */
+        $menu = $this->factory->createItem('root');
+
+        $menu->setChildren([
+            self::ITEM__UPDATE => $this->createMenuItem(
+                self::ITEM__UPDATE,
+                [
+                    'attributes' => [
+                        'class' => 'btn--trigger',
+                        'data-click' => '#ezrepoforms_user_update_update',
+                    ],
+                    'extras' => ['icon' => 'publish'],
+                ]
+            ),
+            self::ITEM__CANCEL => $this->createMenuItem(
+                self::ITEM__CANCEL,
+                [
+                    'attributes' => [
+                        'class' => 'btn--trigger',
+                        'data-click' => '#ezrepoforms_user_update_cancel',
+                    ],
+                    'extras' => ['icon' => 'circle-close'],
+                ]
+            ),
+        ]);
+
+        return $menu;
+    }
+
+    /**
+     * @return Message[]
+     */
+    public static function getTranslationMessages(): array
+    {
+        return [
+            (new Message(self::ITEM__UPDATE, 'menu'))->setDesc('Update'),
+            (new Message(self::ITEM__CANCEL, 'menu'))->setDesc('Cancel'),
+        ];
+    }
+}
