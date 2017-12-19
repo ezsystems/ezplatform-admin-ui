cf38b4806f212c4f627b902c6ba920481a7bd19b
Nattfarinn <nattfarinn@gmail.com>
2017-12-19 13:24:31 +0100

    EZP-28536: It's not possible to specify Locations/Subtree of locations for Limitations of Policies

---
+++ src/bundle/Resources/public/js/scripts/admin.limitation.pick.js
@@ -0,0 +1,28 @@
+(function (global, doc) {
+    const udwContainer = doc.getElementById('react-udw');
+    const token = doc.querySelector('meta[name="CSRF-Token"]').content;
+    const siteaccess = doc.querySelector('meta[name="SiteAccess"]').content;
+    const closeUDW = () => udwContainer.innerHTML = '';
+    const selectLocationsConfirm = (target, data) => {
+        const selectedItems = data.reduce((total, item) => total + `<li>${item.ContentInfo.Content.Name}</li>`, '');
+
+        doc.querySelector(target.dataset.locationInputSelector).value = data.map(item => item.id).join();
+        doc.querySelector(target.dataset.selectedLocationListSelector).innerHTML = selectedItems;
+
+        closeUDW();
+    };
+    const openUDW = (event) => {
+        event.preventDefault();
+
+        ReactDOM.render(React.createElement(global.eZ.modules.UniversalDiscovery, {
+            onConfirm: selectLocationsConfirm.bind(this, event.target),
+            onCancel: closeUDW,
+            confirmLabel: 'Add locations',
+            title: 'Choose locations',
+            multiple: true,
+            restInfo: {token, siteaccess}
+        }), udwContainer);
+    };
+
+    [...doc.querySelectorAll('.ez-pick-location-limitation-button')].forEach(btn => btn.addEventListener('click', openUDW, false));
+})(window, document);
