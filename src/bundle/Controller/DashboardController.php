e5eb461ba323bb7e82759b90ceb2f878dc1a3763
Nattfarinn <nattfarinn@gmail.com>
2017-12-20 21:36:32 +0100

    fix: Dashboard

---
+++ src/bundle/Controller/DashboardController.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUiBundle\Controller;
+
+use EzSystems\EzPlatformAdminUi\Form\Data\Content\Draft\ContentEditData;
+use EzSystems\EzPlatformAdminUi\Form\Factory\FormFactory;
+
+class DashboardController extends Controller
+{
+    protected $formFactory;
+
+    public function __construct(FormFactory $formFactory)
+    {
+        $this->formFactory = $formFactory;
+    }
+
+    public function dashboardAction()
+    {
+        $editForm = $this->formFactory->contentEdit(
+            new ContentEditData()
+        );
+
+        return $this->render('@EzPlatformAdminUi/dashboard/dashboard.html.twig', [
+            'form_edit' => $editForm->createView(),
+        ]);
+    }
+}
