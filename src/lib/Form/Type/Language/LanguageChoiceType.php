bc3432a20c26bae3d6d65c101222912dc983316b
André R <ar@ez.no>
2017-12-31 09:34:19 +0100

    As a Editor I want language chices to be ordered by SiteAccess settings
    E.g. If I configure admin to be [nor-NB, eng-GB], then I expect norwegian to be the default
    choice so I don't need to clock to change it.
    
    Part of overal epic on making Admin UI multi language capable.
    
    Review note:
    - I'm unsure if this is right layer to do this, but until we move forward on SiteAccess aware
      Repo PR, this is currently only place we can do it.

--- src/lib/Form/Type/Language/LanguageChoiceType.php
+++ src/lib/Form/Type/Language/LanguageChoiceType.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 
 namespace EzSystems\EzPlatformAdminUi\Form\Type\Language;
 
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\ConfigResolver;
 use eZ\Publish\API\Repository\LanguageService;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\ChoiceList\Loader\CallbackChoiceLoader;
@@ -24,12 +25,16 @@ class LanguageChoiceType extends AbstractType
     /** @var LanguageService */
     protected $languageService;
 
+    /** @var ConfigResolver */
+    protected $configResolver;
+
     /**
      * @param LanguageService $languageService
      */
-    public function __construct(LanguageService $languageService)
+    public function __construct(LanguageService $languageService, ConfigResolver $configResolver)
     {
         $this->languageService = $languageService;
+        $this->configResolver = $configResolver;
     }
 
     public function getParent()
@@ -39,9 +44,29 @@ class LanguageChoiceType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver)
     {
+        $languageService = $this->languageService;
+        $siteAccessLanguages = $this->configResolver->getParameter('languages');
         $resolver
             ->setDefaults([
-                'choice_loader' => new CallbackChoiceLoader([$this->languageService, 'loadLanguages']),
+                'choice_loader' => new CallbackChoiceLoader(function () use ($languageService, $siteAccessLanguages) {
+                    // Order languages by siteaccess languages first, then the rest
+                    $siteAccessLanguages[] = null;
+                    $languages = [];
+                    $repoLanguages = $languageService->loadLanguages();
+                    foreach ($siteAccessLanguages as $siteAccessLanguage) {
+                        foreach ($repoLanguages as $key => $repoLanguage) {
+                            if ($siteAccessLanguage === null) {
+                                $languages[] = $repoLanguage;
+                            } elseif ($repoLanguage->languageCode === $siteAccessLanguage) {
+                                $languages[] = $repoLanguage;
+                                unset($repoLanguages[$key]);
+                                break;
+                            }
+                        }
+                    }
+
+                    return $languages;
+                }),
                 'choice_label' => 'name',
                 'choice_name' => 'languageCode',
                 'choice_value' => 'languageCode',
