559f824fbebf3fd033cc7190e06a6575d7949a79
Mikołaj Adamczyk <mikadamczyk@gmail.com>
2018-01-11 10:41:34 +0100

    EZP-28722: Add unit tests to Compiler Passes

---
+++ src/bundle/Tests/DependencyInjection/Compiler/SystemInfoTabGroupPassTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUiBundle\DependencyInjection\Compiler;
+
+use EzSystems\EzPlatformAdminUi\Tab\TabGroup;
+use EzSystems\EzPlatformAdminUi\Tab\TabRegistry;
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractCompilerPassTestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+
+class SystemInfoTabGroupPassTest extends AbstractCompilerPassTestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->setDefinition(TabRegistry::class, new Definition());
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     */
+    protected function registerCompilerPass(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new SystemInfoTabGroupPass());
+    }
+
+    public function testProcess()
+    {
+        $tabGroupDefinition = new Definition(TabGroup::class, ['systeminfo']);
+
+        $this->compile();
+
+        $this->assertContainerBuilderHasServiceDefinitionWithMethodCall(
+            TabRegistry::class,
+            'addTabGroup',
+            [$tabGroupDefinition]
+        );
+    }
+}
