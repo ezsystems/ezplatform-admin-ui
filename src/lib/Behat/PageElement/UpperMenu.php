c51afbfb666f89f9313d41aa90199f72466e0dec
m-tyrala <tyrala.maciej@gmail.com>
2018-02-08 09:46:07 +0100

    EZP-28172 Behat Covering of Content Types - return to previous fields storage

--- src/lib/Behat/PageElement/UpperMenu.php
+++ src/lib/Behat/PageElement/UpperMenu.php
@@ -9,11 +9,6 @@ namespace EzSystems\EzPlatformAdminUi\Behat\PageElement;
 /** Element that describes upper menu (Content, Admin, Page and theirs children) */
 class UpperMenu extends Element
 {
-    protected $fields = [
-        'menuButton' => '.nav-link',
-        'submenuButton' => '.navbar-expand-lg .nav-link',
-    ];
-
     /** @var string Name by which Element is recognised */
     public const ELEMENT_NAME = 'Upper Menu';
 
@@ -24,7 +19,7 @@ class UpperMenu extends Element
      */
     public function goToTab(string $tabName): void
     {
-        $this->context->getElementByText($tabName, $this->fields['menuButton'])->click();
+        $this->context->getElementByText($tabName, '.nav-link')->click();
     }
 
     /**
@@ -34,11 +29,11 @@ class UpperMenu extends Element
      */
     public function goToSubTab(string $tabName): void
     {
-        $this->context->getElementByText($tabName, $this->fields['submenuButton'])->click();
+        $this->context->getElementByText($tabName, '.navbar-expand-lg .nav-link')->click();
     }
 
     public function verifyVisibility(): void
     {
-        $this->context->waitUntilElementIsVisible($this->fields['menuButton']);
+        $this->context->waitUntilElementIsVisible('.nav-link');
     }
 }
