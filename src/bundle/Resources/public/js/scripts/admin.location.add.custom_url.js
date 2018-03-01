225fbb4b3f415e19439f0e3145d074bd28f7221e
Dawid Parafiński <dawid.parafinski@ez.no>
2018-03-01 10:10:51 +0100

    [EZP-27840] As an Editor, I want to create a custom URL alias for a content item

---
+++ src/bundle/Resources/public/js/scripts/admin.location.add.custom_url.js
@@ -0,0 +1,27 @@
+(function (global, doc) {
+    const modal = doc.querySelector('#ez-modal--custom-url-alias');
+    const discardBtns = [...modal.querySelectorAll('[data-dismiss="modal"]')];
+    const submitBtn = modal.querySelector('[type="submit"]');
+    const input = modal.querySelector('[required="required"]');
+    const checkboxes = [...modal.querySelectorAll('.ez-field-edit--ezboolean input')];
+    const toggleButtonState = () => {
+        const hasValue = input.value.trim().length !== 0;
+        const methodName = hasValue ? 'removeAttribute' : 'setAttribute';
+
+        submitBtn[methodName]('disabled', true);
+    };
+    const toggleCheckbox = (event) => {
+        const checkbox = event.target;
+        const methodName = checkbox.checked ? 'add' : 'remove';
+
+        checkbox.closest('.ez-data-source__label').classList[methodName]('is-checked');
+    };
+    const clearValues = () => {
+        input.value = '';
+        toggleButtonState();
+    };
+
+    input.addEventListener('input', toggleButtonState, false);
+    checkboxes.forEach(checkbox => checkbox.addEventListener('change', toggleCheckbox, false));
+    discardBtns.forEach(btn => btn.addEventListener('click', clearValues, false));
+})(window, document);
---
+++ src/lib/Form/Data/Content/CustomUrl/CustomUrlAddData.php
@@ -0,0 +1,144 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUi\Form\Data\Content\CustomUrl;
+
+use eZ\Publish\API\Repository\Values\Content\Language;
+use eZ\Publish\API\Repository\Values\Content\Location;
+
+class CustomUrlAddData
+{
+    /** @var Location|null */
+    private $location;
+
+    /** @var string|null */
+    private $path;
+
+    /** @var Language|null */
+    private $language;
+
+    /** @var bool */
+    private $redirect;
+
+    /** @var bool */
+    private $siteRoot;
+
+    public function __construct(
+        ?Location $location = null,
+        ?string $path = null,
+        ?Language $language = null,
+        bool $redirect = true,
+        bool $siteRoot = true
+    ) {
+        $this->location = $location;
+        $this->path = $path;
+        $this->language = $language;
+        $this->redirect = $redirect;
+        $this->siteRoot = $siteRoot;
+    }
+
+    /**
+     * @return Location|null
+     */
+    public function getLocation(): ?Location
+    {
+        return $this->location;
+    }
+
+    /**
+     * @param Location|null $location
+     *
+     * @return CustomUrlAddData
+     */
+    public function setLocation(?Location $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    /**
+     * @return Language|null
+     */
+    public function getLanguage(): ?Language
+    {
+        return $this->language;
+    }
+
+    /**
+     * @param Language|null $language
+     *
+     * @return CustomUrlAddData
+     */
+    public function setLanguage(?Language $language): self
+    {
+        $this->language = $language;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param string|null $path
+     *
+     * @return CustomUrlAddData
+     */
+    public function setPath(?string $path): self
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isRedirect(): bool
+    {
+        return $this->redirect;
+    }
+
+    /**
+     * @param bool $redirect
+     *
+     * @return CustomUrlAddData
+     */
+    public function setRedirect(bool $redirect): self
+    {
+        $this->redirect = $redirect;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSiteRoot(): bool
+    {
+        return $this->siteRoot;
+    }
+
+    /**
+     * @param bool $siteRoot
+     *
+     * @return CustomUrlAddData
+     */
+    public function setSiteRoot(bool $siteRoot): self
+    {
+        $this->siteRoot = $siteRoot;
+
+        return $this;
+    }
+}
