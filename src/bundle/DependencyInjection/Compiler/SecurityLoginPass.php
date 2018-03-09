fef6644941b7899e8f31e77fc595aa2282679594
Dawid Parafiński <dawid.parafinski@ez.no>
2018-03-09 11:36:49 +0100

    EZP-26238: CompilerPass for redirecting after login

---
+++ src/bundle/DependencyInjection/Compiler/SecurityLoginPass.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUiBundle\DependencyInjection\Compiler;
+
+use EzSystems\EzPlatformAdminUi\Security\Authentication\RedirectToDashboardAuthenticationSuccessHandler;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class SecurityLoginPass implements CompilerPassInterface
+{
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $successHandlerDef = $container->getDefinition('security.authentication.success_handler');
+        $successHandlerDef->setClass(RedirectToDashboardAuthenticationSuccessHandler::class);
+        $successHandlerDef->addArgument($container->getParameter('ezpublish.siteaccess.groups'));
+        $successHandlerDef->addArgument('ezplatform.dashboard');
+    }
+}
---
+++ src/lib/Specification/SiteAccess/IsAdmin.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUi\Specification\SiteAccess;
+
+use eZ\Publish\Core\MVC\Symfony\SiteAccess;
+use EzSystems\EzPlatformAdminUi\Exception\InvalidArgumentException;
+use EzSystems\EzPlatformAdminUi\Specification\AbstractSpecification;
+use EzSystems\EzPlatformAdminUiBundle\EzPlatformAdminUiBundle;
+
+class IsAdmin extends AbstractSpecification
+{
+    /** @var array */
+    private $siteAccessGroups;
+
+    /**
+     * @param array $siteAccessGroups
+     */
+    public function __construct(array $siteAccessGroups)
+    {
+        $this->siteAccessGroups = $siteAccessGroups;
+    }
+
+    /**
+     * @param $item
+     *
+     * @return bool
+     *
+     * @throws \EzSystems\EzPlatformAdminUi\Exception\InvalidArgumentException
+     */
+    public function isSatisfiedBy($item): bool
+    {
+        if (!$item instanceof SiteAccess) {
+            throw new InvalidArgumentException($item, sprintf('Must be instance of %s', SiteAccess::class));
+        }
+
+        return in_array($item->name, $this->siteAccessGroups[EzPlatformAdminUiBundle::ADMIN_GROUP_NAME], true);
+    }
+}
