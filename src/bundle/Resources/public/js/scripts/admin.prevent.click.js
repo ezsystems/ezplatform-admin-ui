c2a49b0781ee605038b9a5b1e3e1cc76e540b91c
Dariusz Szut <dew326@gmail.com>
2018-03-19 14:07:38 +0100

    EZP-28682: Error 404 after fast double click

---
+++ src/bundle/Resources/public/js/scripts/admin.prevent.click.js
@@ -0,0 +1,7 @@
+(function (global, doc) {
+    global.onbeforeunload = function() {
+        doc.querySelector('body').classList.add('ez-prevent-click');
+
+        return null;
+    };
+})(window, document);
