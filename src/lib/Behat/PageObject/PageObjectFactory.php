c130f0ef71c6ec450dbbec6216cba8d090b0a0f7
Maciej Tyrała <tyrala.maciej@gmail.com>
2018-04-03 22:25:58 +0200

    EZP-28168 - Behat Covering of SystemInfo (#432)
    * EZP-28168 - Behat Covering of SystemInfo
    
    * EZP-28168 - Behat Covering of System Information - fixes

--- src/lib/Behat/PageObject/PageObjectFactory.php
+++ src/lib/Behat/PageObject/PageObjectFactory.php
@@ -21,7 +21,7 @@ class PageObjectFactory
      * @param string $pageName Name of the Page to creator
      * @param null[]|string[] $parameters additional parameters
      *
-     * @return LoginPage|DashboardPage|ContentStructurePage|ContentTypeGroupsPage|SectionPage|SectionsPage|UpdateItemPage|ContentTypePage|FormManagerPage|FormDetailsPage|FormDeletionConfirmationPage|LanguagesPage|LanguagePage|RolesPage|RolePage
+     * @return LoginPage|SystemInfoPage|DashboardPage|ContentStructurePage|ContentTypeGroupsPage|SectionPage|SectionsPage|UpdateItemPage|ContentTypePage|FormManagerPage|FormDetailsPage|FormDeletionConfirmationPage|LanguagesPage|LanguagePage|RolesPage|RolePage
      */
     public static function createPage(UtilityContext $context, string $pageName, ?string ...$parameters): Page
     {
@@ -48,6 +48,8 @@ class PageObjectFactory
                 return new RolesPage($context);
             case RolePage::PAGE_NAME:
                 return new RolePage($context, $parameters[0]);
+            case SystemInfoPage::PAGE_NAME:
+                return new SystemInfoPage($context);
             case SectionsPage::PAGE_NAME:
                 return new SectionsPage($context);
             case SectionPage::PAGE_NAME:
