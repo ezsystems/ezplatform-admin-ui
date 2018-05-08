a492eb6544fa5fe1c0bcfb161218966c0baede6d
Dawid Parafiński <vinniczek@gmail.com>
2018-05-08 10:00:28 +0200

    EZP-29131: Improvements to disabling/hiding elements (#457)
    * Object state
    
    * section add
    
    * Section view
    
    * Language view

--- src/lib/Form/Type/ObjectState/ContentObjectStateUpdateType.php
+++ src/lib/Form/Type/ObjectState/ContentObjectStateUpdateType.php
@@ -9,10 +9,13 @@ declare(strict_types=1);
 namespace EzSystems\EzPlatformAdminUi\Form\Type\ObjectState;
 
 use eZ\Publish\API\Repository\ObjectStateService;
+use eZ\Publish\API\Repository\PermissionResolver;
+use eZ\Publish\API\Repository\Values\ObjectState\ObjectState;
 use EzSystems\EzPlatformAdminUi\Form\Data\ObjectState\ContentObjectStateUpdateData;
 use EzSystems\EzPlatformAdminUi\Form\Type\Content\ContentInfoType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\ChoiceList\Loader\CallbackChoiceLoader;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
@@ -20,15 +23,20 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ContentObjectStateUpdateType extends AbstractType
 {
-    /** @var ObjectStateService */
+    /** @var \eZ\Publish\API\Repository\ObjectStateService */
     protected $objectStateService;
 
+    /** @var \eZ\Publish\API\Repository\PermissionResolver */
+    private $permissionResolver;
+
     /**
-     * @param ObjectStateService $objectStateService
+     * @param \eZ\Publish\API\Repository\ObjectStateService $objectStateService
+     * @param \eZ\Publish\API\Repository\PermissionResolver $permissionResolver
      */
-    public function __construct(ObjectStateService $objectStateService)
+    public function __construct(ObjectStateService $objectStateService, PermissionResolver $permissionResolver)
     {
         $this->objectStateService = $objectStateService;
+        $this->permissionResolver = $permissionResolver;
     }
 
     /**
@@ -42,18 +50,27 @@ class ContentObjectStateUpdateType extends AbstractType
             ])
             ->add('objectStateGroup', ObjectStateGroupType::class, [
                 'label' => false,
+            ])
+            ->add('set', SubmitType::class, [
+                'label' => /** @Desc("Set") */ 'object_state.button.set',
             ]);
 
         $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
             /** @var ContentObjectStateUpdateData $contentObjectStateUpdateData */
             $contentObjectStateUpdateData = $event->getData();
             $objectStateGroup = $contentObjectStateUpdateData->getObjectStateGroup();
+            $contentInfo = $contentObjectStateUpdateData->getContentInfo();
             $form = $event->getForm();
 
             $form->add('objectState', ObjectStateChoiceType::class, [
                 'label' => false,
-                'choice_loader' => new CallbackChoiceLoader(function () use ($objectStateGroup) {
-                    return $this->objectStateService->loadObjectStates($objectStateGroup);
+                'choice_loader' => new CallbackChoiceLoader(function () use ($objectStateGroup, $contentInfo) {
+                    return array_filter(
+                        $this->objectStateService->loadObjectStates($objectStateGroup),
+                        function (ObjectState $objectState) use ($contentInfo) {
+                            return $this->permissionResolver->canUser('state', 'assign', $contentInfo, [$objectState]);
+                        }
+                    );
                 }),
             ]);
         });
