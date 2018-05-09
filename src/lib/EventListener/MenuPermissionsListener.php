7861fe9b2300afeb1de85e04b943a12d6e969e9e
Mikołaj Adamczyk <mikadamczyk@gmail.com>
2018-05-09 14:17:20 +0200

    EZP-29133: Implement permission to access Admin Panel

---
+++ src/lib/EventListener/MenuPermissionsListener.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUi\EventListener;
+
+use EzSystems\EzPlatformAdminUi\Menu\Event\ConfigureMenuEvent;
+use EzSystems\EzPlatformAdminUi\Menu\MainMenuBuilder;
+use eZ\Publish\API\Repository\PermissionResolver;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class MenuPermissionsListener implements EventSubscriberInterface
+{
+    /** @var \eZ\Publish\API\Repository\PermissionResolver */
+    private $permissionResolver;
+
+    /**
+     * @param \eZ\Publish\API\Repository\PermissionResolver $permissionResolver
+     */
+    public function __construct(PermissionResolver $permissionResolver)
+    {
+        $this->permissionResolver = $permissionResolver;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [ConfigureMenuEvent::MAIN_MENU => 'checkPermissions'];
+    }
+
+    /**
+     * @param \EzSystems\EzPlatformAdminUi\Menu\Event\ConfigureMenuEvent $event
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     */
+    public function checkPermissions(ConfigureMenuEvent $event): void
+    {
+        if (!$this->permissionResolver->hasAccess('setup', 'administrate')) {
+            $menu = $event->getMenu();
+            $menu->removeChild(MainMenuBuilder::ITEM_ADMIN);
+        }
+    }
+}
---
+++ src/lib/Tests/Menu/MainMenuBuilerTest.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUi\Tests\Menu;
+
+use EzSystems\EzPlatformAdminUi\Menu\MainMenuBuilder;
+use Knp\Menu\MenuItem;
+use PHPUnit\Framework\TestCase;
+use EzSystems\EzPlatformAdminUi\Menu\MenuItemFactory;
+use eZ\Publish\API\Repository\PermissionResolver;
+use eZ\Publish\Core\MVC\ConfigResolverInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+class MainMenuBuilerTest extends TestCase
+{
+    /** @var \EzSystems\EzPlatformAdminUi\Menu\MenuItemFactory */
+    private $factory;
+
+    /** @var \Symfony\Component\EventDispatcher\EventDispatcherInterface */
+    private $eventDispatcher;
+
+    /** @var \eZ\Publish\Core\MVC\ConfigResolverInterface */
+    private $configResolver;
+
+    /** @var \eZ\Publish\API\Repository\PermissionResolver */
+    private $permissionResolver;
+
+    public function setUp()
+    {
+        $knpFactory = $this->createMock(\Knp\Menu\FactoryInterface::class);
+
+        $parameterMap = [
+            ['location_ids.content_structure', null, null, 5],
+            ['location_ids.media', null, null, 10],
+            ['location_ids.users', null, null, 20],
+        ];
+
+        $itemMap = [
+            ['root', [], new MenuItem('root', $knpFactory)],
+            [MainMenuBuilder::ITEM_CONTENT, [], new MenuItem(MainMenuBuilder::ITEM_CONTENT, $knpFactory)],
+            [MainMenuBuilder::ITEM_ADMIN, [], new MenuItem(MainMenuBuilder::ITEM_ADMIN, $knpFactory)],
+        ];
+
+        $this->factory = $this->createMock(MenuItemFactory::class);
+        $this->factory->method('createItem')->willReturnMap($itemMap);
+
+        $this->eventDispatcher = $this->createMock(EventDispatcherInterface::class);
+        $this->configResolver = $this->createMock(ConfigResolverInterface::class);
+        $this->configResolver->method('getParameter')->willReturnMap($parameterMap);
+        $this->permissionResolver = $this->createMock(PermissionResolver::class);
+    }
+
+    public function tearDown()
+    {
+        unset($this->factory, $this->eventDispatcher, $this->configResolver, $this->permissionResolver);
+    }
+
+    public function testCreateMenuForUserWithAdministratePolicy()
+    {
+        $accessMap = [
+            ['setup', 'administrate', null, true],
+        ];
+
+        $this->permissionResolver->method('hasAccess')->willReturnMap($accessMap);
+
+        $menuBuilder = new MainMenuBuilder($this->factory, $this->eventDispatcher, $this->configResolver, $this->permissionResolver);
+        $menu = $menuBuilder->createStructure([]);
+
+        $children = $menu->getChildren();
+
+        $this->assertArrayHasKey(MainMenuBuilder::ITEM_CONTENT, $children);
+        $this->assertArrayHasKey(MainMenuBuilder::ITEM_ADMIN, $children);
+    }
+
+    public function testCreateMenuForUserWithoutAdministratePolicy()
+    {
+        $accessMap = [
+            ['setup', 'administrate', null, false],
+        ];
+
+        $this->permissionResolver->method('hasAccess')->willReturnMap($accessMap);
+
+        $menuBuilder = new MainMenuBuilder($this->factory, $this->eventDispatcher, $this->configResolver, $this->permissionResolver);
+        $menu = $menuBuilder->createStructure([]);
+
+        $children = $menu->getChildren();
+
+        $this->assertArrayHasKey(MainMenuBuilder::ITEM_CONTENT, $children);
+        $this->assertArrayHasKey(MainMenuBuilder::ITEM_ADMIN, $children);
+    }
+}
