2d1d6c0d2a03f8bc2f5b1ad4c0865e019aa6b217
Maciej Kobus <maciej.kobus@ez.no>
2018-05-22 14:09:53 +0200

    EZP-29209: Front-end Notification System (from JS) API - backend

---
+++ src/bundle/DependencyInjection/Configuration/Parser/Notifications.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUiBundle\DependencyInjection\Configuration\Parser;
+
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\AbstractParser;
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\SiteAccessAware\ContextualizerInterface;
+use Symfony\Component\Config\Definition\Builder\NodeBuilder;
+
+/**
+ * Configuration parser for subtree related operations.
+ *
+ * Example configuration:
+ * ```yaml
+ * ezpublish:
+ *   system:
+ *      admin_group: # configuration per siteaccess or siteaccess group
+ *          notifications:
+ *              warning: # type of notification
+ *                  timeout: 5000 # in milliseconds
+ * ```
+ */
+class Notifications extends AbstractParser
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function mapConfig(array &$scopeSettings, $currentScope, ContextualizerInterface $contextualizer)
+    {
+        if (empty($scopeSettings['notifications'])) {
+            return;
+        }
+
+        $settings = $scopeSettings['notifications'];
+        $nodes = ['timeout'];
+
+        foreach ($settings as $type => $config) {
+            foreach ($nodes as $key) {
+                if (!isset($config[$key]) || empty($config[$key])) {
+                    continue;
+                }
+
+                $contextualizer->setContextualParameter(
+                    sprintf('notifications.%s.%s', $type, $key),
+                    $currentScope,
+                    $config[$key]
+                );
+            }
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addSemanticConfig(NodeBuilder $nodeBuilder)
+    {
+        $nodeBuilder
+            ->arrayNode('notifications')
+                ->useAttributeAsKey('type')
+                ->info('AdminUI notifications configuration.')
+                ->arrayPrototype()
+                    ->children()
+                        ->scalarNode('timeout')
+                            ->info('Time in milliseconds notifications should disappear after.')
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+    }
+}
---
+++ src/lib/UI/Config/Provider/Notifications.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUi\UI\Config\Provider;
+
+use eZ\Publish\Core\MVC\ConfigResolverInterface;
+use EzSystems\EzPlatformAdminUi\UI\Config\ProviderInterface;
+
+/**
+ * Provides information about notifications.
+ */
+class Notifications implements ProviderInterface
+{
+    public const NOTIFICATION_TYPES = ['error', 'warning', 'info', 'success'];
+
+    /** @var \eZ\Publish\Core\MVC\ConfigResolverInterface */
+    private $configResolver;
+
+    /**
+     * @param \eZ\Publish\Core\MVC\ConfigResolverInterface $configResolver
+     */
+    public function __construct(ConfigResolverInterface $configResolver)
+    {
+        $this->configResolver = $configResolver;
+    }
+
+    /**
+     * @return array
+     */
+    public function getConfig(): array
+    {
+        $config = [];
+        foreach (self::NOTIFICATION_TYPES as $type) {
+            $config[$type] = [
+                'timeout' => $this->configResolver->getParameter(sprintf('notifications.%s.timeout', $type)),
+            ];
+        }
+
+        return $config;
+    }
+}
