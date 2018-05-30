83b942f9ea09ea915d6240deb85f03a99ce51a99
Mikołaj Adamczyk <mikadamczyk@gmail.com>
2018-05-30 14:00:42 +0200

    EZP-29260: Add current Location Id to window.eZ.adminUiConfig object.

--- src/lib/UI/Config/Provider/Module/UniversalDiscoveryWidget.php
+++ src/lib/UI/Config/Provider/Module/UniversalDiscoveryWidget.php
@@ -4,40 +4,49 @@
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
+
 namespace EzSystems\EzPlatformAdminUi\UI\Config\Provider\Module;
 
 use eZ\Publish\API\Repository\ContentTypeService;
 use eZ\Publish\API\Repository\LanguageService;
 use eZ\Publish\Core\MVC\ConfigResolverInterface;
 use EzSystems\EzPlatformAdminUi\UI\Config\ProviderInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
- * Provides information about current user with resolved profile picture.
+ * Provides information about current setting for Universal Discovery Widget.
  */
 class UniversalDiscoveryWidget implements ProviderInterface
 {
-    /** @var ConfigResolverInterface */
+    /** @var \eZ\Publish\Core\MVC\ConfigResolverInterface */
     private $configResolver;
 
-    /** @var LanguageService */
+    /** @var \eZ\Publish\API\Repository\LanguageService */
     private $languageService;
 
-    /** @var ContentTypeService */
+    /** @var \eZ\Publish\API\Repository\ContentTypeService */
     private $contentTypeService;
 
+    /** @var \Symfony\Component\HttpFoundation\RequestStack */
+    private $requestStack;
+
     /**
-     * @param ConfigResolverInterface $configResolver
-     * @param LanguageService $languageService
-     * @param ContentTypeService $contentTypeService
+     * @param \eZ\Publish\Core\MVC\ConfigResolverInterface $configResolver
+     * @param \eZ\Publish\API\Repository\LanguageService $languageService
+     * @param \eZ\Publish\API\Repository\ContentTypeService $contentTypeService
+     * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
      */
     public function __construct(
         ConfigResolverInterface $configResolver,
         LanguageService $languageService,
-        ContentTypeService $contentTypeService
+        ContentTypeService $contentTypeService,
+        RequestStack $requestStack
     ) {
         $this->configResolver = $configResolver;
         $this->languageService = $languageService;
         $this->contentTypeService = $contentTypeService;
+        $this->requestStack = $requestStack;
     }
 
     /**
@@ -48,6 +57,7 @@ class UniversalDiscoveryWidget implements ProviderInterface
         /* config structure has to reflect UDW module's config structure */
         return [
             'startingLocationId' => $this->getStartingLocationId(),
+            'currentLocationId' => $this->getCurrentLocationId(),
         ];
     }
 
@@ -59,5 +69,15 @@ class UniversalDiscoveryWidget implements ProviderInterface
         return $this->configResolver->getParameter(
             'universal_discovery_widget_module.default_location_id'
         );
+    }
+
+    /**
+     * @return int|null
+     */
+    protected function getCurrentLocationId(): ?int
+    {
+        $request = $this->requestStack->getCurrentRequest();
+
+        return $request ? (int)$request->get('locationId') : null;
     }
 }
