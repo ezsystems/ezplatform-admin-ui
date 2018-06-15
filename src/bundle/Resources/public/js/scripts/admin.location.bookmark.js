5b45ed08f18b1d83d02ab6fb6f57a63724d70140
Jakub <symfiz@gmail.com>
2018-06-15 16:07:49 +0200

    EZP-29266: Content item seems to be always bookmarked (#509)
    * EZP-29266: Fixed bookmark icon on a location page; now JS dynamically adds propser CSS class on bookmark form wrapper; small adjustments in CSS styles were added.
    
    * EZP-29266: Enforced bootstrap markup in bookmark form; small improvements in JS.
    
    * EZP-29266: Deleted empty line in JS.

--- src/bundle/Resources/public/js/scripts/admin.location.bookmark.js
+++ src/bundle/Resources/public/js/scripts/admin.location.bookmark.js
@@ -2,6 +2,8 @@
     const SELECTOR_FORM = 'form[name="location_update_bookmark"]';
     const SELECTOR_BOOKMARK_CHECKBOX = '#location_update_bookmark_bookmarked';
     const SELECTOR_BOOKMARK_LOCATION_INPUT = '#location_update_bookmark_location';
+    const SELECTOR_BOOKMARK_WRAPPER = '.ez-add-to-bookmarks';
+    const CLASS_BOOKMARK_CHECKED = 'ez-add-to-bookmarks--checked';
 
     const updateBookmarkLocationInput = doc.querySelector(SELECTOR_BOOKMARK_LOCATION_INPUT);
     const currentLocationId = parseInt(updateBookmarkLocationInput.value, 10);
@@ -15,14 +17,26 @@
     const isCurrentLocation = (locationId) => {
         return parseInt(locationId, 10) === currentLocationId;
     };
+    const toggleBookmarkIconState = (isBookmarked) => {
+        const wrapper = doc.querySelector(SELECTOR_BOOKMARK_WRAPPER);
+
+        wrapper.classList.toggle(CLASS_BOOKMARK_CHECKED, isBookmarked);
+    };
     const updateBookmarkForm = (event) => {
         const { bookmarked, locationId } = event.detail;
 
         if (isCurrentLocation(locationId)) {
             updateBookmarkCheckbox(bookmarked);
+            toggleBookmarkIconState(bookmarked);
         }
+    };
+    const updateBookmarkIconState = (event) => {
+        const checked = event.target.checked;
+
+        toggleBookmarkIconState(checked);
     };
 
     doc.body.addEventListener('ez-bookmark-change', updateBookmarkForm, false);
     doc.querySelector(SELECTOR_BOOKMARK_CHECKBOX).addEventListener('change', submitBookmarkForm, false);
+    doc.querySelector(SELECTOR_BOOKMARK_CHECKBOX).addEventListener('change', updateBookmarkIconState, false);
 })(window, document);
