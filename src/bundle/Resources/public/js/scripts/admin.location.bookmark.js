e419cc9ba6db774999ef7d3a0ca0c7b23a04096c
Jakub Brzegowski <symfiz@gmail.com>
2018-06-15 13:11:48 +0200

    EZP-29266: Fixed bookmark icon on a location page; now JS dynamically adds propser CSS class on bookmark form wrapper; small adjustments in CSS styles were added.

--- src/bundle/Resources/public/js/scripts/admin.location.bookmark.js
+++ src/bundle/Resources/public/js/scripts/admin.location.bookmark.js
@@ -2,6 +2,7 @@
     const SELECTOR_FORM = 'form[name="location_update_bookmark"]';
     const SELECTOR_BOOKMARK_CHECKBOX = '#location_update_bookmark_bookmarked';
     const SELECTOR_BOOKMARK_LOCATION_INPUT = '#location_update_bookmark_location';
+    const SELECTOR_BOOKMARK_WRAPPER = '.ez-add-to-bookmarks';
 
     const updateBookmarkLocationInput = doc.querySelector(SELECTOR_BOOKMARK_LOCATION_INPUT);
     const currentLocationId = parseInt(updateBookmarkLocationInput.value, 10);
@@ -15,14 +16,31 @@
     const isCurrentLocation = (locationId) => {
         return parseInt(locationId, 10) === currentLocationId;
     };
+    const setBookmarkWrapperCheckedClass = (bookmarked) => {
+        const wrapperClassList = document.querySelector(SELECTOR_BOOKMARK_WRAPPER).classList;
+        const bookmarkCheckedClass = 'ez-add-to-bookmarks--checked';
+
+        if (bookmarked) {
+            wrapperClassList.add(bookmarkCheckedClass);
+        } else {
+            wrapperClassList.remove(bookmarkCheckedClass);
+        }
+    };
     const updateBookmarkForm = (event) => {
         const { bookmarked, locationId } = event.detail;
 
         if (isCurrentLocation(locationId)) {
             updateBookmarkCheckbox(bookmarked);
+            setBookmarkWrapperCheckedClass(bookmarked);
         }
+    };
+    const updateBookmarkWrapperClass = (event) => {
+        const checked = event.target.checked;
+
+        setBookmarkWrapperCheckedClass(checked);
     };
 
     doc.body.addEventListener('ez-bookmark-change', updateBookmarkForm, false);
     doc.querySelector(SELECTOR_BOOKMARK_CHECKBOX).addEventListener('change', submitBookmarkForm, false);
+    doc.querySelector(SELECTOR_BOOKMARK_CHECKBOX).addEventListener('change', updateBookmarkWrapperClass, false);
 })(window, document);
