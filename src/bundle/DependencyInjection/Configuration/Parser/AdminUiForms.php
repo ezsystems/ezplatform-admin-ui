3dc6bbe819b6e017b51b4fc38a95553e9089f000
Andrew Longosz <alongosz@users.noreply.github.com>
2018-06-22 15:31:56 +0200

    EZEE-2111: Landing Page field is visible while creating new Page with COTF (#530)
    * EZEE-2111: Created SA-aware setting to inject proper form templates
    
    To be configured using:
    
    ezpublish:
        system:
            <scope>:
                admin_ui_forms:
                    content_edit_form_templates:
    
    * EZEE-2111: Created default configuration for content edit form templates
    
    * EZEE-2111: Exposed form templates setting to admin_ui_config Twig global
    
    * EZEE-2111: Created unit test for mapping prioritized form templates list

---
+++ src/bundle/DependencyInjection/Configuration/Parser/AdminUiForms.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUiBundle\DependencyInjection\Configuration\Parser;
+
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\AbstractParser;
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\SiteAccessAware\ContextualizerInterface;
+use Symfony\Component\Config\Definition\Builder\NodeBuilder;
+
+/**
+ * Configuration parser for Admin UI forms settings.
+ *
+ * Example configuration:
+ * ```yaml
+ * ezpublish:
+ *   system:
+ *      admin_group: # configuration per SiteAccess or SiteAccess group
+ *          admin_ui_forms:
+ *              content_edit_form_templates:
+ *                  - { template: 'template.html.twig', priority: 0 }
+ * ```
+ */
+class AdminUiForms extends AbstractParser
+{
+    const FORM_TEMPLATES_PARAM = 'admin_ui_forms.content_edit_form_templates';
+
+    /**
+     * Adds semantic configuration definition.
+     *
+     * @param \Symfony\Component\Config\Definition\Builder\NodeBuilder $nodeBuilder Node just under ezpublish.system.<siteaccess>
+     */
+    public function addSemanticConfig(NodeBuilder $nodeBuilder)
+    {
+        $nodeBuilder
+            ->arrayNode('admin_ui_forms')
+                ->info('Admin UI forms configuration settings')
+                ->children()
+                    ->arrayNode('content_edit_form_templates')
+                        ->info('A list of Content Edit (and create) default Twig form templates')
+                        ->arrayPrototype()
+                            ->children()
+                                ->scalarNode('template')->end()
+                                ->integerNode('priority')->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function mapConfig(
+        array &$scopeSettings,
+        $currentScope,
+        ContextualizerInterface $contextualizer
+    ): void {
+        $contextualizer->setContextualParameter(
+            static::FORM_TEMPLATES_PARAM,
+            $currentScope,
+            $this->processContentEditFormTemplates(
+                $scopeSettings['admin_ui_forms']['content_edit_form_templates'] ?? []
+            )
+        );
+    }
+
+    /**
+     * Processes given prioritized list of templates, sorts them according to their priorities and
+     * returns as a simple list of templates.
+     *
+     * The input list of the templates needs to be in the form of:
+     * <code>
+     *  [
+     *      [ 'template' => '<file_path>', 'priority' => <int> ],
+     *  ],
+     * </code>
+     *
+     * @param array $formTemplates
+     *
+     * @return array ordered list of templates
+     */
+    private function processContentEditFormTemplates(array $formTemplates)
+    {
+        $priorities = array_column($formTemplates, 'priority');
+        array_multisort($priorities, SORT_DESC, $formTemplates);
+
+        // return as a simple list of templates.
+        return array_column($formTemplates, 'template');
+    }
+}
