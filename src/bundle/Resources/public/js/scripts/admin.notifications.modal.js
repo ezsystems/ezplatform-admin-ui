4c5baa74f992813a2f23f8ec4316b4e1fc4522ab
Dawid Parafiński <vinniczek@gmail.com>
2018-06-27 16:02:32 +0200

    EZEE-2081: Move NotificationBundle into AdminUI (#518)
    * EZEE-2081: Move NotificationBundle into AdminUI
    
    * EZEE-2081: Typehints and docblocks fix
    
    * EZEE-2081: Notification API VO + offset/limit changes
    
    * EZEE-2113: Notifications modal design harmonization
    
    * EZEE-2081: Move notifications up in user menu

---
+++ src/bundle/Resources/public/js/scripts/admin.notifications.modal.js
@@ -0,0 +1,89 @@
+(function (global, doc) {
+    let notificationsCount = 0;
+    const SELECTOR_MODAL_ITEM = '.n-notifications-modal__item';
+    const SELECTOR_DESC_TEXT = '.description__text';
+    const SELECTOR_TABLE = '.n-table--notifications';
+    const SELECTOR_TABLE_BODY = '.n-table--notifications .n-table__body';
+    const CLASS_ELLIPSIS = 'description__text--ellipsis';
+    const INTERVAL = 30000;
+    const token = document.querySelector('meta[name="CSRF-Token"]').content;
+    const modal = doc.querySelector('.n-notifications-modal');
+    const markAsRead = (notification) => {
+        const requestOptions = {
+            headers: {
+                'X-CSRF-Token': token
+            },
+            mode: 'cors',
+            credentials: 'same-origin'
+        };
+
+        fetch(notification.dataset.notificationRead, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                if (data.status === 'success') {
+                    notification.classList.add('n-notifications-modal__item--read');
+                }
+                if (data.redirect) {
+                    window.location = data.redirect;
+                }
+            });
+    };
+    const handleClick = (event) => {
+        if (event.target.classList.contains('description__read-more')) {
+            event.target.closest(SELECTOR_MODAL_ITEM).querySelector(SELECTOR_DESC_TEXT).classList.remove(CLASS_ELLIPSIS);
+
+            return;
+        }
+
+        markAsRead(event.target.closest(SELECTOR_MODAL_ITEM));
+    };
+    const getNotificationsStatus = () => {
+        const requestOptions = {
+            headers: {
+                'X-CSRF-Token': token
+            },
+            mode: 'cors',
+            credentials: 'same-origin'
+        };
+
+        fetch(modal.querySelector(SELECTOR_TABLE).dataset.notificationsCount, requestOptions)
+            .then(response => response.json())
+            .then(data => setPendingNotificationCount(data));
+    };
+    const getNotificationsList = () => {
+        const requestOptions = {
+            headers: {
+                'X-CSRF-Token': token
+            },
+            mode: 'cors',
+            credentials: 'same-origin'
+        };
+
+        fetch(modal.querySelector(SELECTOR_TABLE).dataset.notifications, requestOptions)
+            .then(response => response.text())
+            .then(notifications => doc.querySelector(SELECTOR_TABLE_BODY).innerHTML = notifications);
+    };
+    const setPendingNotificationCount = (notificationsInfo) => {
+        const methodName = notificationsInfo.pending ? 'add' : 'remove';
+        const userAvatar = doc.querySelector('.ez-user-menu__avatar-wrapper');
+
+        userAvatar.setAttribute('data-count', notificationsInfo.pending);
+        userAvatar.classList[methodName]('n-pending-notifications');
+        doc.querySelector('.ez-user-menu__item--notifications').setAttribute('data-count', `(${notificationsInfo.pending})`);
+
+        if (notificationsCount !== notificationsInfo.total) {
+            notificationsCount = notificationsInfo.total;
+
+            getNotificationsList();
+        }
+    };
+
+    if (!modal) {
+        return;
+    }
+
+    [...modal.querySelectorAll(SELECTOR_TABLE_BODY)].forEach(btn => btn.addEventListener('click', handleClick, false));
+
+    getNotificationsStatus();
+    window.setInterval(getNotificationsStatus, INTERVAL);
+})(window, document);
---
+++ src/lib/Strategy/NotificationTwigStrategy.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUi\Strategy;
+
+use eZ\Publish\API\Repository\ContentService;
+use eZ\Publish\API\Repository\Exceptions\NotFoundException;
+use eZ\Publish\API\Repository\Repository;
+use EzSystems\EzPlatformAdminUi\Exception\NoValidResultException;
+
+class NotificationTwigStrategy
+{
+    /** @var \eZ\Publish\API\Repository\Repository */
+    private $repository;
+
+    /** @var \eZ\Publish\API\Repository\ContentService */
+    private $contentService;
+
+    /** @var string */
+    private $defaultTemplate;
+
+    /**
+     * @param \eZ\Publish\API\Repository\Repository $repository
+     * @param \eZ\Publish\API\Repository\ContentService $contentService
+     */
+    public function __construct(
+        Repository $repository,
+        ContentService $contentService
+    ) {
+        $this->repository = $repository;
+        $this->contentService = $contentService;
+    }
+
+    /**
+     * @param string $defaultTemplate
+     */
+    public function setDefault(string $defaultTemplate)
+    {
+        $this->defaultTemplate = $defaultTemplate;
+    }
+
+    /**
+     * @param mixed $contentId
+     *
+     * @return string
+     *
+     * @throws \EzSystems\Notification\Exception\NoValidResultException
+     */
+    public function decide($contentId): string
+    {
+        if ($this->isContentPermanentlyDeleted($contentId)) {
+            return '@EzPlatformAdminUi/notifications/permanently_deleted.html.twig';
+        }
+        if ($this->isContentTrashed($contentId)) {
+            return '@EzPlatformAdminUi/notifications/trashed.html.twig';
+        }
+        if (!empty($this->defaultTemplate)) {
+            return $this->defaultTemplate;
+        }
+
+        throw new NoValidResultException();
+    }
+
+    private function isContentPermanentlyDeleted($contentId): bool
+    {
+        // Using sudo in order to be sure that information is valid in case user no longer have access to content i.e when in trash.
+        try {
+            $this->repository->sudo(
+                function () use ($contentId) {
+                    return $this->contentService->loadContentInfo($contentId);
+                }
+            );
+
+            return false;
+        } catch (NotFoundException $exception) {
+            return true;
+        }
+    }
+
+    private function isContentTrashed($contentId): bool
+    {
+        // Using sudo in order to be sure that information is valid in case user no longer have access to content i.e when in trash.
+        $contentInfo = $this->repository->sudo(
+            function () use ($contentId) {
+                return $this->contentService->loadContentInfo($contentId);
+            }
+        );
+
+        return $contentInfo->isTrashed();
+    }
+}
