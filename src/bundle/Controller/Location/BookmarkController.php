96c6ef03ad94eb8569fa806fbbfb71c8806e4f17
Mikołaj Adamczyk <mikadamczyk@gmail.com>
2018-06-28 11:55:15 +0200

    EZP-29341: I want Bookmark button to work without page reload

--- src/bundle/Controller/Location/BookmarkController.php
+++ src/bundle/Controller/Location/BookmarkController.php
@@ -9,12 +9,18 @@ declare(strict_types=1);
 namespace EzSystems\EzPlatformAdminUiBundle\Controller\Location;
 
 use eZ\Publish\API\Repository\BookmarkService;
-use EzSystems\EzPlatformAdminUi\Form\Data\Location\LocationUpdateBookmarkData;
+use eZ\Publish\API\Repository\Exceptions\InvalidArgumentException;
+use eZ\Publish\API\Repository\Exceptions\NotFoundException;
 use EzSystems\EzPlatformAdminUi\Form\Factory\FormFactory;
 use EzSystems\EzPlatformAdminUi\Form\SubmitHandler;
 use EzSystems\EzPlatformAdminUiBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use eZ\Publish\API\Repository\Exceptions\UnauthorizedException as APIRepositoryUnauthorizedException;
+use EzSystems\EzPlatformAdminUi\UI\Action\EventDispatcherInterface;
+use EzSystems\EzPlatformAdminUi\UI\Action\FormUiActionMappingDispatcher;
+use EzSystems\EzPlatformAdminUi\UI\Action\UiActionEventInterface;
 
 class BookmarkController extends Controller
 {
@@ -27,17 +33,27 @@ class BookmarkController extends Controller
     /** @var \eZ\Publish\API\Repository\BookmarkService */
     private $bookmarkService;
 
+    /** @var \EzSystems\EzPlatformAdminUi\UI\Action\EventDispatcherInterface */
+    private $uiActionEventDispatcher;
+
+    /** @var \EzSystems\EzPlatformAdminUi\UI\Action\FormUiActionMappingDispatcher */
+    private $formUiActionMappingDispatcher;
+
     public function __construct(
         FormFactory $formFactory,
         SubmitHandler $submitHandler,
-        BookmarkService $bookmarkService
+        BookmarkService $bookmarkService,
+        EventDispatcherInterface $uiActionEventDispatcher,
+        FormUiActionMappingDispatcher $formUiActionMappingDispatcher
     ) {
         $this->formFactory = $formFactory;
         $this->submitHandler = $submitHandler;
         $this->bookmarkService = $bookmarkService;
+        $this->uiActionEventDispatcher = $uiActionEventDispatcher;
+        $this->formUiActionMappingDispatcher = $formUiActionMappingDispatcher;
     }
 
-    public function updateBookmarkAction(Request $request): Response
+    public function updateBookmarkAction(Request $request): JsonResponse
     {
         $updateBookmarkForm = $this->formFactory->updateBookmarkLocation();
         $updateBookmarkForm->handleRequest($request);
@@ -47,25 +63,35 @@ class BookmarkController extends Controller
         $location = $data->getLocation();
 
         if ($updateBookmarkForm->isValid()) {
-            $result = $this->submitHandler->handle($updateBookmarkForm, function (LocationUpdateBookmarkData $data) use ($location) {
+            $data = $updateBookmarkForm->getData();
+            try {
                 if ($data->isBookmarked()) {
                     $this->bookmarkService->createBookmark($location);
                 } else {
                     $this->bookmarkService->deleteBookmark($location);
                 }
+                $response = new JsonResponse();
+                $event = $this->formUiActionMappingDispatcher->dispatch($updateBookmarkForm);
+                $event->setResponse($response);
+                $event->setType(UiActionEventInterface::TYPE_SUCCESS);
 
-                return $this->redirectToLocation($location);
-            });
+                $this->uiActionEventDispatcher->dispatch($event);
 
-            if ($result instanceof Response) {
-                return $result;
+                return $response;
+            } catch (APIRepositoryUnauthorizedException $e) {
+                return new JsonResponse(['errors' => [$e->getMessage()]], Response::HTTP_UNAUTHORIZED);
+            } catch (InvalidArgumentException $e) {
+                return new JsonResponse(['errors' => [$e->getMessage()]], Response::HTTP_UNPROCESSABLE_ENTITY);
+            } catch (NotFoundException $e) {
+                return new JsonResponse(['errors' => [$e->getMessage()]], Response::HTTP_NOT_FOUND);
             }
-        }
+        } else {
+            $errors = [];
+            foreach ($updateBookmarkForm->getErrors(true, true) as $formError) {
+                $errors[] = $formError->getMessage();
+            }
 
-        if ($location) {
-            return $this->redirectToLocation($location);
+            return new JsonResponse(['errors' => $errors], Response::HTTP_UNPROCESSABLE_ENTITY);
         }
-
-        return $this->redirectToRoute('ezplatform.dashboard');
     }
 }
