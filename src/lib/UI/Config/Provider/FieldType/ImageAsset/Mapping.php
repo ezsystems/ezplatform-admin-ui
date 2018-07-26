dc757d2d3a390e38b9287e6427895447f5f05d4b
Adam Wójs <adam@wojs.pl>
2018-07-26 22:34:24 +0200

    EZP-29104: Implemented UI for ImageAsset Field Type
    Co-authored-by: Dawid Parafiński <dawid.parafinski@ez.no>
    Co-authored-by: Jakub Brzegowski <symfiz@gmail.com>

---
+++ src/lib/UI/Config/Provider/FieldType/ImageAsset/Mapping.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUi\UI\Config\Provider\FieldType\ImageAsset;
+
+use eZ\Publish\Core\MVC\ConfigResolverInterface;
+use EzSystems\EzPlatformAdminUi\UI\Config\ProviderInterface;
+
+/**
+ * Provide information about ImageAsset Field Type mappings.
+ */
+class Mapping implements ProviderInterface
+{
+    /** @var \eZ\Publish\Core\MVC\ConfigResolverInterface */
+    private $configResolver;
+
+    /**
+     * @param \eZ\Publish\Core\MVC\ConfigResolverInterface $configResolver
+     */
+    public function __construct(ConfigResolverInterface $configResolver)
+    {
+        $this->configResolver = $configResolver;
+    }
+
+    /**
+     * @return array
+     */
+    public function getConfig(): array
+    {
+        $mappings = $this->configResolver->getParameter('fieldtypes.ezimageasset.mappings');
+
+        return [
+            'contentTypeIdentifier' => $mappings['content_type_identifier'],
+            'contentFieldIdentifier' => $mappings['content_field_identifier'],
+            'nameFieldIdentifier' => $mappings['name_field_identifier'],
+            'parentLocationId' => $mappings['parent_location_id'],
+        ];
+    }
+}
---
+++ src/lib/UniversalDiscovery/Event/Subscriber/ImageAssetDefaultLocationId.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUi\UniversalDiscovery\Event\Subscriber;
+
+use EzSystems\EzPlatformAdminUi\UniversalDiscovery\Event\ConfigResolveEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use eZ\Publish\Core\FieldType\ImageAsset\AssetMapper;
+
+class ImageAssetDefaultLocationId implements EventSubscriberInterface
+{
+    /** @var \eZ\Publish\Core\FieldType\ImageAsset\AssetMapper */
+    private $assetMapper;
+
+    /**
+     * @param \eZ\Publish\Core\FieldType\ImageAsset\AssetMapper $assetMapper
+     */
+    public function __construct(AssetMapper $assetMapper)
+    {
+        $this->assetMapper = $assetMapper;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ConfigResolveEvent::NAME => ['onUdwConfigResolve'],
+        ];
+    }
+
+    /**
+     * @param \EzSystems\EzPlatformAdminUi\UniversalDiscovery\Event\ConfigResolveEvent $event
+     */
+    public function onUdwConfigResolve(ConfigResolveEvent $event): void
+    {
+        if ($event->getConfigName() !== 'image_asset') {
+            return;
+        }
+
+        $config = $event->getConfig();
+        $config['starting_location_id'] = $this->assetMapper->getParentLocationId();
+
+        $event->setConfig($config);
+    }
+}
