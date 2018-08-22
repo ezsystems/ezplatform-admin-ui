9646a06bb15cd5495ccf59f36d646e74e30c3a6f
Jakub Brzegowski <symfiz@gmail.com>
2018-08-22 09:18:37 +0200

    EZEE-2062: Fix Add Toolbar visibility - do not show when nothing selected

--- src/bundle/Resources/public/js/alloyeditor/src/toolbars/ez-add.js
+++ src/bundle/Resources/public/js/alloyeditor/src/toolbars/ez-add.js
@@ -8,33 +8,59 @@ export default class EzToolbarAdd extends AlloyEditor.Toolbars.add {
     }
 
     /**
+     * Checks if anything was selected in editor.
+     * There may be situation when editor has focus but none of its elements.
+     *
+     * @returns {Boolean}
+     */
+    checkIsNothingSelected() {
+        const { selectionData } = this.props;
+
+        if (!selectionData || !selectionData.region) {
+            return false;
+        }
+
+        const { top, bottom, left, right } = selectionData.region;
+
+        return !top && !bottom && !left && !right;
+    }
+
+    /**
      * Lifecycle. Renders the UI of the button.
      *
      * @method render
-     * @return {Object} The content which should be rendered.
+     * @returns {Object} The content which should be rendered.
      */
     render() {
         const selectionData = this.props.selectionData;
+        const nothingSelected = this.checkIsNothingSelected();
+        const textSelected = selectionData && selectionData.text;
 
-        if (selectionData && selectionData.text) {
+        if (textSelected || nothingSelected) {
             return null;
         }
 
         const buttons = this._getButtons();
+
+        if (!buttons) {
+            return null;
+        }
+
         const className = this._getToolbarClassName();
 
         return (
             <div
-                aria-label={AlloyEditor.Strings.add} className={className}
-                data-tabindex={this.props.config.tabIndex || 0} onFocus={this.focus}
-                onKeyDown={this.handleKey} role="toolbar" tabIndex="-1"
-            >
-                <div className="ae-container">
-                    {buttons}
-                </div>
+                aria-label={AlloyEditor.Strings.add}
+                className={className}
+                data-tabindex={this.props.config.tabIndex || 0}
+                onFocus={this.focus}
+                onKeyDown={this.handleKey}
+                role="toolbar"
+                tabIndex="-1">
+                <div className="ae-container">{buttons}</div>
             </div>
         );
     }
 }
 
- AlloyEditor.Toolbars[EzToolbarAdd.key] = EzToolbarAdd;
+AlloyEditor.Toolbars[EzToolbarAdd.key] = EzToolbarAdd;
