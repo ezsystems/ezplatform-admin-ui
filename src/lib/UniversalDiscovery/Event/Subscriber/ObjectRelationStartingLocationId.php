9546af6be778aab0ef5be2dd821e1589c14f2c68
Dawid Parafiński <vinniczek@gmail.com>
2018-10-02 15:07:05 +0200

    EZP-29682: Cannot set default Location for objectrelationlist (#652)
    * EZP-29682: Cannot set default Location for objectrelationlist
    
    * EZP-29682: fixed starting location id for udw
    
    * EZP-29682: fixed bc break, move logic from new config to additional context parameter
    
    * EZP-29682: changed default location id value

---
+++ src/lib/UniversalDiscovery/Event/Subscriber/ObjectRelationStartingLocationId.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUi\UniversalDiscovery\Event\Subscriber;
+
+use EzSystems\EzPlatformAdminUi\UniversalDiscovery\Event\ConfigResolveEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class ObjectRelationStartingLocationId implements EventSubscriberInterface
+{
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ConfigResolveEvent::NAME => ['onUdwConfigResolve'],
+        ];
+    }
+
+    /**
+     * @param \EzSystems\EzPlatformAdminUi\UniversalDiscovery\Event\ConfigResolveEvent $event
+     */
+    public function onUdwConfigResolve(ConfigResolveEvent $event): void
+    {
+        $configName = $event->getConfigName();
+        if ('single' !== $configName && 'multiple' !== $configName) {
+            return;
+        }
+
+        $context = $event->getContext();
+        if (
+            !isset($context['type'])
+            || 'object_relation' !== $context['type']
+            || !isset($context['starting_location_id'])
+        ) {
+            return;
+        }
+
+        $config = $event->getConfig();
+        $config['starting_location_id'] = $context['starting_location_id'];
+
+        $event->setConfig($config);
+    }
+}
