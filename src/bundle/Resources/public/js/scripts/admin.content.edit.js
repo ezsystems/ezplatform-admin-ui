444d34b1ee11316b8801a3889bb5ddf06a5d4cad
sunpietro <poczta@piotrnalepa.pl>
2018-10-03 11:06:23 +0200

    EZP-29679: Validation error messages not visible when trying to publish content (#660)
    * EZP-29679: Validation error messages not visible when trying to publish content
    
    * fixup
    
    * fixup

--- src/bundle/Resources/public/js/scripts/admin.content.edit.js
+++ src/bundle/Resources/public/js/scripts/admin.content.edit.js
@@ -1,40 +1,59 @@
 (function(global, doc, eZ) {
     const form = doc.querySelector('.ez-form-validate');
     const submitBtns = form.querySelectorAll('[type="submit"]:not([formnovalidate])');
+    const getValidationResults = (validator) => {
+        const isValid = validator.isValid();
+        const validatorName = validator.constructor.name;
+        const result = { isValid, validatorName };
+
+        return result;
+    };
+    const fields = doc.querySelectorAll('.ez-field-edit');
+    const focusOnFirstError = () => {
+        const invalidFields = [...doc.querySelectorAll('.ez-field-edit.is-invalid')];
+
+        fields.forEach((field) => field.removeAttribute('tabindex'));
+        invalidFields.forEach((field) => field.setAttribute('tabindex', '-1'));
+        invalidFields[0].focus();
+
+        doc.querySelector('.ez-content-item__errors-wrapper').removeAttribute('hidden');
+    };
+    const clickHandler = (event) => {
+        const btn = event.currentTarget;
+
+        if (parseInt(btn.dataset.isFormValid, 10)) {
+            return;
+        }
+
+        event.preventDefault();
+
+        const validators = eZ.fieldTypeValidators;
+        const validationResults = validators.map(getValidationResults);
+        const isFormValid = validationResults.every((result) => result.isValid);
+
+        if (isFormValid) {
+            btn.dataset.isFormValid = 1;
+            // for some reason trying to fire click event inside the event handler flow is impossible
+            // the following line breaks the flow so it's possible to fire click event on a button again.
+            window.setTimeout(() => btn.click(), 0);
+        } else {
+            btn.dataset.validatorsWithErrors = Array.from(
+                validationResults.filter((result) => !result.isValid).reduce((total, result) => {
+                    total.add(result.validatorName);
+
+                    return total;
+                }, new Set())
+            ).join();
+
+            fields.forEach((field) => field.removeAttribute('id'));
+
+            focusOnFirstError();
+        }
+    };
 
     form.setAttribute('novalidate', true);
 
     submitBtns.forEach((btn) => {
-        const clickHandler = (event) => {
-            if (!parseInt(btn.dataset.isFormValid, 10)) {
-                event.preventDefault();
-
-                const validators = eZ.fieldTypeValidators;
-                const validationResults = validators.map((validator) => {
-                    const isValid = validator.isValid();
-                    const validatorName = validator.constructor.name;
-                    const result = { isValid, validatorName };
-
-                    return result;
-                });
-                const isFormValid = validationResults.every((result) => result.isValid);
-
-                if (isFormValid) {
-                    btn.dataset.isFormValid = 1;
-                    // for some reason trying to fire click event inside the event handler flow is impossible
-                    // the following line breaks the flow so it's possible to fire click event on a button again.
-                    window.setTimeout(() => btn.click(), 0);
-                } else {
-                    btn.dataset.validatorsWithErrors = Array.from(
-                        validationResults.filter((result) => !result.isValid).reduce((total, result) => {
-                            total.add(result.validatorName);
-
-                            return total;
-                        }, new Set())
-                    ).join();
-                }
-            }
-        };
         btn.dataset.isFormValid = 0;
         btn.addEventListener('click', clickHandler, false);
     });
