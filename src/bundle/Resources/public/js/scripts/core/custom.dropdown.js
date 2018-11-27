46f59232d496773a28fc0170031efef1497f7ad4
sunpietro <poczta@piotrnalepa.pl>
2018-11-27 13:52:42 +0100

    EZP-29828: Custom Dropdown improvements (#722)
    * EZP-29828: Custom Dropdown improvements
    
    * fixup
    
    * fixup

--- src/bundle/Resources/public/js/scripts/core/custom.dropdown.js
+++ src/bundle/Resources/public/js/scripts/core/custom.dropdown.js
@@ -1,4 +1,5 @@
 (function(global, doc) {
+    const CLASS_CUSTOM_DROPDOWN = 'ez-custom-dropdown';
     const CLASS_ITEMS_HIDDEN = 'ez-custom-dropdown__items--hidden';
     const CLASS_REMOVE_SELECTION = 'ez-custom-dropdown__remove-selection';
     const CLASS_ITEM_SELECTED = 'ez-custom-dropdown__item--selected';
@@ -8,78 +9,88 @@
     const SELECTOR_SELECTION_INFO = '.ez-custom-dropdown__selection-info';
     const EVENT_VALUE_CHANGED = 'valueChanged';
 
-    const createSelectedItem = Symbol('createSelectedItem');
-    const onSelect = Symbol('onSelect');
-    const selectFirstItem = Symbol('selectFirstItem');
-    const clearCurrentSelection = Symbol('clearCurrentSelection');
-    const onClickOutside = Symbol('onClickOutside');
-    const hideOptions = Symbol('hideOptions');
-    const onInputClick = Symbol('onInputClick');
-    const onOptionClick = Symbol('onOptionClick');
-    const canSelectOnlyOne = Symbol('canSelectOnlyOne');
-
     class CustomDropdown {
         constructor(config) {
             this.container = config.container;
             this.sourceInput = config.sourceInput;
             this.itemsContainer = config.itemsContainer;
             this.hasDefaultSelection = config.hasDefaultSelection || false;
-
-            this[canSelectOnlyOne] = !config.sourceInput.multiple;
-            this[createSelectedItem] = this[createSelectedItem].bind(this);
-            this[selectFirstItem] = this[selectFirstItem].bind(this);
-            this[clearCurrentSelection] = this[clearCurrentSelection].bind(this);
-            this[hideOptions] = this[hideOptions].bind(this);
-            this[onSelect] = this[onSelect].bind(this);
-            this[onClickOutside] = this[onClickOutside].bind(this);
-            this[onInputClick] = this[onInputClick].bind(this);
-            this[onOptionClick] = this[onOptionClick].bind(this);
-        }
-
-        [createSelectedItem](value, label) {
-            return `<li
+            this.selectedItemTemplate =
+                config.selectedItemTemplate ||
+                `<li
                 class="ez-custom-dropdown__selected-item"
-                data-value="${value}">
-                    ${label}<span class="${CLASS_REMOVE_SELECTION}"></span>
+                data-value="{{value}}">
+                    {{label}}<span class="${CLASS_REMOVE_SELECTION}"></span>
             </li>`;
+
+            this.canSelectOnlyOne = !config.sourceInput.multiple;
+            this.createSelectedItem = this.createSelectedItem.bind(this);
+            this.selectFirstItem = this.selectFirstItem.bind(this);
+            this.clearCurrentSelection = this.clearCurrentSelection.bind(this);
+            this.hideOptions = this.hideOptions.bind(this);
+            this.onSelect = this.onSelect.bind(this);
+            this.onClickOutside = this.onClickOutside.bind(this);
+            this.onInputClick = this.onInputClick.bind(this);
+            this.onOptionClick = this.onOptionClick.bind(this);
         }
 
-        [selectFirstItem]() {
-            const firstItem = this.itemsContainer.querySelector(`${SELECTOR_ITEM}`);
+        createSelectedItem(value, label) {
+            return this.selectedItemTemplate.replace('{{value}}', value).replace('{{label}}', label);
+        }
 
+        selectFirstItem() {
+            const items = this.itemsContainer.querySelectorAll(`${SELECTOR_ITEM}`);
+            const firstItem = items[0];
+
+            items.forEach((item) => item.classList.remove(CLASS_ITEM_SELECTED));
             firstItem.classList.add(CLASS_ITEM_SELECTED);
 
             this.container
                 .querySelector(SELECTOR_SELECTION_INFO)
-                .insertAdjacentHTML('beforeend', this[createSelectedItem](firstItem.dataset.value, firstItem.innerHTML));
+                .insertAdjacentHTML('beforeend', this.createSelectedItem(firstItem.dataset.value, firstItem.innerHTML));
         }
 
-        [clearCurrentSelection]() {
+        clearCurrentSelection() {
             this.sourceInput.querySelectorAll('option').forEach((option) => (option.selected = false));
             this.itemsContainer.querySelectorAll(SELECTOR_ITEM).forEach((option) => option.classList.remove(CLASS_ITEM_SELECTED));
             this.container.querySelector(SELECTOR_SELECTION_INFO).innerHTML = '';
         }
 
-        [hideOptions]() {
+        hideOptions() {
             return this.itemsContainer.classList.add(CLASS_ITEMS_HIDDEN);
         }
 
-        [onSelect](element, selected) {
-            const value = element.dataset.value;
-            const CSSMethodName = selected ? 'add' : 'remove';
+        deselectPlaceholderOption() {
+            const placeholderItem = this.itemsContainer.querySelector('[data-value=""]');
 
-            if (this[canSelectOnlyOne] && selected) {
-                this[hideOptions]();
-                this[clearCurrentSelection]();
+            if (!placeholderItem) {
+                return;
             }
 
-            this.sourceInput.querySelector(`[value="${value}"]`).selected = selected;
-            this.itemsContainer.querySelector(`[data-value="${value}"]`).classList[CSSMethodName](CLASS_ITEM_SELECTED);
+            placeholderItem.selected = false;
+        }
+
+        onSelect(element, selected) {
+            const value = element.dataset.value;
+            const cssMethodName = selected ? 'add' : 'remove';
+
+            if (this.canSelectOnlyOne && selected) {
+                this.hideOptions();
+                this.clearCurrentSelection();
+            }
+
+            this.deselectPlaceholderOption();
+
+            if (value) {
+                this.sourceInput.querySelector(`[value="${value}"]`).selected = selected;
+            }
+
+            this.itemsContainer.querySelector(`[data-value="${value}"]`).classList[cssMethodName](CLASS_ITEM_SELECTED);
 
             if (selected && value) {
                 this.container
                     .querySelector(SELECTOR_SELECTION_INFO)
-                    .insertAdjacentHTML('beforeend', this[createSelectedItem](value, element.innerHTML));
+                    .insertAdjacentHTML('beforeend', this.createSelectedItem(value, element.innerHTML));
             } else {
                 const valueNode = this.container.querySelector(`${SELECTOR_SELECTION_INFO} [data-value="${value}"]`);
 
@@ -88,26 +99,26 @@
                 }
             }
 
-            if (this[canSelectOnlyOne] && !selected && this.hasDefaultSelection) {
-                this[hideOptions]();
-                this[selectFirstItem]();
+            if (this.canSelectOnlyOne && !selected && this.hasDefaultSelection) {
+                this.hideOptions();
+                this.selectFirstItem();
             }
 
             this.sourceInput.dispatchEvent(new CustomEvent(EVENT_VALUE_CHANGED));
         }
 
-        [onClickOutside](event) {
+        onClickOutside(event) {
             if (event.target.closest(SELECTOR_SELECTION_INFO) || event.target.closest(SELECTOR_ITEMS)) {
                 return;
             }
 
-            this[hideOptions]();
+            this.hideOptions();
             this.sourceInput.dispatchEvent(new CustomEvent(EVENT_VALUE_CHANGED));
         }
 
-        [onInputClick](event) {
+        onInputClick(event) {
             if (event.target.classList.contains(CLASS_REMOVE_SELECTION)) {
-                this[onSelect](event.target.closest(SELECTOR_SELECTED_ITEM), false);
+                this.onSelect(event.target.closest(SELECTOR_SELECTED_ITEM), false);
 
                 return;
             }
@@ -115,24 +126,26 @@
             const methodName = this.itemsContainer.classList.contains(CLASS_ITEMS_HIDDEN) ? 'addEventListener' : 'removeEventListener';
 
             this.itemsContainer.classList.toggle(CLASS_ITEMS_HIDDEN);
-            doc.querySelector('body')[methodName]('click', this[onClickOutside], false);
+            doc.querySelector('body')[methodName]('click', this.onClickOutside, false);
         }
 
-        [onOptionClick](event) {
-            return this[onSelect](event.target, !event.target.classList.contains(CLASS_ITEM_SELECTED));
+        onOptionClick(event) {
+            return this.onSelect(event.target, !event.target.classList.contains(CLASS_ITEM_SELECTED));
         }
 
         init() {
             const isEmpty = !this.container.querySelectorAll(SELECTOR_SELECTED_ITEM).length;
 
-            if (isEmpty && this[canSelectOnlyOne] && this.hasDefaultSelection) {
-                this[selectFirstItem]();
+            this.container.classList.add(CLASS_CUSTOM_DROPDOWN);
+
+            if (isEmpty && this.hasDefaultSelection) {
+                this.selectFirstItem();
             }
 
-            this.container.querySelector(SELECTOR_SELECTION_INFO).onclick = this[onInputClick];
+            this.container.querySelector(SELECTOR_SELECTION_INFO).onclick = this.onInputClick;
             this.itemsContainer
                 .querySelectorAll(`${SELECTOR_ITEM}:not([disabled])`)
-                .forEach((option) => (option.onclick = this[onOptionClick]));
+                .forEach((option) => (option.onclick = this.onOptionClick));
         }
     }
 
