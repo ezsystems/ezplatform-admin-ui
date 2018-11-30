f60d072fe7f3674576a3ae5a0cfb4cdf788a4281
Maciej Kobus <maciej.kobus@ez.no>
2018-11-30 11:16:53 +0100

    EZP-29848: Make existing AdminUI tabs extendable by allowing template path and parameters change

---
+++ src/lib/Tab/Event/TabViewRenderEvent.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUi\Tab\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+class TabViewRenderEvent extends Event
+{
+    /** @var string */
+    private $tabIdentifier;
+
+    /** @var string */
+    private $template;
+
+    /** @var array */
+    private $parameters;
+
+    /**
+     * @param string $tabIdentifier
+     * @param string $template
+     * @param array $parameters
+     */
+    public function __construct(string $tabIdentifier, string $template, array $parameters = [])
+    {
+        $this->tabIdentifier = $tabIdentifier;
+        $this->template = $template;
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTabIdentifier(): string
+    {
+        return $this->tabIdentifier;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTemplate(): string
+    {
+        return $this->template;
+    }
+
+    /**
+     * @param string $template
+     */
+    public function setTemplate(string $template): void
+    {
+        $this->template = $template;
+    }
+
+    /**
+     * @return array
+     */
+    public function getParameters(): array
+    {
+        return $this->parameters;
+    }
+
+    /**
+     * @param array $parameters
+     */
+    public function setParameters(array $parameters): void
+    {
+        $this->parameters = $parameters;
+    }
+}
