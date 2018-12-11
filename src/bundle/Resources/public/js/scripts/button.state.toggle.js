678ddf9ccc4b72af4929d141ccc9ebeef0cbae65
Jakub <symfiz@gmail.com>
2018-12-11 10:30:12 +0100

    EZP-29862: Trash button is disabled after using browser back button (#739)
    * EZP-29862: Fix trash button disabled state after using browser back button + fix for radio buttons in immage asset trash modal
    
    * Fix table buttons in Trash

--- src/bundle/Resources/public/js/scripts/button.state.toggle.js
+++ src/bundle/Resources/public/js/scripts/button.state.toggle.js
@@ -1,15 +1,16 @@
-(function (global, doc) {
+(function(global, doc) {
     const toggleForms = [...doc.querySelectorAll('.ez-toggle-btn-state')];
 
-    toggleForms.forEach(toggleForm => {
+    toggleForms.forEach((toggleForm) => {
         const checkboxes = [...toggleForm.querySelectorAll('.ez-table__cell.ez-table__cell--has-checkbox input[type="checkbox"]')];
         const toggleButtonState = () => {
-            const methodName = checkboxes.some(el => el.checked) ? 'removeAttribute' : 'setAttribute';
+            const isAnythingSelected = checkboxes.some((el) => el.checked);
             const buttonRemove = doc.querySelector(toggleForm.dataset.toggleButtonId);
 
-            buttonRemove[methodName]('disabled', true);
+            buttonRemove.disabled = !isAnythingSelected;
         };
 
-        checkboxes.forEach(checkbox => checkbox.addEventListener('change', toggleButtonState, false));
+        toggleButtonState();
+        checkboxes.forEach((checkbox) => checkbox.addEventListener('change', toggleButtonState, false));
     });
 })(window, document);
