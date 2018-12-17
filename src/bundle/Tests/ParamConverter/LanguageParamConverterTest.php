06e7780028759c7fd8a2cff2bb34d243dac192fc
Adam Wójs <adam.wojs@ez.no>
2018-12-17 10:58:52 +0100

    Added missing cases in \EzSystems\EzPlatformAdminUiBundle\Tests\ParamConverter\LanguageParamConverterTest

--- src/bundle/Tests/ParamConverter/LanguageParamConverterTest.php
+++ src/bundle/Tests/ParamConverter/LanguageParamConverterTest.php
@@ -6,11 +6,12 @@
  */
 namespace EzSystems\EzPlatformAdminUiBundle\Tests\ParamConverter;
 
+use eZ\Publish\API\Repository\Exceptions\NotFoundException;
 use eZ\Publish\API\Repository\LanguageService;
 use eZ\Publish\API\Repository\Values\Content\Language;
 use EzSystems\EzPlatformAdminUiBundle\ParamConverter\LanguageParamConverter;
+use stdClass;
 use Symfony\Component\HttpFoundation\Request;
-use PHPUnit_Framework_MockObject_MockObject as MockObject;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class LanguageParamConverterTest extends AbstractParamConverterTest
@@ -18,20 +19,22 @@ class LanguageParamConverterTest extends AbstractParamConverterTest
     const SUPPORTED_CLASS = Language::class;
     const PARAMETER_NAME = 'language';
 
-    /** @var LanguageParamConverter */
+    /** @var \EzSystems\EzPlatformAdminUiBundle\ParamConverter\LanguageParamConverter */
     protected $converter;
 
-    /** @var MockObject */
+    /** @var \eZ\Publish\API\Repository\LanguageService|\PHPUnit\Framework\MockObject\MockObject */
     protected $serviceMock;
 
     public function setUp()
     {
         $this->serviceMock = $this->createMock(LanguageService::class);
-
         $this->converter = new LanguageParamConverter($this->serviceMock);
     }
 
-    public function testApply()
+    /**
+     * @covers \EzSystems\EzPlatformAdminUiBundle\ParamConverter\LanguageParamConverter::apply
+     */
+    public function testApplyForLanguageId()
     {
         $languageId = 42;
         $valueObject = $this->createMock(Language::class);
@@ -54,20 +57,48 @@ class LanguageParamConverterTest extends AbstractParamConverterTest
         $this->assertInstanceOf(self::SUPPORTED_CLASS, $request->attributes->get(self::PARAMETER_NAME));
     }
 
-    public function testApplyWithWrongAttribute()
+    /**
+     * @covers \EzSystems\EzPlatformAdminUiBundle\ParamConverter\LanguageParamConverter::apply
+     */
+    public function testApplyForLanguageCode()
     {
-        $requestAttributes = [
-            LanguageParamConverter::PARAMETER_LANGUAGE_ID => null,
-        ];
+        $languageCode = 'eng-GB';
+        $valueObject = $this->createMock(Language::class);
 
-        $request = new Request([], [], $requestAttributes);
+        $this->serviceMock
+            ->expects($this->once())
+            ->method('loadLanguage')
+            ->with($languageCode)
+            ->willReturn($valueObject);
+
+        $request = new Request([], [], [
+            LanguageParamConverter::PARAMETER_LANGUAGE_CODE => $languageCode,
+        ]);
+
+        $config = $this->createConfiguration(self::SUPPORTED_CLASS, self::PARAMETER_NAME);
+
+        $this->converter->apply($request, $config);
+
+        $this->assertInstanceOf(self::SUPPORTED_CLASS, $request->attributes->get(self::PARAMETER_NAME));
+    }
+
+    /**
+     * @covers \EzSystems\EzPlatformAdminUiBundle\ParamConverter\LanguageParamConverter::apply
+     * @dataProvider dataProviderForApplyWithWrongAttribute
+     */
+    public function testApplyWithWrongAttribute(array $attributes)
+    {
+        $request = new Request([], [], $attributes);
         $config = $this->createConfiguration(self::SUPPORTED_CLASS, self::PARAMETER_NAME);
 
         $this->assertFalse($this->converter->apply($request, $config));
         $this->assertNull($request->attributes->get(self::PARAMETER_NAME));
     }
 
-    public function testApplyWhenNotFound()
+    /**
+     * @covers \EzSystems\EzPlatformAdminUiBundle\ParamConverter\LanguageParamConverter::apply
+     */
+    public function testApplyWithNonExistingLanguageId()
     {
         $languageId = 42;
 
@@ -78,7 +109,7 @@ class LanguageParamConverterTest extends AbstractParamConverterTest
             ->expects($this->once())
             ->method('loadLanguageById')
             ->with($languageId)
-            ->willReturn(null);
+            ->willThrowException($this->createMock(NotFoundException::class));
 
         $requestAttributes = [
             LanguageParamConverter::PARAMETER_LANGUAGE_ID => $languageId,
@@ -88,5 +119,63 @@ class LanguageParamConverterTest extends AbstractParamConverterTest
         $config = $this->createConfiguration(self::SUPPORTED_CLASS, self::PARAMETER_NAME);
 
         $this->converter->apply($request, $config);
+    }
+
+    /**
+     * @covers \EzSystems\EzPlatformAdminUiBundle\ParamConverter\LanguageParamConverter::apply
+     */
+    public function testApplyWithNonExistingLanguageCode()
+    {
+        $languageCode = 'eng-Gb';
+
+        $this->expectException(NotFoundHttpException::class);
+        $this->expectExceptionMessage(sprintf('Language %s not found!', $languageCode));
+
+        $this->serviceMock
+            ->expects($this->once())
+            ->method('loadLanguage')
+            ->with($languageCode)
+            ->willThrowException($this->createMock(NotFoundException::class));
+
+        $requestAttributes = [
+            LanguageParamConverter::PARAMETER_LANGUAGE_CODE => $languageCode,
+        ];
+
+        $request = new Request([], [], $requestAttributes);
+        $config = $this->createConfiguration(self::SUPPORTED_CLASS, self::PARAMETER_NAME);
+
+        $this->converter->apply($request, $config);
+    }
+
+    /**
+     * @covers \EzSystems\EzPlatformAdminUiBundle\ParamConverter\LanguageParamConverter::supports
+     * @dataProvider dataProviderForSupport
+     */
+    public function testSupport(string $class, bool $expected)
+    {
+        $this->assertEquals($expected, $this->converter->supports($this->createConfiguration($class)));
+    }
+
+    public function dataProviderForSupport(): array
+    {
+        return [
+            [self::SUPPORTED_CLASS, true],
+            [stdClass::class, false],
+        ];
+    }
+
+    public function dataProviderForApplyWithWrongAttribute(): array
+    {
+        return [
+            [
+                [LanguageParamConverter::PARAMETER_LANGUAGE_ID => null],
+            ],
+            [
+                [LanguageParamConverter::PARAMETER_LANGUAGE_CODE => null],
+            ],
+            [
+                [],
+            ],
+        ];
     }
 }
