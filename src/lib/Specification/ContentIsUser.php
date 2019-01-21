adc6ddc934011912734a7ccab15ac57db467880a
André R <andre.romcke@gmail.com>
2019-01-21 16:06:22 +0100

    EZP-29821: Reduce API/SPI lookups in Admin UI (#778)
    * EZP-29821: Reduce SPI lookups in Admin UI
    
    * Change to always use 2.4 API for userService->isUser()
    
    * Fix CS
    
    * Remove extra _types data from ContentTypes as it breaks UDW and should rather be extracted on JS side

--- src/lib/Specification/ContentIsUser.php
+++ src/lib/Specification/ContentIsUser.php
@@ -10,7 +10,6 @@ namespace EzSystems\EzPlatformAdminUi\Specification;
 
 use eZ\Publish\API\Repository\Values\Content\Content;
 use eZ\Publish\API\Repository\UserService;
-use eZ\Publish\API\Repository\Exceptions\NotFoundException;
 
 class ContentIsUser implements ContentSpecification
 {
@@ -34,12 +33,6 @@ class ContentIsUser implements ContentSpecification
      */
     public function isSatisfiedBy(Content $content): bool
     {
-        try {
-            $this->userService->loadUser($content->id);
-
-            return true;
-        } catch (NotFoundException $e) {
-            return false;
-        }
+        return $this->userService->isUser($content);
     }
 }
