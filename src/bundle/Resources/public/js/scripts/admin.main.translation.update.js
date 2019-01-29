5039fdb5009850935174604ea19ef1d5940cf4ca
Pawel Bujakowski <pawelbujakowski@gmail.com>
2019-01-29 14:35:51 +0100

    fixes cs

--- src/bundle/Resources/public/js/scripts/admin.main.translation.update.js
+++ src/bundle/Resources/public/js/scripts/admin.main.translation.update.js
@@ -3,7 +3,7 @@
         doc.querySelector('#main_translation_update_language_code').value = event.target.value;
         doc.querySelector('form[name="main_translation_update"]').submit();
     };
-
     const inputs = doc.querySelectorAll('input[name="main_translation_update"]');
+
     inputs.forEach((input) => input.addEventListener('change', updateMainTranslation, false));
 })(window, document);
