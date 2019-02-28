40fbb512632b8754b699392422cf180e35b10403
Jakub Brzegowski <symfiz@gmail.com>
2019-02-28 09:07:35 +0100

    Add debounce

--- src/bundle/Resources/public/js/scripts/admin.location.content.tree.js
+++ src/bundle/Resources/public/js/scripts/admin.location.content.tree.js
@@ -2,11 +2,13 @@
     const KEY_CONTENT_TREE_EXPANDED = 'ez-content-tree-expanded';
     const CLASS_CONTENT_TREE_EXPANDED = 'ez-content-tree-container--expanded';
     const CLASS_BTN_CONTENT_TREE_EXPANDED = 'ez-btn--content-tree-expanded';
+    const VIEWPORT_CHANGE_TIMEOUT = 50;
     const token = doc.querySelector('meta[name="CSRF-Token"]').content;
     const siteaccess = doc.querySelector('meta[name="SiteAccess"]').content;
     const contentTreeContainer = doc.querySelector('.ez-content-tree-container');
     const contentTreeWrapper = doc.querySelector('.ez-content-tree-container__wrapper');
     const btn = doc.querySelector('.ez-btn--toggle-content-tree');
+    let onViewportChangeTimeout = null;
     const toggleContentTreePanel = () => {
         contentTreeContainer.classList.toggle(CLASS_CONTENT_TREE_EXPANDED);
         btn.classList.toggle(CLASS_BTN_CONTENT_TREE_EXPANDED);
@@ -17,6 +19,15 @@
         const height = Math.min(window.innerHeight - contentTreeContainer.getBoundingClientRect().top, window.innerHeight);
 
         contentTreeWrapper.style.height = `${height}px`;
+        onViewportChangeTimeout = null;
+    };
+    const handleViewportChange = () => {
+        if (onViewportChangeTimeout) {
+            return;
+        }
+
+        window.clearTimeout(onViewportChangeTimeout);
+        onViewportChangeTimeout = window.setTimeout(updateContentTreeWrapperHeight, VIEWPORT_CHANGE_TIMEOUT);
     };
 
     ReactDOM.render(
@@ -36,6 +47,6 @@
 
     updateContentTreeWrapperHeight();
 
-    doc.addEventListener('scroll', updateContentTreeWrapperHeight, false);
-    window.addEventListener('resize', updateContentTreeWrapperHeight, false);
+    doc.addEventListener('scroll', handleViewportChange, false);
+    window.addEventListener('resize', handleViewportChange, false);
 })(window.document, window.React, window.ReactDOM, window.eZ, window.localStorage);
