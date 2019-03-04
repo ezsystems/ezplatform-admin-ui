3f2bd9f1981a847b6b0f4cd7b6554e02a9d9cebd
Adam Wójs <adam@wojs.pl>
2019-03-04 11:28:45 +0100

    EZP-30184: As an Editor I want to collapse/expand content preview (#860)
    * EZP-30184: As an Editor I want to collapse/expand content preview
    
    * fixup! EZP-30184: As an Editor I want to collapse/expand content preview
    
    * fixup! EZP-30184: As an Editor I want to collapse/expand content preview

---
+++ src/bundle/Resources/public/js/scripts/admin.location.tooglecontentpreview.js
@@ -0,0 +1,21 @@
+(function(doc, localStorage, $) {
+    const CONTENT_PREVIEW_COLLAPSE_SELECTOR = '.ez-content-preview-collapse';
+    const CONTENT_PREVIEW_TOGGLE_STATE_KEY = 'ez-content-preview-collapsed';
+    const getContentPreviewToggleState = () => {
+        const value = localStorage.getItem(CONTENT_PREVIEW_TOGGLE_STATE_KEY);
+
+        return !!JSON.parse(value);
+    };
+    const setContentPreviewToggleState = (value) => {
+        localStorage.setItem(CONTENT_PREVIEW_TOGGLE_STATE_KEY, value);
+    };
+
+    doc.querySelectorAll(CONTENT_PREVIEW_COLLAPSE_SELECTOR).forEach((collapsable) => {
+        collapsable = $(collapsable).collapse({
+            toggle: getContentPreviewToggleState(),
+        });
+
+        collapsable.on('hide.bs.collapse', () => setContentPreviewToggleState(true));
+        collapsable.on('show.bs.collapse', () => setContentPreviewToggleState(false));
+    });
+})(document, window.localStorage, window.jQuery);
