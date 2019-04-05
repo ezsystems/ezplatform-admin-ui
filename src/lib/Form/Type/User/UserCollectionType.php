e44b1328644af36a17397442404a34ef51b6e817
Jakub Brzegowski <symfiz@gmail.com>
2019-04-05 14:25:46 +0200

    EZP-30022: Add UDW picker in assign roles view - users/groups

---
+++ src/lib/Form/Type/User/UserCollectionType.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUi\Form\Type\User;
+
+use eZ\Publish\API\Repository\UserService;
+use EzSystems\EzPlatformAdminUi\Form\DataTransformer\UserCollectionTransformer;
+use EzSystems\EzPlatformAdminUi\Form\DataTransformer\UserTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class UserCollectionType extends AbstractType
+{
+    /** @var \eZ\Publish\API\Repository\UserService */
+    protected $userService;
+
+    /**
+     * @param \eZ\Publish\API\Repository\UserService $userService
+     */
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addViewTransformer(new UserCollectionTransformer($this->userService));
+    }
+
+    public function getParent(): ?string
+    {
+        return HiddenType::class;
+    }
+}
