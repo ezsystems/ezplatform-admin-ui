5b52b2b87ede19bfa4015374f8a0e837e3e57b00
Pawel Bujakowski <pawelbujakowski@gmail.com>
2019-04-17 13:45:43 +0200

    split templates, change submit selector

--- src/bundle/Controller/ContentOnTheFlyController.php
+++ src/bundle/Controller/ContentOnTheFlyController.php
@@ -154,7 +154,7 @@ class ContentOnTheFlyController extends Controller
             }
         }
 
-        return new ContentCreateOnTheFlyView('@ezdesign/content/content_on_the_fly/content_create_on_the_fly.html.twig', [
+        return new ContentCreateOnTheFlyView('@ezdesign/content/content_on_the_fly/user_create_on_the_fly.html.twig', [
             'form' => $form->createView(),
             'language' => $language,
             'contentType' => $contentType,
