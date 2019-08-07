4ee5c383c9bcd69408ef82af146aea7558b94ac0
Nattfarinn <nattfarinn@gmail.com>
2019-08-07 09:25:21 +0200

    add: Sections AdminUI

---
+++ src/lib/UI/Config/Provider/Sections.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUi\UI\Config\Provider;
+
+use eZ\Publish\API\Repository\SectionService;
+use EzSystems\EzPlatformAdminUi\UI\Config\ProviderInterface;
+
+/**
+ * Provides information about sections.
+ */
+class Sections implements ProviderInterface
+{
+    /** @var \eZ\Publish\API\Repository\SectionService */
+    private $sectionService;
+
+    public function __construct(
+        SectionService $sectionService
+    ) {
+        $this->sectionService = $sectionService;
+    }
+
+    public function getConfig(): array
+    {
+        $sections = $this->sectionService->loadSections();
+        $config = [];
+
+        /** @var \eZ\Publish\API\Repository\Values\Content\Section $section */
+        foreach ($sections as $section) {
+            $config[$section->identifier] = $section->name;
+        }
+
+        return $config;
+    }
+}
