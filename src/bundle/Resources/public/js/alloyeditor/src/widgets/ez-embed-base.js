e00bb6a3f6d146f3e741b91b0222f89907d4cc11
Serhey Dolgushev <dolgushev.serhey@gmail.com>
2019-08-14 15:24:50 +0300

    EZP-30864: Handle removed content which is embed #2

--- src/bundle/Resources/public/js/alloyeditor/src/widgets/ez-embed-base.js
+++ src/bundle/Resources/public/js/alloyeditor/src/widgets/ez-embed-base.js
@@ -172,8 +172,8 @@ const embedBaseDefinition = {
     handleContentLoaded: function(hits) {
         if (hits.View.Result.searchHits.searchHit.length === 0) {
             const title = Translator.trans(/*@Desc("Removed")*/ 'removed_content.label', {}, 'alloy_editor');
-            //this.renderEmbedPreview(title);
-            //return;
+            this.renderEmbedPreview(title);
+            return;
         }
 
         const isEmbedImage = this.element.$.classList.contains(IMAGE_TYPE_CLASS);
