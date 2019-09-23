b43d8fa3f5b4a426e9e0a9f033fb08643d8ea6af
Jakub Brzegowski <symfiz@gmail.com>
2019-09-23 15:19:29 +0200

    Pass whole icon_url instead of icon id

--- src/lib/Menu/UserMenuBuilder.php
+++ src/lib/Menu/UserMenuBuilder.php
@@ -91,7 +91,7 @@ class UserMenuBuilder extends AbstractBuilder implements TranslationContainerInt
                 $this->createMenuItem(self::ITEM_BOOKMARK, [
                     'route' => 'ezplatform.bookmark.list',
                     'extras' => [
-                        'icon' => 'bookmark-manager',
+                        'icon_url' => '/bundles/ezplatformadminui/img/ez-icons.svg#bookmark-manager',
                         'orderNumber' => 20,
                     ], ])
             );
@@ -101,7 +101,7 @@ class UserMenuBuilder extends AbstractBuilder implements TranslationContainerInt
                     $this->createMenuItem(self::ITEM_DRAFTS, [
                         'route' => 'ezplatform.content_draft.list',
                         'extras' => [
-                            'icon' => 'content-draft',
+                            'icon_url' => '/bundles/ezplatformadminui/img/ez-icons.svg#content-draft',
                             'orderNumber' => 30,
                         ],
                     ])
@@ -112,14 +112,14 @@ class UserMenuBuilder extends AbstractBuilder implements TranslationContainerInt
                 $this->createMenuItem(self::ITEM_USER_SETTINGS, [
                     'route' => 'ezplatform.user_settings.list',
                     'extras' => [
-                        'icon' => 'user',
+                        'icon_url' => '/bundles/ezplatformadminui/img/ez-icons.svg#user',
                         'orderNumber' => 50,
                     ], ])
             );
 
             $menu->addChild(
                 $this->createMenuItem(self::ITEM_LOGOUT, ['route' => 'logout', 'extras' => [
-                    'icon' => 'logout',
+                    'icon_url' => '/bundles/ezplatformadminui/img/ez-icons.svg#logout',
                     'orderNumber' => 60,
                 ]])
             );
