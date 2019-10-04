863fd80903e33fae0898d99549513290603068d8
Mikołaj Adamczyk <mikadamczyk@gmail.com>
2019-10-04 15:29:47 +0200

    Fixed selection of proper tab when using pagination with the embedded content

--- src/bundle/Resources/public/js/scripts/admin.location.tab.js
+++ src/bundle/Resources/public/js/scripts/admin.location.tab.js
@@ -1,5 +1,20 @@
 (function (global, doc, $) {
-    $('.ez-tabs a[href="#' + global.location.hash.split('#')[1] + '"]').tab('show');
+    const location = global.location;
+
+    /**
+     * Returns tab identifier from hash or url parameter
+     *
+     * @function createContentTypeDataMap
+     * @param {Location} location
+     * @returns {string}
+     */
+    const getTabId = (location) => {
+        return location.hash.split('#')[1]
+            || new URL(location.href).searchParams.get("_fragment")
+            || '';
+    };
+
+    $('.ez-tabs a[href="#' + getTabId(location) + '"]').tab('show');
 
     // Change hash for page-reload
     $('.ez-tabs a').on('shown.bs.tab', function (e) {
