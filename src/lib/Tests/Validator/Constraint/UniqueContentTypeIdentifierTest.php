1fc4d5f46a7567da4904633457f43396a8b8e450
Maciej Kobus <maciej.kobus@ez.no>
2019-10-28 12:00:56 +0100

    EZP-31008: Fixed code style

--- src/lib/Tests/Validator/Constraint/UniqueContentTypeIdentifierTest.php
+++ src/lib/Tests/Validator/Constraint/UniqueContentTypeIdentifierTest.php
@@ -4,7 +4,6 @@
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
-
 namespace EzSystems\EzPlatformAdminUi\Tests\Validator\Constraint;
 
 use EzSystems\EzPlatformAdminUi\Validator\Constraints\UniqueContentTypeIdentifier;
