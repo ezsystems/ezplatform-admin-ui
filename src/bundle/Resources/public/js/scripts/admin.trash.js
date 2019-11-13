515f263b0cfc870b1af71a8216571cfd2ab75cdc
Dawid Parafiński <dawid.parafinski@ez.no>
2019-11-13 14:28:06 +0100

    EZP-30949: fixed redundant confirm checkbox state

--- src/bundle/Resources/public/js/scripts/admin.trash.js
+++ src/bundle/Resources/public/js/scripts/admin.trash.js
@@ -3,12 +3,10 @@
     const submitButton = form.querySelector('button[type="submit"]');
     const allOptions = form.querySelectorAll('.ez-modal__trash-option');
     const confirmCheckbox = form.querySelector('input[name="location_trash[confirm][]"]');
-
     const enableButton = (button) => {
         button.disabled = false;
         button.classList.remove('disabled');
     };
-
     const disableButton = (button) => {
         button.disabled = true;
         button.classList.add('disabled');
@@ -22,13 +20,13 @@
 
     const toggleSubmitButton = () => {
         const areAllOptionsChecked = [...allOptions].every((option) => {
-            const inputs = [...option.querySelectorAll('input'), confirmCheckbox];
+            const inputs = [...option.querySelectorAll('input')];
             const isInputChecked = (input) => input.checked;
 
             return inputs.length === 0 || inputs.some(isInputChecked);
         });
 
-        areAllOptionsChecked ? enableButton(submitButton) : disableButton(submitButton);
+        areAllOptionsChecked && confirmCheckbox.checked ? enableButton(submitButton) : disableButton(submitButton);
     };
 
     form.addEventListener('change', toggleSubmitButton, false);
