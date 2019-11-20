c309af7ee8367fa008e49c152961ab82cf7e32f9
DominikaK <dominika.kurek@ez.no>
2019-11-20 08:38:20 +0100

    Apply suggestions from code review
    Co-Authored-By: Michal Milc <48127225+michalmilc90@users.noreply.github.com>

--- src/lib/Behat/PageObject/PlatformPageObjectFactory.php
+++ src/lib/Behat/PageObject/PlatformPageObjectFactory.php
@@ -72,7 +72,7 @@ class PlatformPageObjectFactory extends PageObjectFactory
             case FolderPreview::CONTENT_TYPE:
                 return FolderPreview::PAGE_NAME;
             default:
-                throw new \InvalidArgumentException(sprintf('Unrecognised preview for Content Type: %s', $contentType));
+                throw new \InvalidArgumentException(sprintf('Unrecognized preview for Content Type: %s', $contentType));
         }
     }
 }
