afa887cbf02b2ab3dd31cbd5d3e76cb1f4508f07
Mateusz Debinski <mateusz.debinski@ez.no>
2020-01-03 12:22:37 +0100

    EZEE-2904: Can't delete Custom URL Alias for Form

--- src/bundle/Resources/public/js/scripts/button.state.toggle.js
+++ src/bundle/Resources/public/js/scripts/button.state.toggle.js
@@ -1,21 +1,27 @@
-(function(global, doc) {
-    const toggleForms = [...doc.querySelectorAll('.ez-toggle-btn-state')];
+(function (global, doc) {
+    doc.addEventListener(
+        'DOMContentLoaded',
+        () => {
+            const toggleForms = [...doc.querySelectorAll('.ez-toggle-btn-state')];
 
-    toggleForms.forEach((toggleForm) => {
-        const checkboxes = [...toggleForm.querySelectorAll('.ez-table__cell.ez-table__cell--has-checkbox input[type="checkbox"]')];
-        const buttonRemove = doc.querySelector(toggleForm.dataset.toggleButtonId);
+            toggleForms.forEach((toggleForm) => {
+                const checkboxes = [...toggleForm.querySelectorAll('.ez-table__cell.ez-table__cell--has-checkbox input[type="checkbox"]')];
+                const buttonRemove = doc.querySelector(toggleForm.dataset.toggleButtonId);
 
-        if (!buttonRemove) {
-            return;
-        }
+                if (!buttonRemove) {
+                    return;
+                }
 
-        const toggleButtonState = () => {
-            const isAnythingSelected = checkboxes.some((el) => el.checked);
+                const toggleButtonState = () => {
+                    const isAnythingSelected = checkboxes.some((el) => el.checked);
 
-            buttonRemove.disabled = !isAnythingSelected;
-        };
+                    buttonRemove.disabled = !isAnythingSelected;
+                };
 
-        toggleButtonState();
-        checkboxes.forEach((checkbox) => checkbox.addEventListener('change', toggleButtonState, false));
-    });
+                toggleButtonState();
+                checkboxes.forEach((checkbox) => checkbox.addEventListener('change', toggleButtonState, false));
+            });
+        },
+        false
+    );
 })(window, document);
