0a69030aa781302fdcefb244a636b57ff4e01f6f
Lukasz Ostafin <lukasz.ostafin@ez.no>
2020-01-10 13:54:43 +0100

    Added changes after code review

--- src/bundle/Resources/public/js/scripts/core/custom.tooltip.js
+++ src/bundle/Resources/public/js/scripts/core/custom.tooltip.js
@@ -1,22 +1,28 @@
-(function(doc) {
-    jQuery(document).ready(() => {
-        const tooltipsNode = doc.querySelectorAll('[data-toggle="tooltip"]');
-        let delay, extraClass;
+(function(global, doc, eZ, $) {
+    eZ.helpers.tooltips = {
+        parse: () => {
+            const tooltipsNode = doc.querySelectorAll('[title]');
+            let delay, extraClasses;
 
-        for (tooltipNode of tooltipsNode) {
-            delay = {
-                show: tooltipNode.getAttribute('data-delay-show') || 150,
-                hide: tooltipNode.getAttribute('data-delay-hide') || 75,
-            };
-            extraClass = tooltipNode.getAttribute('data-extra-class') || '';
+            for (tooltipNode of tooltipsNode) {
+                if (tooltipNode.title.length) {
+                    delay = {
+                        show: tooltipNode.getAttribute('data-delay-show') || 150,
+                        hide: tooltipNode.getAttribute('data-delay-hide') || 75,
+                    };
+                    extraClasses = tooltipNode.getAttribute('data-extra-classes') || '';
 
-            jQuery(tooltipNode).tooltip({
-                delay: delay,
-                template: `<div class="tooltip ez-tooltip ${extraClass}">
-                                <div class="arrow ez-tooltip__arrow"></div>
-                                <div class="tooltip-inner ez-tooltip__inner"></div>
-                            </div>`,
-            });
-        }
-    });
-})(window.document);
+                    $(tooltipNode).tooltip({
+                        delay,
+                        template: `<div class="tooltip ez-tooltip ${extraClasses}">
+                                            <div class="arrow ez-tooltip__arrow"></div>
+                                            <div class="tooltip-inner ez-tooltip__inner"></div>
+                                        </div>`,
+                    });
+                }
+            }
+        },
+    };
+
+    eZ.helpers.tooltips.parse();
+})(window, window.document, window.eZ, window.jQuery);
