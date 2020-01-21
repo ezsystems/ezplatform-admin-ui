8728a0b42a94007932c992e4f4c4f51d798859c8
mateuszdebinski <58430570+mateuszdebinski@users.noreply.github.com>
2020-01-21 10:43:22 +0100

    EZEE-2914: Custom inline style does not update textarea after click on (#1192)

--- src/bundle/Resources/encore/ez.js.config.js
+++ src/bundle/Resources/encore/ez.js.config.js
@@ -46,6 +46,7 @@ const alloyEditor = [
     path.resolve(__dirname, '../public/js/alloyeditor/src/buttons/ez-btn-underline.js'),
     path.resolve(__dirname, '../public/js/alloyeditor/src/buttons/ez-btn-subscript.js'),
     path.resolve(__dirname, '../public/js/alloyeditor/src/buttons/ez-btn-superscript.js'),
+    path.resolve(__dirname, '../public/js/alloyeditor/src/buttons/ez-btn-styleslistitem.js'),
     path.resolve(__dirname, '../public/js/alloyeditor/src/buttons/ez-btn-quote.js'),
     path.resolve(__dirname, '../public/js/alloyeditor/src/buttons/ez-btn-strike.js'),
     path.resolve(__dirname, '../public/js/alloyeditor/src/buttons/ez-btn-link.js'),
