03f06596314793cdb41371d7abd6e42fd08fece2
Marek Nocoń <marek.nocon@ez.no>
2020-02-19 11:55:57 +0100

    [Behat] Refatored Map Location test to give OpenStreetMap time to respond

--- src/lib/Behat/PageElement/Fields/MapLocation.php
+++ src/lib/Behat/PageElement/Fields/MapLocation.php
@@ -4,6 +4,8 @@
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
+
 namespace EzSystems\EzPlatformAdminUi\Behat\PageElement\Fields;
 
 use EzSystems\EzPlatformAdminUi\Behat\Helper\UtilityContext;
@@ -14,38 +16,38 @@ class MapLocation extends EzFieldElement
     /** @var string Name by which Element is recognised */
     public const ELEMENT_NAME = 'Map location';
 
+    private const OPEN_STREET_MAP_TIMEOUT = 20;
+
     public function __construct(UtilityContext $context, string $locator, string $label)
     {
         parent::__construct($context, $locator, $label);
         $this->fields['latitude'] = '#ezrepoforms_content_edit_fieldsData_ezgmaplocation_value_latitude';
         $this->fields['longitude'] = '#ezrepoforms_content_edit_fieldsData_ezgmaplocation_value_longitude';
         $this->fields['address'] = '#ezrepoforms_content_edit_fieldsData_ezgmaplocation_value_address';
+        $this->fields['searchButton'] = '.btn--search-by-address';
     }
 
     public function setValue(array $parameters): void
     {
-        $this->setSpecificCoordinate('latitude', $parameters['latitude']);
-        $this->setSpecificCoordinate('longitude', $parameters['longitude']);
         $this->setSpecificCoordinate('address', $parameters['address']);
-    }
+        $this->context->findElement($this->fields['searchButton'])->click();
 
-    public function setSpecificCoordinate(string $coordinateName, string $value): void
-    {
-        $fieldInput = $this->context->findElement(
-            sprintf('%s %s', $this->fields['fieldContainer'], $this->fields[$coordinateName])
-        );
+        // wait until OpenStreetMap respondes with data
+        $expectedLongitude = $parameters['longitude'];
+        $expectedLatitude = $parameters['latitude'];
 
-        Assert::assertNotNull($fieldInput, sprintf('Input %s for field %s not found.', $coordinateName, $this->label));
+        $this->context->waitUntil(self::OPEN_STREET_MAP_TIMEOUT, function () use ($expectedLatitude, $expectedLongitude) {
+            $currentValue = $this->getValue();
 
-        $fieldInput->setValue('');
-        $fieldInput->setValue($value);
+            return $currentValue['latitude'] === $expectedLatitude && $currentValue['longitude'] === $expectedLongitude;
+        });
     }
 
     public function getValue(): array
     {
         return [
-            'latitude' => $this->getSpecificCoordinate('latitude'),
-            'longitude' => $this->getSpecificCoordinate('longitude'),
+            'latitude' => $this->formatToOneDecimalPlace($this->getSpecificCoordinate('latitude')),
+            'longitude' => $this->formatToOneDecimalPlace($this->getSpecificCoordinate('longitude')),
             'address' => $this->getSpecificCoordinate('address'),
             ];
     }
@@ -87,5 +89,25 @@ class MapLocation extends EzFieldElement
             $this->context->findElement($this->fields['fieldContainer'])->getText(),
             'Field has wrong value'
         );
+    }
+
+    private function setSpecificCoordinate(string $coordinateName, string $value): void
+    {
+        $fieldInput = $this->context->findElement(
+            sprintf('%s %s', $this->fields['fieldContainer'], $this->fields[$coordinateName])
+        );
+
+        Assert::assertNotNull($fieldInput, sprintf('Input %s for field %s not found.', $coordinateName, $this->label));
+
+        $fieldInput->setValue('');
+        $fieldInput->setValue($value);
+    }
+
+    private function formatToOneDecimalPlace(string $value): string
+    {
+        $number = (float) $value;
+        $formattedNumber = number_format($number, 1);
+
+        return sprintf('%f', $formattedNumber);
     }
 }
