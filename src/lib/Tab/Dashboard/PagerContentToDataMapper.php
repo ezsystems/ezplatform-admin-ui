4ab09a01a4637ef04134e949fde5f70e6fbd3fc4
michal-myszka <34576224+michal-myszka@users.noreply.github.com>
2020-02-21 11:00:58 +0100

    Update src/lib/Tab/Dashboard/PagerContentToDataMapper.php
    Co-Authored-By: Adam Wójs <adam.wojs@ez.no>

--- src/lib/Tab/Dashboard/PagerContentToDataMapper.php
+++ src/lib/Tab/Dashboard/PagerContentToDataMapper.php
@@ -107,7 +107,7 @@ class PagerContentToDataMapper
                 'available_translations' => $availableTranslations,
                 'available_enabled_translations' => array_filter(
                     $availableTranslations,
-                    (function (\eZ\Publish\API\Repository\Values\Content\Language $translation) {
+                    (static function (Language $translation): bool {
                         return $translation->enabled;
                     })
                 ),
