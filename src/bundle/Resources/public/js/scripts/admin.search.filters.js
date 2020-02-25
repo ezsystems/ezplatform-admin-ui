8667dbb4ed16b88231fd7795bb4edb1cf2dcd57a
Lukasz Ostafin <lukasz.ostafin@ez.no>
2020-02-25 10:44:07 +0100

    EZP-29948: Improved date range select for search screen

--- src/bundle/Resources/public/js/scripts/admin.search.filters.js
+++ src/bundle/Resources/public/js/scripts/admin.search.filters.js
@@ -1,18 +1,21 @@
 (function(global, doc, eZ, $, flatpickr) {
     let getUsersTimeout;
+    const CLASS_DATE_RANGE = 'ez-filters__range-wrapper';
+    const CLASS_VISIBLE_DATE_RANGE = 'ez-filters__range-wrapper--visible';
     const token = doc.querySelector('meta[name="CSRF-Token"]').content;
     const siteaccess = doc.querySelector('meta[name="SiteAccess"]').content;
     const filterBtn = doc.querySelector('.ez-btn--filter');
     const filters = doc.querySelector('.ez-filters');
     const clearBtn = filters.querySelector('.ez-btn-clear');
     const applyBtn = filters.querySelector('.ez-btn-apply');
-    const selectBtns = doc.querySelectorAll('.ez-btn--select');
-    const dateFields = doc.querySelectorAll('.ez-date-select');
+    const dateFields = doc.querySelectorAll('.ez-filters__range-select');
     const contentTypeSelector = doc.querySelector('.ez-content-type-selector');
     const contentTypeSelect = doc.querySelector('.ez-filters__item--content-type .ez-filters__select');
     const sectionSelect = doc.querySelector('.ez-filters__item--section .ez-filters__select');
     const lastModifiedSelect = doc.querySelector('.ez-filters__item--modified .ez-filters__select');
+    const lastModifiedDateRange = doc.querySelector('.ez-filters__item--modified .ez-filters__range-select');
     const lastCreatedSelect = doc.querySelector('.ez-filters__item--created .ez-filters__select');
+    const lastCreatedDateRange = doc.querySelector('.ez-filters__item--created .ez-filters__range-select');
     const creatorInput = doc.querySelector('.ez-filters__item--creator .ez-filters__input');
     const searchCreatorInput = doc.querySelector('#search_creator');
     const usersList = doc.querySelector('.ez-filters__item--creator .ez-filters__user-list');
@@ -20,17 +23,24 @@
     const listGroupsTitle = doc.querySelectorAll('.ez-content-type-selector__group-title');
     const contentTypeCheckboxes = doc.querySelectorAll('.ez-content-type-selector__item [type="checkbox"]');
     const subtreeInput = doc.querySelector('#search_subtree');
+    const dateConfig = {
+        mode: 'range',
+        locale: {
+            rangeSeparator: ' - ',
+        },
+        formatDate: (date) => eZ.helpers.timezone.formatShortDateTime(date, null),
+    };
     const clearFilters = (event) => {
         event.preventDefault();
 
         const option = contentTypeSelect.querySelector('option');
         const defaultText = option.dataset.default;
-        const lastModifiedModal = doc.querySelector(lastModifiedSelect.dataset.targetSelector);
-        const lastCreatedModal = doc.querySelector(lastCreatedSelect.dataset.targetSelector);
-        const lastModifiedPeriod = doc.querySelector(lastModifiedModal.dataset.periodSelector);
-        const lastModifiedEnd = doc.querySelector(lastModifiedModal.dataset.endSelector);
-        const lastCreatedPeriod = doc.querySelector(lastCreatedModal.dataset.periodSelector);
-        const lastCreatedEnd = doc.querySelector(lastCreatedModal.dataset.endSelector);
+        const lastModifiedDataRange = doc.querySelector(lastModifiedSelect.dataset.targetSelector);
+        const lastCreatedDataRange = doc.querySelector(lastCreatedSelect.dataset.targetSelector);
+        const lastModifiedPeriod = doc.querySelector(lastModifiedDataRange.dataset.periodSelector);
+        const lastModifiedEnd = doc.querySelector(lastModifiedDataRange.dataset.endSelector);
+        const lastCreatedPeriod = doc.querySelector(lastCreatedDataRange.dataset.periodSelector);
+        const lastCreatedEnd = doc.querySelector(lastCreatedDataRange.dataset.endSelector);
 
         option.innerHTML = defaultText;
         contentTypeCheckboxes.forEach((checkbox) => {
@@ -59,10 +69,33 @@
         const contentTypeOption = contentTypeSelect.querySelector('option');
         const isContentTypeSelected = contentTypeOption.innerHTML !== contentTypeOption.dataset.default;
         const isSectionSelected = sectionSelect ? !!sectionSelect.value : false;
-        const isModifiedSelected = !!lastModifiedSelect.value;
-        const isCreatedSelected = !!lastCreatedSelect.value;
         const isCreatorSelected = !!searchCreatorInput.value;
         const isSubtreeSelected = !!subtreeInput.value.trim().length;
+        let isModifiedSelected = !!lastModifiedSelect.value;
+        let isCreatedSelected = !!lastCreatedSelect.value;
+
+        if (lastModifiedSelect.value === 'custom_range') {
+            const lastModifiedWrapper = lastModifiedDateRange.closest(`.${CLASS_DATE_RANGE}`);
+            const { periodSelector, endSelector } = lastModifiedWrapper.dataset;
+            const lastModifiedPeriodValue = doc.querySelector(periodSelector).value;
+            const lastModifiedEndDate = doc.querySelector(endSelector).value;
+
+            if (!lastModifiedPeriodValue || !lastModifiedEndDate) {
+                isModifiedSelected = false;
+            }
+        }
+
+        if (lastCreatedSelect.value === 'custom_range') {
+            const lastCreatedWrapper = lastCreatedDateRange.closest(`.${CLASS_DATE_RANGE}`);
+            const { periodSelector, endSelector } = lastCreatedWrapper.dataset;
+            const lastCreatedPeriodValue = doc.querySelector(periodSelector).value;
+            const lastCreatedEndDate = doc.querySelector(endSelector).value;
+
+            if (!lastCreatedPeriodValue || !lastCreatedEndDate) {
+                isCreatedSelected = false;
+            }
+        }
+
         const isEnabled =
             isContentTypeSelected || isSectionSelected || isModifiedSelected || isCreatedSelected || isCreatorSelected || isSubtreeSelected;
         const methodName = isEnabled ? 'removeAttribute' : 'setAttribute';
@@ -91,19 +124,20 @@
         contentTypeSelector.classList.toggle('ez-content-type-selector--collapsed');
         doc.querySelector('body')[methodName]('click', handleClickOutside, false);
     };
-    const toggleModalVisibility = (event) => {
-        const modal = $(event.target.dataset.targetSelector);
+    const toggleDatesSelectVisibility = (event) => {
+        const datesRangeNode = doc.querySelector(event.target.dataset.targetSelector);
 
         if (event.target.value !== 'custom_range') {
-            doc.querySelector(modal[0].dataset.periodSelector).value = event.target.value;
-            doc.querySelector(modal[0].dataset.endSelector).value = '';
+            doc.querySelector(datesRangeNode.dataset.periodSelector).value = event.target.value;
+            doc.querySelector(datesRangeNode.dataset.endSelector).value = '';
+            datesRangeNode.classList.remove(CLASS_VISIBLE_DATE_RANGE);
 
             toggleDisabledStateOnApplyBtn();
 
             return;
         }
 
-        modal.modal('show');
+        datesRangeNode.classList.add(CLASS_VISIBLE_DATE_RANGE);
     };
     const toggleGroupState = (event) => {
         event.preventDefault();
@@ -120,62 +154,26 @@
 
         toggleDisabledStateOnApplyBtn();
     };
-    const dateConfig = {
-        formatDate: (date) => eZ.helpers.timezone.formatShortDateTime(date, null),
-    };
-    const checkSelectFieldsFilled = (modal) => {
-        const inputs = modal.querySelectorAll('.ez-date-select');
-        const isFilled = [...inputs].every((input) => !!doc.querySelector(input.dataset.targetSelector).value.trim());
-        const methodName = isFilled ? 'removeAttribute' : 'setAttribute';
+    const setSelectedDateRange = (selectedDates, dateString, instance) => {
+        const dateRange = instance.input.closest('.ez-filters__range-wrapper');
 
-        modal.querySelector('.ez-btn--select')[methodName]('disabled', !isFilled);
-    };
-    const setSelectedDateRange = (event) => {
-        const modal = event.target.closest('.ez-modal');
-        const startInput = modal.querySelector('.ez-date-select--start');
-        const targetStartInput = doc.querySelector(startInput.dataset.targetSelector);
-        const endInput = modal.querySelector('.ez-date-select--end');
-        const targetEndInput = doc.querySelector(endInput.dataset.targetSelector);
-        const startDate = parseInt(targetStartInput.value, 10);
-        const endDate = parseInt(targetEndInput.value, 10);
-        const datePeriod = endDate - startDate;
-        const secondsInDay = 60 * 60 * 24;
-        const days = datePeriod / secondsInDay;
+        if (selectedDates.length === 2) {
+            const startDate = getUnixTimestampUTC(selectedDates[0]);
+            const endDate = getUnixTimestampUTC(selectedDates[1]);
+            const secondsInDay = 86400;
+            const days = (endDate - startDate) / secondsInDay;
 
-        doc.querySelector(modal.dataset.periodSelector).value = `P0Y0M${days}D`;
-        doc.querySelector(modal.dataset.endSelector).value = endDate;
+            doc.querySelector(dateRange.dataset.periodSelector).value = `P0Y0M${days}D`;
+            doc.querySelector(dateRange.dataset.endSelector).value = endDate;
+        }
 
         toggleDisabledStateOnApplyBtn();
     };
-    const updateSourceInputValue = (sourceInput, date) => {
-        if (!date.length) {
-            sourceInput.value = '';
-            sourceInput.dispatchEvent(event);
+    const getUnixTimestampUTC = (dateObject) => {
+        date = new Date(Date.UTC(dateObject.getFullYear(), dateObject.getMonth(), dateObject.getDate()));
+        date = Math.floor(date.getTime() / 1000);
 
-            return;
-        }
-
-        date = new Date(date[0]);
-        date = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
-
-        sourceInput.value = Math.floor(date.getTime() / 1000);
-
-        checkSelectFieldsFilled(sourceInput.closest('.ez-modal'));
-    };
-    const initFlatPickr = (field) => {
-        const sourceInput = doc.querySelector(field.dataset.targetSelector);
-        const flatPickrInput = field;
-        let defaultDate;
-
-        if (sourceInput.value) {
-            defaultDate = new Date(sourceInput.value * 1000);
-        }
-
-        flatpickr(flatPickrInput, {
-            ...dateConfig,
-            onChange: updateSourceInputValue.bind(null, sourceInput),
-            defaultDate,
-        });
+        return date;
     };
     const getUsersList = (value) => {
         const body = JSON.stringify({
@@ -212,8 +210,9 @@
             .then((response) => response.json())
             .then(showUsersList);
     };
-    const createUsersListItem = (user) =>
-        `<li data-id="${user._id}" data-name="${user.TranslatedName}" class="ez-filters__user-item">${user.TranslatedName}</li>`;
+    const createUsersListItem = (user) => {
+        return `<li data-id="${user._id}" data-name="${user.TranslatedName}" class="ez-filters__user-item">${user.TranslatedName}</li>`;
+    };
     const showUsersList = (data) => {
         const hits = data.View.Result.searchHits.searchHit;
         const users = hits.reduce((total, hit) => total + createUsersListItem(hit.value.Content), '');
@@ -265,9 +264,18 @@
         usersList.classList.add('ez-filters__user-list--hidden');
         doc.querySelector('body').removeEventListener('click', handleClickOutsideUserList, false);
     };
+    const initFlatPickr = (dateRangePickerNode) => {
+        const { start, end } = dateRangePickerNode.dataset;
+        const defaultDate = start && end ? [start, end] : [];
+
+        flatpickr(dateRangePickerNode, {
+            ...dateConfig,
+            onChange: setSelectedDateRange,
+            defaultDate,
+        });
+    };
 
     dateFields.forEach(initFlatPickr);
-
     filterByContentType();
 
     clearBtn.addEventListener('click', clearFilters, false);
@@ -279,12 +287,11 @@
     }
 
     subtreeInput.addEventListener('change', toggleDisabledStateOnApplyBtn, false);
-    lastModifiedSelect.addEventListener('change', toggleModalVisibility, false);
-    lastCreatedSelect.addEventListener('change', toggleModalVisibility, false);
+    lastModifiedSelect.addEventListener('change', toggleDatesSelectVisibility, false);
+    lastCreatedSelect.addEventListener('change', toggleDatesSelectVisibility, false);
     creatorInput.addEventListener('keyup', handleTyping, false);
     usersList.addEventListener('click', handleSelectUser, false);
     resetCreatorBtn.addEventListener('click', handleResetUser, false);
     listGroupsTitle.forEach((group) => group.addEventListener('click', toggleGroupState, false));
     contentTypeCheckboxes.forEach((checkbox) => checkbox.addEventListener('change', filterByContentType, false));
-    selectBtns.forEach((btn) => btn.addEventListener('click', setSelectedDateRange, false));
 })(window, window.document, window.eZ, window.jQuery, window.flatpickr);
