4e20847765e350132e39d31dfd56bd81d7b1b012
Michał Grabowski <michal.grabowski@ez.no>
2020-03-02 15:37:53 +0100

    EZP-31421: Change class fieldtype-container to toggle-group (#1262)
    * EZP-31421: change class fieldtype-container to toggle-group
    
    * EZP-31421: added event to initialize toggle
    
    * EZP-31421: added prefix to event

---
+++ src/bundle/Resources/public/js/scripts/admin.card.toggle.group.js
@@ -0,0 +1,13 @@
+(function(global, doc) {
+    const togglers = doc.querySelectorAll('.ez-card__body-display-toggler');
+    const toggleFieldTypeView = (event) => {
+        event.preventDefault();
+
+        event.currentTarget.closest('.ez-card--toggle-group').classList.toggle('ez-card--collapsed');
+    };
+    const attachToggleField = (btn) => btn.addEventListener('click', toggleFieldTypeView);
+
+    togglers.forEach((btn) => attachToggleField(btn));
+
+    doc.body.addEventListener('ez-initialize-card-toggle-group', (event) => attachToggleField(event.detail.button));
+})(window, window.document);
