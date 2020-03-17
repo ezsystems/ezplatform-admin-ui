772600a994c72dd0fe34d1884abe10072601da23
Dawid Parafiński <dawid.parafinski@ez.no>
2020-03-17 16:27:47 +0100

    EZP-31494: Added time diff filter for twig

---
+++ src/bundle/Templating/Twig/TimeDiffExtension.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUiBundle\Templating\Twig;
+
+use DateTime;
+use DateTimeInterface;
+use EzSystems\EzPlatformAdminUi\UI\Service\DateTimeFormatter;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class TimeDiffExtension extends AbstractExtension
+{
+    /** @var \EzSystems\EzPlatformAdminUi\UI\Service\DateTimeFormatter */
+    private $dateTimeFormatter;
+
+    public function __construct(DateTimeFormatter $dateTimeFormatter)
+    {
+        $this->dateTimeFormatter = $dateTimeFormatter;
+    }
+
+    public function getFilters()
+    {
+        return [
+            new TwigFilter(
+                'ez_datetime_diff',
+                [$this, 'diff'],
+                ['is_safe' => ['html']]
+            ),
+        ];
+    }
+
+    public function diff(DateTimeInterface $from, ?DateTimeInterface $to = null): string
+    {
+        return $this->dateTimeFormatter->formatDiff($from, $to ?? new DateTime());
+    }
+}
