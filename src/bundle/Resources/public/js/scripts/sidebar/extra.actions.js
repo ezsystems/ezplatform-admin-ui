27c108493e3898f64411bb1531488a1066a3a489
Michał Grabowski <michal.grabowski@ez.no>
2020-03-27 08:43:07 +0100

    EZP-31403: Widget header is not visible (#1306)
    * EZP-31403: added zindex to menu
    
    * EZP-31403: added extra class to whole menu

--- src/bundle/Resources/public/js/scripts/sidebar/extra.actions.js
+++ src/bundle/Resources/public/js/scripts/sidebar/extra.actions.js
@@ -1,8 +1,10 @@
 (function(global, doc) {
     const CLASS_HIDDEN = 'ez-extra-actions--hidden';
+    const CLASS_EXPANDED = 'ez-context-menu--expanded';
     const CLASS_ACTIVE_BUTTON = 'ez-btn--active-button';
     const CLASS_PREVENT_SHOW = 'ez-extra-actions--prevent-show';
     const btns = doc.querySelectorAll('.ez-btn--extra-actions');
+    const menu = doc.querySelector('.bg-secondary.ez-context-menu');
     const haveHiddenPart = (element) => element.classList.contains(CLASS_HIDDEN) && !element.classList.contains(CLASS_PREVENT_SHOW);
 
     btns.forEach((btn) => {
@@ -11,8 +13,10 @@
             () => {
                 const actions = doc.querySelector(`.ez-extra-actions[data-actions="${btn.dataset.actions}"]`);
 
-                const methodNameButton = haveHiddenPart(actions) ? 'add' : 'remove';
-                const methodNameContainer = haveHiddenPart(actions) ? 'remove' : 'add';
+                const isHidden = haveHiddenPart(actions);
+                const methodNameButton = isHidden ? 'add' : 'remove';
+                const methodNameContainer = isHidden ? 'remove' : 'add';
+                const methodNameMenu = isHidden ? 'add' : 'remove';
                 const focusElement = actions.querySelector(btn.dataset.focusElement);
                 const detectClickOutside = (event) => {
                     const isNotButton = event.target !== btn || !btn.contains(event.target);
@@ -22,12 +26,14 @@
                     if (isNotButton && isNotExtraActions && isNotCalendar) {
                         btn.classList.remove(CLASS_ACTIVE_BUTTON);
                         actions.classList.add(CLASS_HIDDEN);
+                        menu.classList.remove(CLASS_EXPANDED);
                         doc.body.removeEventListener('click', detectClickOutside, false);
                     }
                 };
 
                 btn.classList[methodNameButton](CLASS_ACTIVE_BUTTON);
                 actions.classList[methodNameContainer](CLASS_HIDDEN);
+                menu.classList[methodNameMenu](CLASS_EXPANDED);
 
                 if (!actions.classList.contains(CLASS_HIDDEN)) {
                     doc.body.addEventListener('click', detectClickOutside, false);
