d8dc2bad6ee8be9aa2c32a9b8ccf90c5f82d91c7
konradoboza <konrad.oboza@ez.no>
2020-03-27 15:20:42 +0100

    EZP-30881: Cannot add table after list in RTE

--- src/bundle/Resources/public/js/alloyeditor/src/buttons/ez-btn-table.js
+++ src/bundle/Resources/public/js/alloyeditor/src/buttons/ez-btn-table.js
@@ -8,6 +8,32 @@ export default class EzBtnTable extends Component {
     }
 
     /**
+     * Checks if the command is disabled in the current selection.
+     *
+     * @method isDisabled
+     * @return {Boolean} True if the command is disabled, false otherwise.
+     */
+    isDisabled() {
+        const editor = this.props.editor.get('nativeEditor');
+        const predecessors = editor.elementPath().elements;
+        const restrictedPredecessors = ['li'];
+
+        let isDisabled = false;
+
+        predecessors.forEach((predecessor) => {
+            const predecessorName = predecessor.getName();
+
+            if (restrictedPredecessors.includes(predecessorName)) {
+                isDisabled = true;
+
+                return;
+            }
+        });
+
+        return isDisabled;
+    }
+
+    /**
      * Lifecycle. Renders the UI of the button.
      *
      * @method render
@@ -20,9 +46,15 @@ export default class EzBtnTable extends Component {
 
         const label = Translator.trans(/*@Desc("Table")*/ 'table_btn.label', {}, 'alloy_editor');
         const css = 'ae-button ez-btn-ae ez-btn-ae--table';
+        const disabled = this.isDisabled();
 
         return (
-            <button className={css} onClick={this.props.requestExclusive} tabIndex={this.props.tabIndex} title={label}>
+            <button
+                className={css}
+                disabled={disabled}
+                onClick={this.props.requestExclusive}
+                tabIndex={this.props.tabIndex}
+                title={label}>
                 <svg className="ez-icon ez-btn-ae__icon">
                     <use xlinkHref="/bundles/ezplatformadminui/img/ez-icons.svg#table-add" />
                 </svg>
