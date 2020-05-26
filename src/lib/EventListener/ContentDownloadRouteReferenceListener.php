336313a1c3034af58166d3a85edbc1de7ac9f5b2
Adam Wójs <adam@wojs.pl>
2020-05-26 15:58:33 +0200

    Fixed EZP-31639: mdia and file fields not loaded correctly in edit mode

---
+++ src/lib/EventListener/ContentDownloadRouteReferenceListener.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUi\EventListener;
+
+use eZ\Publish\Core\MVC\Symfony\Event\RouteReferenceGenerationEvent;
+use eZ\Publish\Core\MVC\Symfony\MVCEvents;
+use eZ\Publish\Core\MVC\Symfony\SiteAccess;
+use EzSystems\EzPlatformAdminUi\Specification\SiteAccess\IsAdmin;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Ensures that download urls generated in ezplatform-admin-ui are in the scope of admin siteaccess.
+ */
+final class ContentDownloadRouteReferenceListener implements EventSubscriberInterface
+{
+    public const CONTENT_DOWNLOAD_ROUTE_NAME = 'ez_content_download';
+
+    /** @var array */
+    private $siteAccessGroups;
+
+    public function __construct(array $siteAccessGroups)
+    {
+        $this->siteAccessGroups = $siteAccessGroups;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            MVCEvents::ROUTE_REFERENCE_GENERATION => 'onRouteReferenceGeneration',
+        ];
+    }
+
+    public function onRouteReferenceGeneration(RouteReferenceGenerationEvent $event): void
+    {
+        $routeReference = $event->getRouteReference();
+
+        if ($routeReference->getRoute() != self::CONTENT_DOWNLOAD_ROUTE_NAME) {
+            return;
+        }
+
+        /** @var \eZ\Publish\Core\MVC\Symfony\SiteAccess $siteaccess */
+        $siteaccess = $event->getRequest()->attributes->get('siteaccess');
+        if ($this->isAdminSiteAccess($siteaccess)) {
+            $routeReference->set('siteaccess', $siteaccess->name);
+        }
+    }
+
+    private function isAdminSiteAccess(SiteAccess $siteAccess): bool
+    {
+        return (new IsAdmin($this->siteAccessGroups))->isSatisfiedBy($siteAccess);
+    }
+}
