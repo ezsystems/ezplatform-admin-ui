9c162a21a42d173c00aaa85cb5fa7e87915c55dd
Dawid Parafiński <dawid.parafinski@ez.no>
2020-07-03 13:37:42 +0200

    EZP-31693: CS fix

--- src/lib/View/Filter/AdminSearchViewFilter.php
+++ src/lib/View/Filter/AdminSearchViewFilter.php
@@ -86,6 +86,7 @@ class AdminSearchViewFilter implements EventSubscriberInterface
 
         if (!$this->isAdminSiteAccess($request)) {
             $this->innerFilter->handleSearchForm($event);
+
             return;
         }
 
