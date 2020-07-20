1b2d0b8ace74ac4692319f7ba46673216b69465b
lukasz_ostafin <ostafin.lucas@gmail.com>
2020-07-20 15:34:22 +0200

    After CR

--- src/bundle/Resources/public/js/scripts/helpers/table.helper.js
+++ src/bundle/Resources/public/js/scripts/helpers/table.helper.js
@@ -1,12 +1,11 @@
 (function (global, doc, eZ) {
-    const onChangeHandler = (activeClass) => {
+    const onChangeHandler = (activeClass, event) => {
         const { checked } = event.target;
         const action = checked ? 'add' : 'remove';
         const parentRow = event.target.closest('tr');
 
         parentRow.classList[action](activeClass);
     };
-
     const parseCheckbox = (checkboxSelector, activeClass) => {
         doc.querySelectorAll(checkboxSelector).forEach((checkboxNode) => {
             checkboxNode.addEventListener('change', onChangeHandler.bind(this, activeClass), false);
