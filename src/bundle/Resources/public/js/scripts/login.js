0e3260b77a618d489b70fbeac425119381741db5
lukasz_ostafin <ostafin.lucas@gmail.com>
2020-07-22 09:47:58 +0200

    After CR

--- src/bundle/Resources/public/js/scripts/login.js
+++ src/bundle/Resources/public/js/scripts/login.js
@@ -1,19 +1,17 @@
-(function (global, doc, eZ) {
+(function (global, doc) {
     const passwordInputNode = doc.querySelector('.ez-login__input--password');
     const viewIconNode = doc.querySelector('.ez-login__password-visibility-toggler .ez-icon--view');
     const viewHideIconNode = doc.querySelector('.ez-login__password-visibility-toggler .ez-icon--view-hide');
 
-    doc.querySelector('.ez-login__password-visibility-toggler').addEventListener('click', () => {
-        if (passwordInputNode) {
-            let inputTypeToSet = 'password';
+    doc.querySelector('.ez-login__password-visibility-toggler').addEventListener('click', (event) => {
+        event.preventDefault();
 
-            if (passwordInputNode.type.toLowerCase() === 'password') {
-                inputTypeToSet = 'text';
-            }
+        if (passwordInputNode) {
+            const inputTypeToSet = passwordInputNode.type === 'password' ? 'text' : 'password';
 
             passwordInputNode.type = inputTypeToSet;
             viewIconNode.classList.toggle('d-none');
             viewHideIconNode.classList.toggle('d-none');
         }
     });
-})(window, window.document, window.eZ);
+})(window, window.document);
