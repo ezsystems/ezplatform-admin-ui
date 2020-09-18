2d3247b5cc0333f2844bad77081cc7b4e4aa32f5
Dariusz Szut <dew326@gmail.com>
2020-09-18 11:15:35 +0200

    EZP-31913: Back button is not properly styled in sub-items pagination

--- src/bundle/ui-dev/src/modules/common/pagination/pagination.js
+++ src/bundle/ui-dev/src/modules/common/pagination/pagination.js
@@ -81,7 +81,6 @@ const Pagination = ({ totalCount, itemsPerPage, proximity, activePageIndex, onPa
                 pageIndex={previousPage}
                 label={backLabel}
                 additionalClasses="prev"
-                buttonAdditionalClasses="btn btn-primary"
                 disabled={isFirstPage || paginationDisabled}
                 onPageChange={onPageChange}
             />
@@ -90,7 +89,6 @@ const Pagination = ({ totalCount, itemsPerPage, proximity, activePageIndex, onPa
                 pageIndex={nextPage}
                 label={nextLabel}
                 additionalClasses="next"
-                buttonAdditionalClasses="btn btn-primary"
                 disabled={isLastPage || paginationDisabled}
                 onPageChange={onPageChange}
             />
