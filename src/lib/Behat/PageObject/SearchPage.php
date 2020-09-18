0e71648b6d942d21d209def791c68d59de7971a2
Marek Nocoń <mnocon@users.noreply.github.com>
2020-09-18 09:00:38 +0200

    [Behat] Adapted page title selector to Search redesign

--- src/lib/Behat/PageObject/SearchPage.php
+++ src/lib/Behat/PageObject/SearchPage.php
@@ -21,7 +21,7 @@ class SearchPage extends Page
         parent::__construct($context);
         $this->route = '/search';
         $this->pageTitle = 'Search';
-        $this->pageTitleLocator = '.ez-page-title__content-name';
+        $this->pageTitleLocator = '.ez-search-form > .form-group .ez-label';
         $this->fields = [
             'inputField' => '.ez-search-form #search_query',
             'buttonSearch' => '.ez-btn--search',
