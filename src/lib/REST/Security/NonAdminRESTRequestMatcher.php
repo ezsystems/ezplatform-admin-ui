13104907dad985f6da9c61d7c8b562c973b1c039
Kamil Madejski <kamil.madejski@ez.no>
2020-10-03 17:00:35 +0200

    Renamed matcher to better reflect its purpose

--- src/lib/REST/Security/NonAdminRESTRequestMatcher.php
+++ src/lib/REST/Security/NonAdminRESTRequestMatcher.php
@@ -4,13 +4,15 @@
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
+
 namespace EzSystems\EzPlatformAdminUi\REST\Security;
 
 use EzSystems\EzPlatformAdminUi\Specification\SiteAccess\IsAdmin;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestMatcherInterface;
 
-final class AdminRESTRequestMatcher implements RequestMatcherInterface
+final class NonAdminRESTRequestMatcher implements RequestMatcherInterface
 {
     /** @var string[][] */
     private $siteAccessGroups;
