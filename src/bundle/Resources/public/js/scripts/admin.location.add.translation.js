f987a0176f563de7a601a746c38082aa092ded50
Dariusz Szut <dew326@gmail.com>
2020-10-06 12:28:00 +0200

    EZP-31990: Cannot add translation to Content Type

--- src/bundle/Resources/public/js/scripts/admin.location.add.translation.js
+++ src/bundle/Resources/public/js/scripts/admin.location.add.translation.js
@@ -1,13 +1,27 @@
 (function(global, doc) {
     const SELECTOR_MODAL = '.ez-modal';
 
-    doc.querySelectorAll('.ez-translation__language-wrapper--language').forEach((select) => {
-        select.addEventListener('valueChanged', (event) => {
-            const modal = event.target.closest(SELECTOR_MODAL);
-            const buttonCreate = modal.querySelector('.ez-btn--create-translation');
-            const method = event.target.value ? 'removeAttribute' : 'setAttribute';
+    doc.querySelectorAll('.ez-translation .ez-custom-dropdown__wrapper').forEach((container) => {
+        const dropdown = new eZ.core.CustomDropdown({
+            container,
+            itemsContainer: container.querySelector('.ez-custom-dropdown__items'),
+            sourceInput: doc.querySelector(container.dataset.sourceSelector),
+        });
 
-            buttonCreate[method]('disabled', true);
-        }, false)
+        dropdown.init();
+    });
+
+    doc.querySelectorAll('.ez-translation__language-wrapper--language').forEach((select) => {
+        select.addEventListener(
+            'valueChanged',
+            (event) => {
+                const modal = event.target.closest(SELECTOR_MODAL);
+                const buttonCreate = modal.querySelector('.ez-btn--create-translation');
+                const method = event.target.value ? 'removeAttribute' : 'setAttribute';
+
+                buttonCreate[method]('disabled', true);
+            },
+            false
+        );
     });
 })(window, window.document);
