c80c2db8f4668b9ef4fb0124578491b971a4741b
Maciej Kobus <maciej.kobus@ez.no>
2020-11-13 12:18:35 +0100

    EZP-32164: Exposed icons sets to AdminUI Config

---
+++ src/lib/UI/Config/Provider/IconPaths.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformAdminUi\UI\Config\Provider;
+
+use eZ\Publish\Core\MVC\ConfigResolverInterface;
+use EzSystems\EzPlatformAdminUi\UI\Config\ProviderInterface;
+
+class IconPaths implements ProviderInterface
+{
+    /** @var \eZ\Publish\Core\MVC\ConfigResolverInterface */
+    protected $configResolver;
+
+    public function __construct(ConfigResolverInterface $configResolver)
+    {
+        $this->configResolver = $configResolver;
+    }
+
+    public function getConfig(): array
+    {
+        return [
+            'iconSets' => $this->configResolver->getParameter('assets.icon_sets'),
+            'defaultIconSet' => $this->configResolver->getParameter('assets.default_icon_set'),
+        ];
+    }
+}
