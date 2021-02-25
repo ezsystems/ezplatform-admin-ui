8cfb9caae1dc26f9799872512d01fa393d881f03
lukasz_ostafin <ostafin.lucas@gmail.com>
2021-02-25 13:32:36 +0100

    EC-359: Refator charts

--- src/bundle/Resources/public/js/scripts/core/base.chart.js
+++ src/bundle/Resources/public/js/scripts/core/base.chart.js
@@ -2,52 +2,126 @@
     const IBEXA_WHITE = '#fff';
     const IBEXA_COLOR_BASE = '#e0e0e8';
     const IBEXA_COLOR_BASE_DARK = '#878b90';
+    const defaultOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+            display: false,
+        },
+        tooltips: {
+            enabled: true,
+            mode: 'nearest',
+            cornerRadius: 4,
+            borderWidth: 1,
+            borderColor: IBEXA_COLOR_BASE,
+            titleFontStyle: 'light',
+            titleFontColor: IBEXA_COLOR_BASE_DARK,
+            xPadding: 12,
+            yPadding: 12,
+            backgroundColor: IBEXA_WHITE,
+            callbacks: {
+                labelTextColor: (tooltipItem, chart) => {
+                    return IBEXA_COLOR_BASE_DARK;
+                },
+            },
+        },
+    };
+    const calculateCords = (ctxElement) => {
+        const centerAngle = ctxElement.startAngle + (ctxElement.endAngle - ctxElement.startAngle) / 2;
+        const rangeFromCenter = (ctxElement.outerRadius - ctxElement.innerRadius) / 2 + ctxElement.innerRadius;
+        const x = ctxElement.x + Math.cos(centerAngle) * rangeFromCenter;
+        const y = ctxElement.y + Math.sin(centerAngle) * rangeFromCenter;
+
+        return { x, y };
+    };
+    const dataLabelPlugin = {
+        beforeRender: function(chart) {
+            if (chart.config.options.showDataLabel) {
+                chart.pluginTooltips = [];
+
+                chart.config.data.datasets.forEach(function(dataset, i) {
+                    chart.getDatasetMeta(i).data.forEach(function(sector, j) {
+                        chart.pluginTooltips.push(
+                            new Chart.Tooltip(
+                                {
+                                    _chart: chart.chart,
+                                    _chartInstance: chart,
+                                    _data: chart.data,
+                                    _options: chart.options.tooltips,
+                                    _active: [sector],
+                                },
+                                chart
+                            )
+                        );
+                    });
+                });
+            }
+        },
+        afterDraw: function(chart, easing) {
+            if (chart.config.options.showDataLabel) {
+                const minAngelToShowDataLabel = 0.1;
+
+                Chart.helpers.each(chart.pluginTooltips, function(tooltip, i) {
+                    const ctxActiveDataset = tooltip._active[0];
+                    const { startAngle, endAngle, x, y } = ctxActiveDataset._model;
+                    const ctxActiveDatasetAngel = endAngle - startAngle;
+
+                    if (!ctxActiveDataset.hidden && ctxActiveDatasetAngel >= minAngelToShowDataLabel) {
+                        const dataset = tooltip._data.datasets[0];
+                        const textCords = chart.ctx.measureText(dataset.data[i]);
+                        const boxWidth = textCords.width * 1.5;
+                        const boxHeight = 10;
+                        const cornerRadius = 5;
+                        const cords = calculateCords(ctxActiveDataset._model);
+
+                        tooltip.initialize();
+                        tooltip._options.caretPadding = 20;
+                        tooltip.update();
+
+                        chart.ctx.textAlign = 'center';
+                        chart.ctx.textBaseline = 'middle';
+                        chart.ctx.font = 'normal 12px';
+                        chart.ctx.fillStyle = '#fff';
+                        chart.ctx.fillText(dataset.data[i], cords.x, cords.y);
+
+                        chart.ctx.lineJoin = 'round';
+                        chart.ctx.lineWidth = boxHeight;
+                        chart.ctx.strokeStyle = 'rgba(255, 255, 255, 0.3)';
+
+                        chart.ctx.strokeRect(cords.x - boxWidth / 2, cords.y - boxHeight / 2, boxWidth, boxHeight);
+                    }
+                });
+            }
+        },
+    };
 
     class BaseChart {
-        constructor(data) {
+        constructor(data, options = {}) {
             this.setData(data);
+            this.setOptions(options);
             this.lang = document.documentElement.lang.replace('_', '-'); // TO DO: Get this config from settings
         }
 
         setData(data) {
             this.datasets = data.datasets;
             this.labels = data.labels;
-
-            if (data.summary) {
-                this.summary = data.summary;
-            }
         }
 
-        setCheckboxBackground(checkbox) {
-            const { checkedColor } = checkbox.dataset;
-            const { checked } = checkbox;
-
-            if (checked) {
-                checkbox.style.backgroundColor = checkedColor;
-                checkbox.style.borderColor = checkedColor;
-            } else {
-                checkbox.style.backgroundColor = IBEXA_WHITE;
-                checkbox.style.borderColor = IBEXA_COLOR_BASE_DARK;
-            }
+        setOptions(options) {
+            this.options = {
+                ...defaultOptions,
+                ...options,
+                ...{
+                    animation: {
+                        onComplete: (animation) => this.onCompleteAnimationCallback(animation),
+                    },
+                },
+            };
         }
-
-        setLegendCheckboxes() {}
 
         getType() {}
 
-        getSummaryValue() {}
-
-        getSummaryName(value) {
-            return value.name;
-        }
-
-        getLayoutOptions() {}
-
-        getScaleOptions() {}
-
-        getLegendOptions(chart) {}
-
-        updateSummary() {}
+        callbackAfterRender() {}
 
         updateChartMessageDisplay() {
             const chartBody = this.chart.canvas.closest('.ez-chart__body');
@@ -64,14 +138,16 @@
             this.chart.update();
 
             this.updateChartMessageDisplay();
+            this.callbackAfterRender();
+        }
 
-            if (this.legendContainer) {
-                this.setLegendCheckboxes();
-            }
+        onCompleteAnimationCallback(animation) {
+            const chart = animation.chart;
+            const chartMethod = chart.config.data.datasets.length ? 'remove' : 'add';
+            const chartNode = chart.canvas.closest('.ez-chart');
 
-            if (this.summaryContainer) {
-                this.updateSummary();
-            }
+            chartNode.dispatchEvent(new CustomEvent('ez-chart-animation-complete'));
+            chartNode.classList[chartMethod]('ez-chart--no-data');
         }
 
         render() {
@@ -81,64 +157,14 @@
                     labels: this.labels,
                     datasets: this.datasets,
                 },
-                options: {
-                    responsive: true,
-                    maintainAspectRatio: false,
-                    layout: this.getLayoutOptions(),
-                    elements: {
-                        point: {
-                            radius: 2,
-                        },
-                        line: {
-                            tension: 0,
-                        },
-                    },
-                    legendCallback: (chart) => this.getLegendOptions(chart),
-                    legend: {
-                        display: false,
-                    },
-                    tooltips: {
-                        enabled: true,
-                        mode: 'nearest',
-                        cornerRadius: 4,
-                        borderWidth: 1,
-                        borderColor: IBEXA_COLOR_BASE,
-                        titleFontStyle: 'light',
-                        titleFontColor: IBEXA_COLOR_BASE_DARK,
-                        xPadding: 12,
-                        yPadding: 12,
-                        backgroundColor: IBEXA_WHITE,
-                        callbacks: {
-                            labelTextColor: (tooltipItem, chart) => {
-                                return IBEXA_COLOR_BASE_DARK;
-                            },
-                        },
-                    },
-                    animation: {
-                        onComplete: (animation) => {
-                            const chart = animation.chart;
-                            const chartMethod = chart.config.data.datasets.length ? 'remove' : 'add';
-                            const chartNode = chart.canvas.closest('.ez-chart');
-
-                            chartNode.dispatchEvent(new CustomEvent('ez-chart-animation-complete'));
-                            chartNode.classList[chartMethod]('ez-chart--no-data');
-                        },
-                    },
-                    scales: this.getScaleOptions(),
-                },
+                options: this.options,
             });
 
             this.updateChartMessageDisplay();
-
-            if (this.legendContainer) {
-                this.setLegendCheckboxes();
-            }
-
-            if (this.summaryContainer) {
-                this.updateSummary();
-            }
+            this.callbackAfterRender();
         }
     }
 
     eZ.addConfig('core.BaseChart', BaseChart);
+    Chart.plugins.register(dataLabelPlugin);
 })(window, window.document, window.eZ, window.Chart);
