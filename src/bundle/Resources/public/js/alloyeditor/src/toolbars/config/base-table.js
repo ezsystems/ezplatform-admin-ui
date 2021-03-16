8e7712ac80cffe52e441bc0ace6e1ae9308fa0e7
mateuszdebinski <58430570+mateuszdebinski@users.noreply.github.com>
2021-03-16 12:59:58 +0100

    EZP-31632: Floating table toolbar (#1721)

--- src/bundle/Resources/public/js/alloyeditor/src/toolbars/config/base-table.js
+++ src/bundle/Resources/public/js/alloyeditor/src/toolbars/config/base-table.js
@@ -1,7 +1,9 @@
-import AlloyEditor from 'alloyeditor';
+import EzConfgiFixedBase from './base-fixed';
 
-export default class EzConfigTableBase {
+export default class EzConfigTableBase extends EzConfgiFixedBase {
     constructor(config) {
+        super(config);
+
         this.name = this.getConfigName();
 
         const editAttributesButton = config.attributes[this.name] || config.classes[this.name] ? `${this.name}edit` : '';
@@ -16,12 +18,13 @@ export default class EzConfigTableBase {
             'eztableremove',
             ...config.extraButtons[this.name],
         ];
-
-        this.getArrowBoxClasses = AlloyEditor.SelectionGetArrowBoxClasses.table;
-        this.setPosition = AlloyEditor.SelectionSetPosition.table;
     }
 
     getConfigName() {
         return '';
+    }
+
+    getArrowBoxClasses() {
+        return 'ae-toolbar-floating';
     }
 }
