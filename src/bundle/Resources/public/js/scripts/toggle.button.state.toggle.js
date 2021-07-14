acfde14c7e9eaec5ba5c254d28aaa49086ba7e81
Michał Grabowski <michal.grabowski@ez.no>
2021-07-14 12:42:06 +0200

    IBX-617: After CR

--- src/bundle/Resources/public/js/scripts/toggle.button.state.toggle.js
+++ src/bundle/Resources/public/js/scripts/toggle.button.state.toggle.js
@@ -4,38 +4,43 @@
     const toggleState = (event) => {
         event.preventDefault();
 
-        if (event.currentTarget.classList.contains('ibexa-toggle--is-disabled')) {
+        const toggler = event.currentTarget;
+
+        if (toggler.classList.contains('ibexa-toggle--is-disabled')) {
             return;
         }
 
-        event.currentTarget.classList.toggle('ibexa-toggle--is-checked');
+        const isChecked = toggler.classList.toggle('ibexa-toggle--is-checked');
 
-        const isChecked = event.currentTarget.classList.contains('ibexa-toggle--is-checked');
-        const valueToSet = isChecked ? 1 : 0;
+        if (toggler.classList.contains('ibexa-toggle--radio')) {
+            const valueToSet = isChecked ? 1 : 0;
 
-        if (event.currentTarget.classList.contains('ibexa-toggle--radio')) {
-            event.currentTarget.querySelector(`.form-check input[value="${valueToSet}"]`).checked = true;
+            toggler.querySelector(`.form-check input[value="${valueToSet}"]`).checked = true;
         } else {
-            event.currentTarget.querySelector('.ibexa-toggle__input').checked = isChecked;
+            toggler.querySelector('.ibexa-toggle__input').checked = isChecked;
         }
     };
     const addFocus = (event) => {
         event.preventDefault();
 
-        if (event.currentTarget.classList.contains('ibexa-toggle--is-disabled')) {
+        const toggler = event.currentTarget.closest('.ibexa-toggle');
+
+        if (toggler.classList.contains('ibexa-toggle--is-disabled')) {
             return;
         }
 
-        event.currentTarget.closest('.ibexa-toggle').classList.add('ibexa-toggle--is-focused');
+        toggler.classList.add('ibexa-toggle--is-focused');
     };
     const removeFocus = (event) => {
         event.preventDefault();
 
-        if (event.currentTarget.classList.contains('ibexa-toggle--is-disabled')) {
+        const toggler = event.currentTarget.closest('.ibexa-toggle');
+
+        if (toggler.classList.contains('ibexa-toggle--is-disabled')) {
             return;
         }
 
-        event.currentTarget.closest('.ibexa-toggle').classList.remove('ibexa-toggle--is-focused');
+        toggler.classList.remove('ibexa-toggle--is-focused');
     };
 
     toggleFields.forEach((toggleField) => toggleField.addEventListener('click', toggleState, false));
