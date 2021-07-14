45fe8cecc5fdd3690c8ba36cf92d7893ae256c6f
Jakub Brzegowski <symfiz@gmail.com>
2021-07-14 10:21:55 +0200

    Delete dumb validating mechanism

--- src/bundle/Resources/public/js/scripts/admin.input.text.js
+++ src/bundle/Resources/public/js/scripts/admin.input.text.js
@@ -1,7 +1,5 @@
 (function (global, doc, $, eZ) {
     const textInputClearBtns = doc.querySelectorAll('.ibexa-input-text-wrapper__clear-btn');
-    const textInputsInvalid = doc.querySelectorAll('.ibexa-input--text.is-invalid');
-    const textInputsInvalidOriginalValuesMap = new Map();
     const clearText = (event) => {
         const inputWrapper = event.target.closest('.ibexa-input-text-wrapper');
         const textInput = inputWrapper.querySelector('.ibexa-input--text');
@@ -10,14 +8,6 @@
         textInput.dispatchEvent(new Event('input'));
         textInput.select();
     };
-    const handleInputChange = (event) => {
-        const textInput = event.target;
-        const isValueDifferentFromOriginal = textInput.value !== textInputsInvalidOriginalValuesMap.get(textInput);
 
-        textInput.classList.toggle('is-invalid', !isValueDifferentFromOriginal);
-    };
-
-    textInputsInvalid.forEach((textInputInvalid) => textInputsInvalidOriginalValuesMap.set(textInputInvalid, textInputInvalid.value));
-    textInputsInvalid.forEach((textInput) => textInput.addEventListener('input', handleInputChange, false));
     textInputClearBtns.forEach((clearBtn) => clearBtn.addEventListener('click', clearText, false));
 })(window, window.document, window.jQuery, window.eZ);
