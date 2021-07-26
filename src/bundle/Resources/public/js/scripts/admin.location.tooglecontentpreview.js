41269a526568d83157907b6f12ea4c6ed7ab5de4
Dariusz Szut <dew326@gmail.com>
2021-07-26 09:18:54 +0200

    added bootstrap

--- src/bundle/Resources/public/js/scripts/admin.location.tooglecontentpreview.js
+++ src/bundle/Resources/public/js/scripts/admin.location.tooglecontentpreview.js
@@ -1,4 +1,4 @@
-(function(global, doc, localStorage) {
+(function(global, doc, localStorage, bootstrap) {
     const CONTENT_PREVIEW_COLLAPSE_SELECTOR = '.ez-content-preview-collapse';
     const DEFAULT_CONTENT_PREVIEW_TOGGLE_STATE_KEY = 'ez-content-preview-collapsed';
     const getStateKey = (collapseTarget) => {
@@ -23,4 +23,4 @@
         collapsable.addEventListener('hide.bs.collapse', (event) => setContentPreviewToggleState(event, true));
         collapsable.addEventListener('show.bs.collapse', (event) => setContentPreviewToggleState(event, false));
     });
-})(window, window.document, window.localStorage);
+})(window, window.document, window.localStorage, window.bootstrap);
