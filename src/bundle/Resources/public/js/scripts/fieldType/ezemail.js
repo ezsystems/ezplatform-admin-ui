1618982b22824bb595ac4c7013f89682bcf3da1a
Dariusz Szut <dew326@gmail.com>
2021-08-12 14:53:59 +0200

    IBX-643: Redesigned ezemail

--- src/bundle/Resources/public/js/scripts/fieldType/ezemail.js
+++ src/bundle/Resources/public/js/scripts/fieldType/ezemail.js
@@ -1,6 +1,6 @@
 (function(global, doc, eZ) {
-    const SELECTOR_FIELD = '.ez-field-edit--ezemail';
-    const SELECTOR_ERROR_NODE = '.ez-data-source';
+    const SELECTOR_FIELD = '.ibexa-field-edit--ezemail';
+    const SELECTOR_ERROR_NODE = '.ibexa-form-error';
 
     class EzEmailValidator extends eZ.BaseFieldValidator {
         /**
@@ -17,7 +17,7 @@
             const isEmpty = !input.value.trim();
             const isValid = eZ.errors.emailRegexp.test(input.value);
             const isError = (isRequired && isEmpty) || (!isEmpty && !isValid);
-            const label = input.closest(SELECTOR_FIELD).querySelector('.ez-field-edit__label').innerHTML;
+            const label = input.closest(SELECTOR_FIELD).querySelector('.ibexa-field-edit__label').innerHTML;
             const result = { isError };
 
             if (isRequired && isEmpty) {
@@ -35,7 +35,7 @@
         fieldSelector: SELECTOR_FIELD,
         eventsMap: [
             {
-                selector: '.ez-field-edit--ezemail input',
+                selector: '.ibexa-field-edit--ezemail input',
                 eventName: 'blur',
                 callback: 'validateInput',
                 errorNodeSelectors: [SELECTOR_ERROR_NODE],
