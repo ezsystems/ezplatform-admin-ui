3dc23d9fd3db0c537e4c9213fe334624d8a775d0
Nattfarinn <nattfarinn@gmail.com>
2021-08-31 11:09:43 +0200

    [Composer] Upgraded Ibexa Code Style to v1.0

---
+++ .php-cs-fixer.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+use Ibexa\CodeStyle\PhpCsFixer\InternalConfigFactory;
+
+$configFactory = new InternalConfigFactory();
+$configFactory->withRules([
+    'declare_strict_types' => false,
+]);
+
+return $configFactory
+    ->buildConfig()
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->in(__DIR__ . '/src')
+            ->files()->name('*.php')
+    );
