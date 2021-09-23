ece784b4d6477ac3f8d1aa7ce97c12b9221c580f
Michał Grabowski <michal.grabowski@ez.no>
2021-09-23 11:27:25 +0200

    IBX-1097: Change properties for reusable Dropdown widget

--- src/bundle/Resources/public/js/scripts/admin.dropdown.js
+++ src/bundle/Resources/public/js/scripts/admin.dropdown.js
@@ -1,5 +1,5 @@
 (function(global, doc, eZ) {
-    const dropdowns = doc.querySelectorAll('.ibexa-dropdown');
+    const dropdowns = doc.querySelectorAll('.ibexa-dropdown:not(.ibexa-dropdown--custom-init)');
 
     dropdowns.forEach((dropdownContainer) => {
         const dropdown = new eZ.core.Dropdown({
