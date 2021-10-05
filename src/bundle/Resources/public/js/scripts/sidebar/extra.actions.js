f3e58551285cf6946443012023af457c188bcafb
Lukasz Ostafin <ostafin.lucas@gmail.com>
2021-10-05 11:32:13 +0200

    Redesign pc attributes

--- src/bundle/Resources/public/js/scripts/sidebar/extra.actions.js
+++ src/bundle/Resources/public/js/scripts/sidebar/extra.actions.js
@@ -28,9 +28,12 @@
         btn.addEventListener(
             'click',
             () => {
-                const actions = doc.querySelector(`.ibexa-extra-actions[data-actions="${btn.dataset.actions}"]`);if (btn.dataset.validate && !parseInt(btn.dataset.isFormValid, 10)) {
+                const actions = doc.querySelector(`.ibexa-extra-actions[data-actions="${btn.dataset.actions}"]`);
+
+                if (btn.dataset.validate && !parseInt(btn.dataset.isFormValid, 10)) {
                     return;
                 }
+                
                 const isHidden = haveHiddenPart(actions);
                 const methodNameButton = isHidden ? 'add' : 'remove';
                 const methodNameContainer = isHidden ? 'remove' : 'add';
