739280d2c9254d3eaca327e61562535649d0caeb
Adam Wójs <adam@wojs.pl>
2021-10-13 11:47:58 +0200

    IBX-1051: Implemented global search form (#1956)
    * IBX-1051: Implemented global search form
    
    Co-authored-by: Dariusz Szut <dew326@gmail.com>

---
+++ src/lib/Form/Type/Search/GlobalSearchType.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\AdminUi\Form\Type\Search;
+
+use Ibexa\Platform\Bundle\Search\Form\Data\SearchData;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class GlobalSearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('query', SearchType::class, [
+            'required' => false,
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => SearchData::class,
+        ]);
+    }
+}
