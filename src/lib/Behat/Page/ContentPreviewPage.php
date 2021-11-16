67a36af5a1ff1636410bc5ae10e08ea7aa9a7169
Dariusz Szut <dew326@gmail.com>
2021-11-16 15:50:29 +0100

    removed title locator

--- src/lib/Behat/Page/ContentPreviewPage.php
+++ src/lib/Behat/Page/ContentPreviewPage.php
@@ -35,7 +35,6 @@ class ContentPreviewPage extends Page
         return [
             new VisibleCSSLocator('previewNav', '.ibexa-preview-header'),
             new VisibleCSSLocator('backToEdit', '.ibexa-preview-header .ibexa-preview-header__item--back a'),
-            new VisibleCSSLocator('title', '.ibexa-preview-header .ibexa-preview-header__item--description'),
             new VisibleCSSLocator('desktop', '.ibexa-preview-header .ibexa-preview-header__item--actions .ibexa-icon--desktop'),
             new VisibleCSSLocator('tablet', '.ibexa-preview-header .ibexa-preview-header__item--actions .ibexa-icon--tablet'),
             new VisibleCSSLocator('mobile', '.ibexa-preview-header .ibexa-preview-header__item--actions .ibexa-icon--mobile'),
