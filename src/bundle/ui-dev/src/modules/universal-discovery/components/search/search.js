eea83781ca4a92a9385daf609b8d9e86fcf6574a
Mateusz Dębiński <58430570+mateuszdebinski@users.noreply.github.com>
2021-11-29 11:52:54 +0100

    IBX-1465: Prioritize current site access when choosing language (#2018)
    * IBX-1465: Prioritize current site access when choosing language
    
    * CS
    
    * Added missing $siteAccessService argument in yaml
    
    * changed SiteAccessService to SiteAccessServiceInterface
    
    Co-authored-by: Mateusz Dębiński <mateusz.debinski@ibexa.co>

--- src/bundle/ui-dev/src/modules/universal-discovery/components/search/search.js
+++ src/bundle/ui-dev/src/modules/universal-discovery/components/search/search.js
@@ -26,7 +26,10 @@ const selectedContentTypesReducer = (state, action) => {
     }
 };
 
-const languages = Object.values(window.eZ.adminUiConfig.languages.mappings);
+const configLanguages = window.eZ.adminUiConfig.languages;
+const languages = configLanguages.priority.map((value) => {
+    return configLanguages.mappings[value];
+});
 
 const Search = ({ itemsPerPage }) => {
     const filtersLabel = Translator.trans(/*@Desc("Filters")*/ 'search.filters', {}, 'universal_discovery_widget');
