972eaf40633046cb60a091a8881e4d8028de330d
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2021-12-02 15:07:42 +0100

    IBX-1514: Applied remark

--- src/lib/UniversalDiscovery/Event/ConfigResolveEvent.php
+++ src/lib/UniversalDiscovery/Event/ConfigResolveEvent.php
@@ -14,7 +14,7 @@ class ConfigResolveEvent extends Event
 {
     public const NAME = 'udw.resolve.config';
 
-    public const READ_SPECIFIC_CONFIGURATIONS = ['richtext_embed', 'richtext_embed_image', 'browse'];
+    private const READ_SPECIFIC_CONFIGURATIONS = ['richtext_embed', 'richtext_embed_image', 'browse'];
 
     /** @var string */
     protected $configName;
