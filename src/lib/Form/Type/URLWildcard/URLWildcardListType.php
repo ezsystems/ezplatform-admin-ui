ea73aff53d24f71bc74b1692270cc87bc1b4266c
Mateusz Dębiński <mateusz.debinski@ibexa.co>
2022-05-24 10:36:25 +0200

    Added implements TranslationContainerInterface

--- src/lib/Form/Type/URLWildcard/URLWildcardListType.php
+++ src/lib/Form/Type/URLWildcard/URLWildcardListType.php
@@ -9,6 +9,8 @@ declare(strict_types=1);
 namespace Ibexa\AdminUi\Form\Type\URLWildcard;
 
 use Ibexa\AdminUi\Form\Data\URLWildcard\URLWildcardListData;
+use JMS\TranslationBundle\Model\Message;
+use JMS\TranslationBundle\Translation\TranslationContainerInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
@@ -19,16 +21,20 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 /**
  * URLWildcard list form.
  */
-final class URLWildcardListType extends AbstractType
+final class URLWildcardListType extends AbstractType implements TranslationContainerInterface
 {
+    private const TYPE_DIRECT = 'url_wildcard.type.direct';
+    private const TYPE_FORWARD = 'url_wildcard.type.forward';
+    private const PLACEHOLDER = 'url_wildcard.type.all';
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add('type', ChoiceType::class, [
             'choices' => [
-                'url_wildcard.type.direct' => true,
-                'url_wildcard.type.forward' => false,
+                self::TYPE_DIRECT => true,
+                self::TYPE_FORWARD => false,
             ],
-            'placeholder' => 'url_wildcard.type.all',
+            'placeholder' => self::PLACEHOLDER,
             'required' => false,
         ]);
 
@@ -46,5 +52,20 @@ final class URLWildcardListType extends AbstractType
             'data_class' => URLWildcardListData::class,
             'translation_domain' => 'ezplatform_url_wildcard',
         ]);
+    }
+
+
+    /**
+     * Returns an array of messages.
+     *
+     * @return array<Message>
+     */
+    public static function getTranslationMessages()
+    {
+        return [
+            (new Message(self::TYPE_DIRECT, 'ezplatform_url_wildcard'))->setDesc('Direct'),
+            (new Message(self::TYPE_FORWARD, 'ezplatform_url_wildcard'))->setDesc('Forward'),
+            (new Message(self::PLACEHOLDER, 'ezplatform_url_wildcard'))->setDesc('All'),
+        ];
     }
 }
