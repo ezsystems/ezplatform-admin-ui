157264ea7ad16e7ffd88f621814df11048f31b08
Dawid Parafiński <dawid.parafinski@ez.no>
2022-05-27 11:06:58 +0200

    IBX-2995: Fixed parameters order for AuthenticationSuccessHandler (#2051)

--- src/lib/Security/Authentication/RedirectToDashboardAuthenticationSuccessHandler.php
+++ src/lib/Security/Authentication/RedirectToDashboardAuthenticationSuccessHandler.php
@@ -10,6 +10,7 @@ namespace EzSystems\EzPlatformAdminUi\Security\Authentication;
 
 use eZ\Publish\Core\MVC\Symfony\Security\Authentication\DefaultAuthenticationSuccessHandler;
 use EzSystems\EzPlatformAdminUi\Specification\SiteAccess\IsAdmin;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\HttpUtils;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
@@ -29,14 +30,16 @@ class RedirectToDashboardAuthenticationSuccessHandler extends DefaultAuthenticat
      * @param array $options
      * @param array $siteAccessGroups
      * @param string $defaultTargetPath
+     * @param \Psr\Log\LoggerInterface|null $logger
      */
     public function __construct(
         HttpUtils $httpUtils,
         array $options,
         array $siteAccessGroups,
-        string $defaultTargetPath
+        string $defaultTargetPath,
+        LoggerInterface $logger = null
     ) {
-        parent::__construct($httpUtils, $options);
+        parent::__construct($httpUtils, $options, $logger);
         $this->siteAccessGroups = $siteAccessGroups;
         $this->defaultTargetPath = $defaultTargetPath;
     }
