29644a298d6020e862a3c0f37593b7d02176e303
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2022-08-09 15:08:43 +0200

    IBX-2964: Removed unused arguments

--- src/bundle/Resources/public/js/scripts/widgets/flatpickr.js
+++ src/bundle/Resources/public/js/scripts/widgets/flatpickr.js
@@ -1,8 +1,8 @@
 import flatpickrLanguages
     from '../../../../../../../../../../public/bundles/ezplatformadminuiassets/vendors/flatpickr/dist/l10n';
 
-(function (global, doc, eZ, flatpickr) {
+(function (eZ, flatpickr) {
     const backOfficeLanguage = eZ.adminUiConfig.backOfficeLanguage;
     const flatpickrLanguage = flatpickrLanguages[backOfficeLanguage] || flatpickrLanguages.default;
     flatpickr.localize(flatpickrLanguage);
-})(window, window.document, window.eZ, window.flatpickr);
+})(window.eZ, window.flatpickr);
